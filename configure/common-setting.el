37dbc53f378adb8873ced4da1e217939f05fdc9a
zhoujd <zjd-405@163.com>
2012-05-12 22:32:22 +0800

    modify font to origin

--- configure/common-setting.el
+++ configure/common-setting.el
@@ -45,40 +45,38 @@
   (t (my-set-language-utf-8)))
 
 ;;font setting
-(defun my-font-existsp (font)
-  (if (null (x-list-fonts font)) nil t))
-
-(defun my-make-font-string (font-name font-size)
-  (if (and (stringp font-size)
-           (equal ":" (string (elt font-size 0))))
-     (format "%s%s" font-name font-size)     
-     (format "%s %s" font-name font-size)))
-
-(defun my-set-font (english-fonts english-font-size
-                    chinese-fonts &optional chinese-font-size)
-  "english-font-size could be set to \":pixelsize=18\" or a integer.
-If set/leave chinese-font-size to nil, it will follow english-font-size"
-  ;; The following 2 method cannot make the font settig work in new frames.
-  ;;(set-default-font "Consolas:pixelsize=18")
-  ;;(add-to-list 'default-frame-alist '(font . "Consolas:pixelsize=18"))
-  ;; We have to use set-face-attribute
-  (require 'cl)   ; for find if
-  (let ((en-font (my-make-font-string
-                  (find-if #'my-font-existsp english-fonts)
-                  english-font-size))
-        (zh-font (font-spec :family (find-if #'my-font-existsp chinese-fonts)
-                            :size chinese-font-size))) 
-    (set-face-attribute 'default nil :font en-font)
-    (dolist (charset '(kana han symbol cjk-misc bopomofo))
-      (set-fontset-font (frame-parameter nil 'font)
-                        charset
-                        zh-font))))
-
 (defvar en-font-list '("Consolas"  "Monaco" "DejaVu Sans Mono" "Monospace" "Courier New"))
 (defvar cn-font-list '("Microsoft Yahei" "文泉驿等宽微米黑" "黑体" "新宋体" "宋体")) 
 
-(my-set-font en-font-list "11"
-             cn-font-list  12)
+(defvar my-font-en-name (nth 0 en-font-list))
+(defvar my-font-en-size 11)
+
+(defvar my-font-cn-name (nth 0 cn-font-list))
+(defvar my-font-cn-size 12)
+
+(setq my-font-string
+      (concat my-font-en-name " "
+              (number-to-string my-font-en-size)))
+
+(defun my-frame-font ()
+  "my frame font setting"
+  ;; Setting English Font
+  (set-face-attribute
+   'default nil :font my-font-string)
+  ;; Chinese Font
+  (dolist (charset '(kana han symbol cjk-misc bopomofo))
+    (set-fontset-font (frame-parameter nil 'font)
+                      charset
+                      (font-spec :family my-font-cn-name
+                                 :size   my-font-cn-size))))
+
+(defun my-console-font ()
+  "my console font setting"
+  (progn
+    (add-to-list 'default-frame-alist
+                 '(font . my-font-string))))
+
+(if window-system (my-frame-font) (my-console-font))
 
 ;;server-mode
 ;;emacsclientw.exe -f "~\.emacs.d\server\server" -n -a "runemacs.exe" path\to\file

1ea4406e5a9044ef75ee075cd49cd6a6570efc43
zhoujd <zjd-405@163.com>
2012-05-13 12:53:53 +0800

    server-mode unsafe change

--- configure/common-setting.el
+++ configure/common-setting.el
@@ -84,7 +84,7 @@
 ;;~/.emacs.d/server的属主由Administrators组改为当前用户（右键属性--安全--高级--所有者）
 (require 'server)
 (when (and (= emacs-major-version 23)
-	   (equal window-system 'w32))
+           (or (eq window-system 'w32) (eq window-system 'win32)))
   (defun server-ensure-safe-dir (dir) "Noop" t)) ; Suppress error "directory
 					                             ; ~/.emacs.d/server is unsafe"
 					                             ; on windows.

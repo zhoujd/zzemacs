67a16a6a12c71ca1de3895c698563cd684b82119
zhoujd <zjd-405@163.com>
2012-05-29 23:15:10 +0800

    win32 find grep fixede

--- configure/other-setting.el
+++ configure/other-setting.el
@@ -420,8 +420,10 @@ Emacs buffer are those starting with “*”."
 ;;win32 find grep set
 (if (or (eq window-system 'w32)
         (eq window-system 'win32))
-    (setq find-program "\"find2.exe\"")
-    (setq grep-program "\"grep.exe\""))
+    (progn
+      (setq find-program "\"find2.exe\"")
+      (setq grep-program "\"grep.exe\"")
+      ))
 
 (provide 'other-setting)
 

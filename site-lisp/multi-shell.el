cfeb189cb1c54643d18081539d64aa660f149bc9
zhoujd <zjd-405@163.com>
2012-06-18 20:57:30 +0800

    multi-shell modify

--- site-lisp/multi-shell.el
+++ site-lisp/multi-shell.el
@@ -1,5 +1,55 @@
 ;;; multi-shell.el --- Multi-Shell Manager
 
+;; Filename: multi-shell.el
+;; Description: Multi-Shell Manager
+;; Author: Andy Stewart lazycat.manatee@gmail.com
+;; Maintainer: Andy Stewart lazycat.manatee@gmail.com
+;; Copyright (C) 2008, 2009, Andy Stewart, all rights reserved.
+;; Created: 2008-12-10 12:03:20
+;; Version: 0.2.2
+;; Last-Updated: 2009-06-29 18:04:34
+;;           By: Andy Stewart
+;; URL:
+;; Keywords: multi-shell, shell
+;; Compatibility: GNU Emacs 23.0.60.1
+;;
+;; Features that might be required by this library:
+;;
+;; `shell' `ansi-color' `cl'
+;;
+
+;;; This file is NOT part of GNU Emacs
+
+;;; License
+;;
+;; This program is free software; you can redistribute it and/or modify
+;; it under the terms of the GNU General Public License as published by
+;; the Free Software Foundation; either version 3, or (at your option)
+;; any later version.
+
+;; This program is distributed in the hope that it will be useful,
+;; but WITHOUT ANY WARRANTY; without even the implied warranty of
+;; MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+;; GNU General Public License for more details.
+
+;; You should have received a copy of the GNU General Public License
+;; along with this program; see the file COPYING.  If not, write to
+;; the Free Software Foundation, Inc., 51 Franklin Street, Fifth
+;; Floor, Boston, MA 02110-1301, USA.
+
+;;; Commentary:
+;;
+;; Multi-Shell Manager
+;;
+;; Here, have below command you can use:
+;;
+;;      `multi-shell-new'                create a new shell buffer.
+;;      `multi-shell-next'               switch to next shell buffer.
+;;      `multi-shell-prev'               switch to previous shell buffer.
+;;      `multi-shell-current-directory'  create a new shell with current-directory.
+;;
+;;      choose your like key bind above command. ;)
+
 ;;; Require
 (require 'shell)
 (require 'ansi-color)

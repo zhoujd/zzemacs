322525c795c6f6b336d1f391c812b0fb88499c18
zhoujd <zjd-405@163.com>
2012-06-23 00:53:34 +0800

    build.sh

--- bin/build.sh
+++ bin/build.sh
@@ -1,5 +1,6 @@
-#/bin/sh
+#!/bin/sh
 
+sudo apt-get build-dep emacs23
 sudo apt-get install libxpm-dev libpng12-dev libjpeg-dev libtiff4-dev libgif-dev
 sudo apt-get install libxaw7-dev libncurses5-dev libgtk2.0-dev librsvg2-dev libdbus-1-dev libgconf2-dev
 sudo apt-get install libotf-dev libgpm-dev libm17n-dev libgnutls-dev libselinux1-dev imagemagick

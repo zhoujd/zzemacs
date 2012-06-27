e7df7071df18b32867448d10db0aa427e5b0b7d6
zhoujd <zjd-405@163.com>
2012-06-27 23:10:04 +0800

    perl filename

--- configure/perl-setting.el
+++ configure/perl-setting.el
@@ -5,6 +5,12 @@
 
 (defalias 'perl-mode 'cperl-mode)
 
+;; Use cperl-mode instead of the default perl-mode
+(add-to-list 'auto-mode-alist '("\\.\\([pP][Llm]\\|al\\)\\'" . cperl-mode))
+(add-to-list 'interpreter-mode-alist '("perl" . cperl-mode))
+(add-to-list 'interpreter-mode-alist '("perl5" . cperl-mode))
+(add-to-list 'interpreter-mode-alist '("miniperl" . cperl-mode))
+
 (defun pde-perl-mode-hook ()
   (abbrev-mode t)
   (hs-minor-mode t)

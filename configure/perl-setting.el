3462c66e6aa2baf442a740ffe1b11961925adc24
zhoujd <zjd-405@163.com>
2012-06-27 22:23:47 +0800

    perl setting

--- configure/perl-setting.el
+++ configure/perl-setting.el
@@ -4,11 +4,27 @@
 (load "pde-load")
 
 (defalias 'perl-mode 'cperl-mode)
-(add-hook 'cperl-mode-hook
-	  (lambda ()
-	    (local-set-key (kbd "C-h f") 'cperl-perldoc)
-        (setq cperl-auto-newline nil)
-        ))
+
+(defun pde-perl-mode-hook ()
+  (abbrev-mode t)
+  (hs-minor-mode t)
+  (add-to-list 'cperl-style-alist
+               '("PDE"
+                 (cperl-auto-newline                         . nil)
+                 (cperl-brace-offset                         . 0)
+                 (cperl-close-paren-offset                   . -4)
+                 (cperl-continued-brace-offset               . 0)
+                 (cperl-continued-statement-offset           . 4)
+                 (cperl-extra-newline-before-brace           . nil)
+                 (cperl-extra-newline-before-brace-multiline . nil)
+                 (cperl-indent-level                         . 4)
+                 (cperl-indent-parens-as-block               . t)
+                 (cperl-label-offset                         . -4)
+                 (cperl-merge-trailing-else                  . t)
+                 (cperl-tab-always-indent                    . t)))
+  (cperl-set-style "PDE"))
+
+(add-hook 'cperl-mode-hook 'pde-perl-mode-hook)
 
 (provide 'perl-setting)
 

62d7e34e061419a233220df80ddba04bafbaf0f9
zhoujd <zjd-405@163.com>
2012-06-29 18:38:49 +0800

    chmod u+x *.sh

--- bin/build.sh
+++ bin/build.sh
@@ -1,9 +1,9 @@
 #!/bin/sh
 
 sudo apt-get build-dep emacs23
-sudo apt-get install libxpm-dev libpng12-dev libjpeg-dev libtiff4-dev libgif-dev
-sudo apt-get install libxaw7-dev libncurses5-dev libgtk2.0-dev librsvg2-dev libdbus-1-dev libgconf2-dev
-sudo apt-get install libotf-dev libgpm-dev libm17n-dev libgnutls-dev libselinux1-dev imagemagick
+sudo apt-get install -y libxpm-dev libpng12-dev libjpeg-dev libtiff4-dev libgif-dev
+sudo apt-get install -y libxaw7-dev libncurses5-dev libgtk2.0-dev librsvg2-dev libdbus-1-dev libgconf2-dev
+sudo apt-get install -y libotf-dev libgpm-dev libm17n-dev libgnutls-dev libselinux1-dev imagemagick
 
 ./configure
 make

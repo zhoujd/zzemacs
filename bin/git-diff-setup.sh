b6332f563cd6023453fd30997f9630c99b20072e
zhoujd <zjd-405@163.com>
2012-06-29 21:51:33 +0800

    git diff save

--- bin/git-diff-setup.sh
+++ bin/git-diff-setup.sh
@@ -2,7 +2,7 @@
 
 GIT_SETUP_HOME=`pwd`
 
-echo git diff setup end ...
+echo git diff setup start ...
 
 # setup packages
 sudo apt-get install -y python-nautilus python-configobj python-gtk2 python-glade2 python-svn python-dbus meld
@@ -10,11 +10,14 @@ sudo apt-get install -y python-meld3
 sudo apt-get install -y git-core
 
 # setup git configure
-git config --global user.name  "zhoujd"          # 姓名全拼 
-git config --global user.email "zjd-405@163.com" # 公司邮箱 
+git config --global user.name  "zhoujd"
+git config --global user.email "zjd-405@163.com"
 git config --global merge.tool "meld" 
-git config --global color.ui   true              # 使用 git 默认的配色方案，推荐 git config --
-git config --list                                # 查看配置信息 
+git config --global color.ui   true
+
+echo ===========git config start ===============
+git config --list
+echo ===========git config end =================
 
 # setup diff setting
 chmod +x $GIT_SETUP_HOME/git-diff-wrapper.sh 

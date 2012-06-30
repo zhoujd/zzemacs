26dc0044d5376a9d1d30106f5bae5c171db098f9
zhoujd <zjd-405@163.com>
2012-07-01 01:02:46 +0800

    git diff tools

--- bin/git-diff-wrapper.sh
+++ bin/git-diff-wrapper.sh
@@ -1,7 +1,10 @@
 #!/bin/sh
 
-DIFF_TOOL="D:/develop/BCompare3/BCompare.exe"
-#DIFF_TOOL="meld"
+if [ "$OS" = "Windows_NT" ] ; then
+    DIFF_TOOL="D:/develop/BCompare3/BCompare.exe"
+else
+    DIFF_TOOL="meld"
+fi
 
 # diff is called by git with 7 parameters:
 # path old-file old-hex old-mode new-file new-hex new-mode

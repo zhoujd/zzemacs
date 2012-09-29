5ba3f4979cf7cc6b49bf2368a701167eff52a0bd
zhoujd <jiandongx.zhou@intel.com>
2012-09-29 10:39:49 +0800

    add muse support

---
+++ configure/muse-setting.el
@@ -0,0 +1,32 @@
+;;;this muse setting
+
+(zz-load-path "site-lisp/muse/lisp")
+
+(require 'muse-mode)
+
+(require 'muse-html)        
+(require 'muse-latex)       
+(require 'muse-texinfo)     
+(require 'muse-docbook)     
+(require 'muse-wiki nil t)   
+(require 'muse-project)     
+
+;;set code format to utf-8
+(setq muse-html-meta-content-type (concat "text/html; charset=utf-8"))
+
+;;html charset
+(setq muse-html-charset-default "utf-8")
+
+;;html encoding
+(setq muse-html-encoding-default "utf8")
+
+;;add my wiki project
+(setq muse-project-alist
+      '(("MyWiki"
+         ("~/wiki" :default "index")
+         (:base "html" :path "~/wiki/publish"))))
+
+
+
+
+;;; muse-setting.el

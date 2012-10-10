9fb760ac5c0f60a84ed223c04f4ca4068c5fc446
zhoujd <zjd-405@163.com>
2012-10-11 05:00:58 +0800

    key for console/x-window

--- configure/key-setting.el
+++ configure/key-setting.el
@@ -1,116 +1,101 @@
 ;;;; key-setting.el --- key config file
 ;;;
 
-;;myself key map
-(defvar esc-f2-map (make-sparse-keymap) "Keymap for self related commands.")
-(define-key esc-map [f2] esc-f2-map)
+;;f2 key map 
+(defvar f2-map (make-sparse-keymap) "Keymap for self related commands.")
+(define-key global-map [f2] f2-map)
 
-(define-key esc-f2-map (kbd "1")  'my-utf-8)
+;;f3 key map 
+(defvar f3-map (make-sparse-keymap) "Keymap for self related commands.")
+(define-key global-map [f3] f3-map)
 
-;;f12 key map 
-(defvar f12-map (make-sparse-keymap) "Keymap for self related commands.")
-(define-key global-map [f12] f12-map)
-
-(define-key f12-map (kbd "C-1") 'my-utf-8)
-(define-key f12-map (kbd "C-h") 'sourcepair-jump-to-headerfile)
-(unless (or (eq window-system 'w32)
-            (eq window-system 'win32))
-  (define-key f12-map (kbd "C-d") 'open-with-nautilus)
-  (define-key f12-map (kbd "C-t") 'open-with-terminal))
+;;f4 key map 
+(defvar f4-map (make-sparse-keymap) "Keymap for self related commands.")
+(define-key global-map [f4] f4-map)
 
 ;;switch to shells
-(define-key f12-map [f9]
+(define-key ctl-x-map [f9]
   (lambda () (interactive) (switch-to-shell "*shell-f9*")))
-(define-key f12-map [f10]
+(define-key ctl-x-map [f10]
   (lambda () (interactive) (switch-to-shell "*shell-f10*")))
-(define-key f12-map [f11]
+(define-key ctl-x-map [f11]
   (lambda () (interactive) (switch-to-shell "*shell-f11*")))
 
 ;;for info
-(global-set-key [S-f1]   'session-save)
-(global-set-key [C-f1]   'session-restore)
-(global-set-key [M-S-f1] 'recentf-open-files)
-(global-set-key [C-S-f1] 'recentf-open-files-compl)
+(global-set-key (kbd "<f1> <f1>")   'session-save)
+(global-set-key (kbd "<f2> <f1>")   'session-restore)
+(global-set-key (kbd "<f3> <f1>")   'recentf-open-files)
+(global-set-key (kbd "<f4> <f1>")   'recentf-open-files-compl)
 
-;(global-set-key [C-f2]   'bm-toggle)
-;(global-set-key [f2]     'bm-next)
-;(global-set-key [S-f2]   'bm-previous)
-;(global-set-key [C-S-f2] 'bm-remove-all-current-buffer)
-;(global-set-key [M-f2]   'bm-menu-show)
-;(global-set-key [M-S-f2] 'bm-menu-show)
+(global-set-key (kbd "<f1> <f2>")   'bc-set)
+(global-set-key (kbd "<f2> <f2>")   'bc-clear)
+(global-set-key (kbd "<f3> <f2>")   'bc-next)
+(global-set-key (kbd "<f4> <f2>")   'bc-previous)
+(define-key esc-map [f2] 'bc-list)
 
-(global-set-key [C-f2]   'bc-set)
-(global-set-key [f2]     'bc-next)
-(global-set-key [S-f2]   'bc-previous)
-(global-set-key [C-S-f2] 'bc-clear)
-(global-set-key [M-f2]   'bc-list)
-(global-set-key [M-S-f2] 'bc-list)
+(global-set-key (kbd "<f1> <f3>")   'my-last-buffer-go)
+(global-set-key (kbd "<f2> <f3>")   'list-bookmarks)
+(global-set-key (kbd "<f4> <f3>")   'my-occur)
 
-(global-set-key [f3]     'my-last-buffer-go)
-(global-set-key [C-f3]   'list-bookmarks)
-(global-set-key [M-f3]   'my-occur)
+(global-set-key (kbd "<f1> <f4>")   'bc-local-previous)
+(global-set-key (kbd "<f2> <f4>")   'bc-local-next)
+(global-set-key (kbd "<f3> <f4>")   'undo-kill-buffer)
+(global-set-key (kbd "<f4> <f4>")   'kill-this-buffer)
 
-(global-set-key [f4]     'kill-this-buffer)
-(global-set-key [C-f4]   'undo-kill-buffer)
-
-(global-set-key [C-S-f3]   'highlight-symbol-next)
-(global-set-key [C-S-f4]   'highlight-symbol-prev)
-
-(global-set-key [S-f3]   'bc-local-previous)
-(global-set-key [S-f4]   'bc-local-next)
-
-(global-set-key [f5]     'speedbar-get-focus)
-(global-set-key [S-f5]   'sr-speedbar-toggle)
-(global-set-key [C-S-f5] 'line-to-top-of-window)
-(global-set-key [C-f5]   'etags-stack-show)
+(global-set-key (kbd "<f1> <f5>")   'speedbar-get-focus)
+(global-set-key (kbd "<f2> <f5>")   'sr-speedbar-toggle)
+(global-set-key (kbd "<f3> <f5>")   'line-to-top-of-window)
+(global-set-key (kbd "<f4> <f5>")   'etags-stack-show)
 
 (if (or (eq window-system 'w32)
         (eq window-system 'win32))
  (progn ;; For Windows
-   (global-set-key [f6]     'multi-shell-new)
-   (global-set-key [S-f6]   'multi-shell-current-directory)
-   (global-set-key [C-S-f6] 'switch-to-shell)
-   (global-set-key [C-f6]   'multi-shell-next)
-   (global-set-key [M-f6]   'multi-shell-prev))   
+   (global-set-key (kbd "<f1> <f6>")  'multi-shell-new)
+   (global-set-key (kbd "<f2> <f6>")  'multi-shell-current-directory)
+   (global-set-key (kbd "<f3> <f6>")  'multi-shell-next)
+   (global-set-key (kbd "<f4> <f6>")  'multi-shell-prev)
+   (define-key esc-map [f6] 'switch-to-shell)
+   )   
  (progn ;; For Linux
-   (global-set-key [f6]     'get-term)
-   (global-set-key [S-f6]   'multi-term-dedicated-toggle)
-   (global-set-key [C-S-f6] 'switch-to-term)
-   (global-set-key [C-f6]   'multi-term-next)
-   (global-set-key [M-f6]   'multi-term-prev)))
+   (global-set-key (kbd "<f1> <f6>")  'get-term)
+   (global-set-key (kbd "<f2> <f6>")  'multi-term-dedicated-toggle)
+   (global-set-key (kbd "<f3> <f6>")  'multi-term-next)
+   (global-set-key (kbd "<f4> <f6>")  'multi-term-prev)
+   (define-key esc-map [f6] 'switch-to-term)
+   ))
 
-(global-set-key [f7]     'compile)
-(global-set-key [S-f7]   'switch-to-compilation)
-(global-set-key [C-f7]   'next-error)
-(global-set-key [M-f7]   'previous-error)
-(global-set-key [C-S-f7] 'previous-error)
+(global-set-key (kbd "<f1> <f7>")   'compile)
+(global-set-key (kbd "<f2> <f7>")   'switch-to-compilation)
+(global-set-key (kbd "<f3> <f7>")   'next-error)
+(global-set-key (kbd "<f4> <f7>")   'previous-error)
 
-(global-set-key [f8]     'gdb)
-(global-set-key [M-f8]   'gud-tooltip-mode)
-(global-set-key [C-S-f8] 'gud-kill)
-(global-set-key [S-f8]   'gdb-many-windows)
-(global-set-key [C-f8]   'gdb-restore-windows)
+(global-set-key (kbd "<f1> <f8>")   'gdb)
+(global-set-key (kbd "<f2> <f8>")   'gud-tooltip-mode)
+(global-set-key (kbd "<f3> <f8>")   'gdb-many-windows)
+(global-set-key (kbd "<f4> <f8>")   'gdb-restore-windows)
+(define-key esc-map [f8] 'gud-kill)
 
-(global-set-key [f9]     'shell)
-(global-set-key [S-f9]   'multi-shell-new)
-(global-set-key [C-f9]   'switch-to-scratch)
-(global-set-key [M-f9]   'popup-term)
-(global-set-key [C-S-f9] 'switch-to-shell)
+(global-set-key (kbd "<f1> <f9>")   'shell)
+(global-set-key (kbd "<f2> <f9>")   'multi-shell-new)
+(global-set-key (kbd "<f3> <f9>")   'switch-to-scratch)
+(global-set-key (kbd "<f4> <f9>")   'popup-term)
+(define-key esc-map [f9]  'switch-to-shell)
 
-(global-set-key [C-f10]   'tool-bar-mode)
-(global-set-key [S-f10]   'menu-bar-mode)
-(global-set-key [M-f10]   'my-toggle-maxframe)
-(global-set-key [C-S-f10] 'scroll-bar-mode)
-(global-set-key [M-S-f10] 'my-toggle-fullscreen)
+(global-set-key (kbd "<f1> <f10>")  'tool-bar-mode)
+(global-set-key (kbd "<f2> <f10>")  'scroll-bar-mode)
+(global-set-key (kbd "<f3> <f10>")  'my-toggle-maxframe)
+(global-set-key (kbd "<f4> <f10>")  'my-toggle-fullscreen)
 
-(global-set-key [f11]     'linum-mode)
-(global-set-key [C-f11]   'hl-line-mode)
-(global-set-key [M-f11]   'blank-mode)
-(global-set-key [S-f11]   'fci-mode)
+(global-set-key (kbd "<f1> <f11>")   'linum-mode)
+(global-set-key (kbd "<f2> <f11>")   'hl-line-mode)
+(global-set-key (kbd "<f3> <f11>")   'blank-mode)
+(global-set-key (kbd "<f4> <f11>")   'fci-mode)
 
-(global-set-key [C-f12] 'find-name-dired)
-(global-set-key [M-f12] 'my-c-rgrep)
-(global-set-key [S-f12] 'rgrep)
+(global-set-key (kbd "<f1> <f12>") 'find-name-dired)
+(global-set-key (kbd "<f2> <f12>") 'my-c-rgrep)
+(global-set-key (kbd "<f3> <f12>") 'rgrep)
+(global-set-key (kbd "<f4> <f12>") 'find-grep)
+
 (global-set-key (kbd "C-x <f12>") 'my-unicad-switch)
 (global-set-key (kbd "C-c <f12>") 'my-os-file-switch)
 
@@ -154,7 +139,7 @@
 (global-set-key (kbd "<f1> =") 'gud-down)
 
 (global-set-key (kbd "M-0") 'other-window)
-(global-set-key (kbd "C-0") 'other-frame)
+(global-set-key (kbd "\e0") 'other-frame)
 
 (global-unset-key (kbd "C-x C-b"))
 (global-set-key (kbd "C-x C-b") 'ibuffer)
@@ -164,21 +149,6 @@
 
 (global-set-key (kbd "C-x M-s") 'sudo-unset-ro-or-save)
 (global-set-key (kbd "C-x M-f") 'sudo-find-file)
-
-;;quick move other windows
-(global-set-key [M-up]    'windmove-up)
-(global-set-key [M-down]  'windmove-down)
-(global-set-key [M-right] 'windmove-right)
-(global-set-key [M-left]  'windmove-left)
-;;window size change
-(global-set-key [C-S-up]    'enlarge-window)
-(global-set-key [C-S-down]  'shrink-window)
-(global-set-key [C-S-right] 'enlarge-window-horizontally)
-(global-set-key [C-S-left]  'shrink-window-horizontally)
-
-;;winner restore
-(global-set-key (kbd "C-,") 'winner-undo)
-(global-set-key (kbd "C-.") 'winner-redo)
 
 (global-set-key (kbd "%") 'match-paren)
 
@@ -190,10 +160,6 @@
 (global-set-key (kbd "M-]") 'tabbar-forward)
 (global-set-key (kbd "M-[") 'tabbar-backward)
 
-; C-- is undo (removes the shift), C-= is redo
-(global-set-key [(control -)] 'undo)
-(global-set-key [(control =)] 'redo)
-
 (if (or (eq window-system 'w32)
         (eq window-system 'win32))
  (progn ;; For Windows
@@ -203,10 +169,41 @@
   (global-set-key [C-mouse-4] 'text-scale-increase)
   (global-set-key [C-mouse-5] 'text-scale-decrease)))
 
-;;shift+space for mark
-(global-set-key (kbd "S-<SPC>") 'set-mark-command)
+
 ;;Control tab quotes a tab.
 (global-set-key [C-tab] "\C-q\t")
+
+;;f2-map setting
+(define-key f2-map (kbd "C-1") 'my-utf-8)
+(define-key f2-map (kbd "C-h") 'sourcepair-jump-to-headerfile)
+(unless (or (eq window-system 'w32)
+            (eq window-system 'win32))
+  (define-key f2-map (kbd "C-d") 'open-with-nautilus)
+  (define-key f2-map (kbd "C-t") 'open-with-terminal))
+
+;;undo/redo
+(define-key f2-map (kbd "-") 'undo)
+(define-key f2-map (kbd "=") 'redo)
+
+;;winner restore
+(define-key f2-map (kbd ",") 'winner-undo)
+(define-key f2-map (kbd ".") 'winner-redo)
+
+;;quick move other windows
+(define-key f2-map [up]    'windmove-up)
+(define-key f2-map [down]  'windmove-down)
+(define-key f2-map [right] 'windmove-right)
+(define-key f2-map [left]  'windmove-left)
+
+;;for mark
+(define-key f2-map (kbd "<SPC>") 'set-mark-command)
+
+;;;f3-map setting
+;;window size change
+(define-key f3-map [up]    'enlarge-window)
+(define-key f3-map [down]  'shrink-window)
+(define-key f3-map [right] 'enlarge-window-horizontally)
+(define-key f3-map [left]  'shrink-window-horizontally)
 
 
 (provide 'key-setting)

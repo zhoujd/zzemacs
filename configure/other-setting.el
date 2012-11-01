182a6a5be553ac2a76854cb172f05238d4263e77
zhoujd <jiandongx.zhou@intel.com>
2012-11-01 13:17:36 +0800

    for shell exit

--- configure/other-setting.el
+++ configure/other-setting.el
@@ -208,17 +208,17 @@ Emacs buffer are those starting with “*”."
  '(uniquify-buffer-name-style 'reverse)
  '(uniquify-after-kill-buffer-p t))
 
-(defun kill-buffer-when-exit ()
-  "Close assotiated buffer when a process exited"
-  (let ((current-process (ignore-errors (get-buffer-process (current-buffer)))))
-    (when current-process
-      (set-process-sentinel current-process
-                            (lambda (watch-process change-state)
-                              (when (string-match "//(finished//|exited//)" change-state)
-                                (kill-buffer (process-buffer watch-process))))))))
+;(defun kill-buffer-when-exit ()
+;  "Close assotiated buffer when a process exited"
+;  (let ((current-process (ignore-errors (get-buffer-process (current-buffer)))))
+;    (when current-process
+;      (set-process-sentinel current-process
+;                            (lambda (watch-process change-state)
+;                              (when (string-match "//(finished//|exited//)" change-state)
+;                                (kill-buffer (process-buffer watch-process))))))))
 
-(add-hook 'gdb-mode-hook 'kill-buffer-when-exit)
-(add-hook 'shell-mode-hook 'kill-buffer-when-exit)
+;(add-hook 'gdb-mode-hook 'kill-buffer-when-exit)
+;(add-hook 'shell-mode-hook 'kill-buffer-when-exit)
 
 ;;tramp setting
 (require 'tramp)

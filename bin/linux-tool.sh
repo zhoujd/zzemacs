ed0bd0234d615eabdea378cee2f34b192b60b071
zhoujd <zjd-405@163.com>
2013-04-25 20:57:30 +0800

    add develop libs

--- bin/linux-tool.sh
+++ bin/linux-tool.sh
@@ -1,6 +1,11 @@
 #!/bin/sh
 
 echo install linux-tools begin...
-sudo apt-get install cairo-dock cairo-dock-plug-ins
+sudo apt-get install -y cairo-dock cairo-dock-plug-ins
+
+##develop package
+sudo apt-get install -y gnome-core-devel
+sudo apt-get install -y libx11-dev
+sudo apt-get install -y xorg-dev
 
 echo install linux-tools end...

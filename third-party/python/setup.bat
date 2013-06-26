7981de98cb084d584af46313d9a54fda99db92a6
zhoujd <zjd-405@163.com>
2013-06-26 14:40:48 +0800

    update for win7

---
+++ third-party/python/setup.bat
@@ -0,0 +1,32 @@
+@echo off
+
+set CURRENT_ROOT="D:\develop\zznix\home\zhoujd\zzemacs\third-party\python"
+set PYTHON_LIB_ROOT="D:\develop\Python27\Lib\site-packages"
+
+echo for python develop start ...
+
+cd %CURRENT_ROOT%\pymacs
+python pppp -C ppppconfig.py pppp.rst.in pymacs.el.in pymacs.rst.in Pymacs.py.in contrib tests
+python setup.py install
+cd %CURRENT_ROOT%
+
+cd %CURRENT_ROOT%\rope
+python setup.py install
+cd %CURRENT_ROOT%
+
+cd %CURRENT_ROOT%\ropemode
+python setup.py install
+cd %CURRENT_ROOT%
+
+cd %CURRENT_ROOT%\ropemacs
+python setup.py install
+cd %CURRENT_ROOT%
+
+echo install pycomplete.py to python lib
+copy %CURRENT_ROOT%\pycomplete.py %PYTHON_LIB_ROOT% > nul
+
+echo for python develop end   ...
+set CURRENT_ROOT=
+set PYTHON_LIB_ROOT=
+pause
+@echo on

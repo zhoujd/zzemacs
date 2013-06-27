a5edfa532c07e117f1d3e5ba9c711ff2012d9ca9
zhoujd <zjd-405@163.com>
2013-06-27 17:43:34 +0800

    for zznix

--- setup.bat
+++ setup.bat
@@ -1,7 +1,7 @@
 @echo off
 echo setup to %OS% start ...
 set MYCFG_PATH="%APPDATA%\.emacs"
-set MYHOME="d:/develop/znix/home/zhoujd"
+set MYHOME="d:/develop/zznix/home/zhoujd"
 
 if exist %MYCFG_PATH% del %MYCFG_PATH%
 

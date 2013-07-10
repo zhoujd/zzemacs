2182dda0fcd005c3797c8387b8514871de8eefe5
zhoujd <zjd-405@163.com>
2013-07-10 21:14:36 +0800

    update env-setting

--- configure/env-setting.el
+++ configure/env-setting.el
@@ -1,6 +1,16 @@
 ;;;; common-setting.el --- common config file
 ;;
 
+;;add path for excute files
+(if-ms-windows
+ (setq my-env-path '(
+                     ""
+                     ))
+ (setq my-env-path '(
+                     ""
+                     ))
+
+(mapcar 'zz-add-os-path my-env-path)
 
 (provide 'env-setting)
 

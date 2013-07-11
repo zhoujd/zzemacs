dea5210fa04b58b1c081cf8109485073434c3aa2
zhoujd <zjd-405@163.com>
2013-07-11 09:54:29 +0800

    save for change on env-setting.el

--- configure/env-setting.el
+++ configure/env-setting.el
@@ -5,8 +5,22 @@
 (when-ms-windows
  (setq my-env-path (list
                     (concat zzemacs-path "bin")
+                    "C:/zznix/bin"
                     "C:/Program Files (x86)/Mozilla Firefox"
                     "C:/Program Files (x86)/Beyond Compare 3"
+                    "C:/Scheme/Gambit-C/v4.6.7/bin"
+                    "C:/Python27/Scripts"
+                    "C:/Lisp/ecl"
+                    "C:/Program Files (x86)/Microsoft Visual Studio 10.0/VC/bin"
+                    "C:/Python27"
+                    "C:/Git/bin"
+                    "C:/emacs-23.4/bin"
+                    "C:/Program Files (x86)/EsafeNet/Cobra DocGuard Client"
+                    "C:/Lisp/clisp"
+                    "C:/Vim/vim73"
+                    "C:/Program Files/Windows Kits/8.0/Debuggers/x64"
+                    "C:/Scheme/Racket"
+                    "C:/Program Files (x86)/Microsoft Visual Studio 10.0/VC"
                     )))
 (unless-ms-windows
  (setq my-env-path (list

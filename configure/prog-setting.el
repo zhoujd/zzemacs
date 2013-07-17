de28ed99e01de3c0b78219b38038b86c9f6f02c6
zhoujd <zjd-405@163.com>
2013-07-18 02:27:58 +0800

    add ssh test

--- configure/prog-setting.el
+++ configure/prog-setting.el
@@ -420,7 +420,8 @@ the mru bookmark stack."
 ;;github ssh-keygen
 ;;$ ssh-keygen -t rsa -C "your_email@example.com"
 ;;gitisos ssh-keygen
-;;$ ssh-keygen -t dsa 
+;;$ ssh-keygen -t dsa
+;;$ ssh -T git@github.com
 (zz-load-path "site-lisp/git-emacs")
 (require 'git-emacs-autoloads)
 (require 'git-show)

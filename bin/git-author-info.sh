0aa178f5ebf0f5e3d9422c06d3b31fd6c41466bf
zhoujd <zjd-405@163.com>
2013-09-04 17:49:34 +0800

    save script

--- bin/git-author-info.sh
+++ bin/git-author-info.sh
@@ -2,25 +2,32 @@
 
 ###https://help.github.com/articles/changing-author-info
 
-git filter-branch --env-filter ' 
-an="$GIT_AUTHOR_NAME"
-am="$GIT_AUTHOR_EMAIL"
-cn="$GIT_COMMITTER_NAME"
-cm="$GIT_COMMITTER_EMAIL"
+new_name="zhoujd"
+new_email="zjd-405@163.com"
+
+match_email="$1"
+
+git filter-branch --env-filter " 
+
+an=\"\$GIT_AUTHOR_NAME\"
+am=\"\$GIT_AUTHOR_EMAIL\"
+cn=\"\$GIT_COMMITTER_NAME\"
+cm=\"\$GIT_COMMITTER_EMAIL\"
  
-if [ "$GIT_COMMITTER_EMAIL" = "your@email.to.match" ]
+if [ \"\$GIT_COMMITTER_EMAIL\" = \"$match_email\" ]
 then
-    cn="zhoujd"
-    cm="zjd-405@163.com"
+    an=\"$new_name\"
+    am=\"$new_email\"
 fi
-if [ "$GIT_AUTHOR_EMAIL" = "your@email.to.match" ]
+if [ \"\$GIT_AUTHOR_EMAIL\" = \"$match_email\" ]
 then
-    an="zhoujd"
-    am="zjd-405@163.com"
+    an=\"$new_name\"
+    am=\"$new_email\"
 fi
  
-export GIT_AUTHOR_NAME="$an"
-export GIT_AUTHOR_EMAIL="$am"
-export GIT_COMMITTER_NAME="$cn"
-export GIT_COMMITTER_EMAIL="$cm"
-'
+export GIT_AUTHOR_NAME=\"\$an\"
+export GIT_AUTHOR_EMAIL=\"\$am\"
+export GIT_COMMITTER_NAME=\"\$cn\"
+export GIT_COMMITTER_EMAIL=\"\$cm\"
+
+"

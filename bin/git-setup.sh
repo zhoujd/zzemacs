d5e79f32a09b46448d777323f2e8ce01c3cb01e5
zhoujd <zjd-405@163.com>
2013-09-18 12:48:00 +0800

    add fixed fatal: index-pack failed

--- bin/git-setup.sh
+++ bin/git-setup.sh
@@ -20,8 +20,12 @@ git config --global user.name    "zhoujd"
 git config --global user.email   "zjd-405@163.com"
 git config --global color.ui     true
 
-## error: RPC failed; result=18, HTTP code = 0
-git config --global http.postBuffer 524288000
+### fatal: index-pack failed for win7
+#git config --global pack.windowMemory  10m
+#git config --global pack.packSizeLimit 20m
+
+### error: RPC failed; result=18, HTTP code = 0
+#git config --global http.postBuffer 524288000
 
 ### git diff is called by git with 7 parameters:
 ### path old-file old-hex old-mode new-file new-hex new-mode

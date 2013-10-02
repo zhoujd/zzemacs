ef98176a264c5a4cc805e7cc7f531641c9838ead
zhoujd <zjd-405@163.com>
2013-10-02 14:48:23 +0800

    up/down/right/left

--- configure/key-setting.el
+++ configure/key-setting.el
@@ -103,44 +103,44 @@
 (apply-keys-to-map
  global-map
  (list
-  (kbd "M-1")     (lookup-key zz/alt-map "1")
-  (kbd "M-2")     (lookup-key zz/alt-map "2")
-  (kbd "M-3")     (lookup-key zz/alt-map "3")
-  (kbd "M-4")     (lookup-key zz/alt-map "4")
+  (kbd "M-1") (lookup-key zz/alt-map "1")
+  (kbd "M-2") (lookup-key zz/alt-map "2")
+  (kbd "M-3") (lookup-key zz/alt-map "3")
+  (kbd "M-4") (lookup-key zz/alt-map "4")
   ;;gud control setting
-  (kbd "M-5")     (lookup-key zz/alt-map "5")
-  (kbd "M-6")     (lookup-key zz/alt-map "6")
-  (kbd "M-7")     (lookup-key zz/alt-map "7")
-  (kbd "M-8")     (lookup-key zz/alt-map "8")
-  (kbd "M-9")     (lookup-key zz/alt-map "9")
-  (kbd "M-0")     (lookup-key zz/alt-map "0")
+  (kbd "M-5") (lookup-key zz/alt-map "5")
+  (kbd "M-6") (lookup-key zz/alt-map "6")
+  (kbd "M-7") (lookup-key zz/alt-map "7")
+  (kbd "M-8") (lookup-key zz/alt-map "8")
+  (kbd "M-9") (lookup-key zz/alt-map "9")
+  (kbd "M-0") (lookup-key zz/alt-map "0")
 
-  (kbd "M-#")     (lookup-key zz/alt-map "#")
+  (kbd "M-#") (lookup-key zz/alt-map "#")
   ;;tabbar switch group
-  (kbd "M-]")     (lookup-key zz/alt-map "]")
-  (kbd "M-[")     (lookup-key zz/alt-map "[")
+  (kbd "M-]") (lookup-key zz/alt-map "]")
+  (kbd "M-[") (lookup-key zz/alt-map "[")
 
   ;;;ctrl number setting
-  (kbd "C-`")     (lookup-key zz/ctrl-map "`")
-  (kbd "C-1")     (lookup-key zz/ctrl-map "1")
-  (kbd "C-2")     (lookup-key zz/ctrl-map "2")
-  (kbd "C-3")     (lookup-key zz/ctrl-map "3")
-  (kbd "C-4")     (lookup-key zz/ctrl-map "4")
+  (kbd "C-`") (lookup-key zz/ctrl-map "`")
+  (kbd "C-1") (lookup-key zz/ctrl-map "1")
+  (kbd "C-2") (lookup-key zz/ctrl-map "2")
+  (kbd "C-3") (lookup-key zz/ctrl-map "3")
+  (kbd "C-4") (lookup-key zz/ctrl-map "4")
   ;;gud control setting
-  (kbd "C-5")     (lookup-key zz/ctrl-map "5")
-  (kbd "C-6")     (lookup-key zz/ctrl-map "6")
-  (kbd "C-7")     (lookup-key zz/ctrl-map "7")
-  (kbd "C-8")     (lookup-key zz/ctrl-map "8")
-  (kbd "C-9")     (lookup-key zz/ctrl-map "9")
-  (kbd "C-0")     (lookup-key zz/ctrl-map "0")
+  (kbd "C-5") (lookup-key zz/ctrl-map "5")
+  (kbd "C-6") (lookup-key zz/ctrl-map "6")
+  (kbd "C-7") (lookup-key zz/ctrl-map "7")
+  (kbd "C-8") (lookup-key zz/ctrl-map "8")
+  (kbd "C-9") (lookup-key zz/ctrl-map "9")
+  (kbd "C-0") (lookup-key zz/ctrl-map "0")
   
   ;;undo/redo
-  (kbd "C--")     (lookup-key zz/ctrl-map "-")
-  (kbd "C-=")     (lookup-key zz/ctrl-map "=")
+  (kbd "C--") (lookup-key zz/ctrl-map "-")
+  (kbd "C-=") (lookup-key zz/ctrl-map "=")
 
   ;;winner restore
-  (kbd "C-,")     (lookup-key zz/ctrl-map ",")
-  (kbd "C-.")     (lookup-key zz/ctrl-map ".")
+  (kbd "C-,") (lookup-key zz/ctrl-map ",")
+  (kbd "C-.") (lookup-key zz/ctrl-map ".")
 
   ;;Control tab quotes a tab => "\C-q\t"
   [(control tab)] (lookup-key zz/ctrl-map [(tab)])
@@ -214,6 +214,23 @@
   (kbd "0") 'gdb-frame-gdb-buffer
   (kbd "-") 'gud-up
   (kbd "=") 'gud-down
+
+  ;;window size change
+  [up]      'enlarge-window
+  [down]    'shrink-window
+  [right]   'enlarge-window-horizontally
+  [left]    'shrink-window-horizontally  
+  ))
+
+;;esc-map
+(apply-keys-to-map
+ esc-map
+ (list
+  ;;quick move other windows
+  [up]      'windmove-up
+  [down]    'windmove-down
+  [right]   'windmove-right
+  [left]    'windmove-left
   ))
 
 (apply-keys-to-map
@@ -236,16 +253,16 @@
   (kbd "C-c h")   'helm-mini
 
   ;;quick move other windows
-  [M-up]          'windmove-up
-  [M-down]        'windmove-down
-  [M-right]       'windmove-right
-  [M-left]        'windmove-left
+  [M-up]          (lookup-key esc-map [up])
+  [M-down]        (lookup-key esc-map [down])
+  [M-right]       (lookup-key esc-map [right])
+  [M-left]        (lookup-key esc-map [left])
   
   ;;window size change
-  [S-up]          'enlarge-window
-  [S-down]        'shrink-window
-  [S-right]       'enlarge-window-horizontally
-  [S-left]        'shrink-window-horizontally
+  [S-up]          (lookup-key help-map [up])
+  [S-down]        (lookup-key help-map [down])
+  [S-right]       (lookup-key help-map [right])
+  [S-left]        (lookup-key help-map [left])
 
   ;;set apps do M+x
   [apps]          'execute-extended-command

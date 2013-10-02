c41a20b3d25c21e3a9ebd1d269451429bf53b3a8
zhoujd <zjd-405@163.com>
2013-10-02 14:00:03 +0800

    format code

--- configure/key-setting.el
+++ configure/key-setting.el
@@ -72,6 +72,7 @@
   "0" 'other-frame
   "-" 'undo
   "=" 'redo
+  ;;undo/redo
   "," 'winner-undo
   "." 'winner-redo
 
@@ -107,30 +108,53 @@
 (apply-keys-to-map
  global-map
  (list
-  (kbd "M-1") (lookup-key zz/alt-map "1")
-  (kbd "M-2") (lookup-key zz/alt-map "2")
-  (kbd "M-3") (lookup-key zz/alt-map "3")
-  (kbd "M-4") (lookup-key zz/alt-map "4")
+  (kbd "M-1")     (lookup-key zz/alt-map "1")
+  (kbd "M-2")     (lookup-key zz/alt-map "2")
+  (kbd "M-3")     (lookup-key zz/alt-map "3")
+  (kbd "M-4")     (lookup-key zz/alt-map "4")
   ;;gud control setting
-  (kbd "M-5") (lookup-key zz/alt-map "5")
-  (kbd "M-6") (lookup-key zz/alt-map "6")
-  (kbd "M-7") (lookup-key zz/alt-map "7")
-  (kbd "M-8") (lookup-key zz/alt-map "8")
-  (kbd "M-9") (lookup-key zz/alt-map "9")
-  (kbd "M-0") (lookup-key zz/alt-map "0")
+  (kbd "M-5")     (lookup-key zz/alt-map "5")
+  (kbd "M-6")     (lookup-key zz/alt-map "6")
+  (kbd "M-7")     (lookup-key zz/alt-map "7")
+  (kbd "M-8")     (lookup-key zz/alt-map "8")
+  (kbd "M-9")     (lookup-key zz/alt-map "9")
+  (kbd "M-0")     (lookup-key zz/alt-map "0")
 
-  (kbd "C-`") (lookup-key zz/ctrl-map "`")
-  (kbd "C-1") (lookup-key zz/ctrl-map "1")
-  (kbd "C-2") (lookup-key zz/ctrl-map "2")
-  (kbd "C-3") (lookup-key zz/ctrl-map "3")
-  (kbd "C-4") (lookup-key zz/ctrl-map "4")
+  (kbd "M-#")     (lookup-key zz/alt-map "#")
+  ;;tabbar switch group
+  (kbd "M-]")     (lookup-key zz/alt-map "]")
+  (kbd "M-[")     (lookup-key zz/alt-map "[")
+
+   ;;quick move other windows
+  [M-up]          (lookup-key zz/alt-map "k")
+  [M-down]        (lookup-key zz/alt-map "j")
+  [M-right]       (lookup-key zz/alt-map "l")
+  [M-left]        (lookup-key zz/alt-map "h")
+
+  ;;;ctrl number setting
+  (kbd "C-`")     (lookup-key zz/ctrl-map "`")
+  (kbd "C-1")     (lookup-key zz/ctrl-map "1")
+  (kbd "C-2")     (lookup-key zz/ctrl-map "2")
+  (kbd "C-3")     (lookup-key zz/ctrl-map "3")
+  (kbd "C-4")     (lookup-key zz/ctrl-map "4")
   ;;gud control setting
-  (kbd "C-5") (lookup-key zz/ctrl-map "5")
-  (kbd "C-6") (lookup-key zz/ctrl-map "6")
-  (kbd "C-7") (lookup-key zz/ctrl-map "7")
-  (kbd "C-8") (lookup-key zz/ctrl-map "8")
-  (kbd "C-9") (lookup-key zz/ctrl-map "9")
-  (kbd "C-0") (lookup-key zz/ctrl-map "0")
+  (kbd "C-5")     (lookup-key zz/ctrl-map "5")
+  (kbd "C-6")     (lookup-key zz/ctrl-map "6")
+  (kbd "C-7")     (lookup-key zz/ctrl-map "7")
+  (kbd "C-8")     (lookup-key zz/ctrl-map "8")
+  (kbd "C-9")     (lookup-key zz/ctrl-map "9")
+  (kbd "C-0")     (lookup-key zz/ctrl-map "0")
+  
+  ;;undo/redo
+  (kbd "C--")     (lookup-key zz/ctrl-map "-")
+  (kbd "C-=")     (lookup-key zz/ctrl-map "=")
+
+  ;;winner restore
+  (kbd "C-,")     (lookup-key zz/ctrl-map ",")
+  (kbd "C-.")     (lookup-key zz/ctrl-map ".")
+
+  ;;Control tab quotes a tab => "\C-q\t"
+  [(control tab)] (lookup-key zz/ctrl-map [(tab)])
   ))
 
 ;;key for f1-backquote-map
@@ -209,7 +233,6 @@
   (kbd "C-x C-b") 'ibuffer
   (kbd "C-x C-j") 'dired-jump
   (kbd "C-c w")   'compare-windows
-  (kbd "M-#")     (lookup-key zz/alt-map "#")
 
   (kbd "%")       'match-paren
 
@@ -221,28 +244,7 @@
   [C-mouse-4]     (unless-ms-windows 'text-scale-increase)
   [C-mouse-5]     (unless-ms-windows 'text-scale-decrease)
 
-  ;;Control tab quotes a tab => "\C-q\t"
-  [(control tab)] (lookup-key zz/ctrl-map [(tab)])
-
-  ;;tabbar switch group
-  (kbd "M-]")     (lookup-key zz/alt-map "]")
-  (kbd "M-[")     (lookup-key zz/alt-map "[")
-
   (kbd "C-c h")   'helm-mini
-
-  ;;undo/redo
-  (kbd "C--")     (lookup-key zz/ctrl-map "-")
-  (kbd "C-=")     (lookup-key zz/ctrl-map "=")
-
-  ;;winner restore
-  (kbd "C-,")     (lookup-key zz/ctrl-map ",")
-  (kbd "C-.")     (lookup-key zz/ctrl-map ".")
-
-  ;;quick move other windows
-  [M-up]          (lookup-key zz/alt-map "k")
-  [M-down]        (lookup-key zz/alt-map "j")
-  [M-right]       (lookup-key zz/alt-map "l")
-  [M-left]        (lookup-key zz/alt-map "h")
 
   ;;window size change
   [S-up]         'enlarge-window

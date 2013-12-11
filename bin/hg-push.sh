552dad721ce81311f71ad1cd09b99e58c37fdcf0
zhoujd <zjd-405@163.com>
2013-12-11 15:19:05 +0800

    add hg-push.sh

---
+++ bin/hg-push.sh
@@ -0,0 +1,11 @@
+####http support push
+##first goto hg project directory
+##$hg serve -p 80
+##$hg clone http://127.0.0.1 hg-proj
+##$hg push
+
+echo "[web]" >>.hg/hgrc
+echo "allow_read = *" >>.hg/hgrc
+echo "allow_push = *" >>.hg/hgrc
+echo "push_ssl   = false" >>.hg/hgrc
+

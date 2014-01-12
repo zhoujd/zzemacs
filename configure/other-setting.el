c06ed803e97b147ca12be15e1aad4d77ac88b643
zhoujd <zjd-405@163.com>
2014-01-12 09:51:02 +0800

    add markdown-mode

--- configure/other-setting.el
+++ configure/other-setting.el
@@ -332,6 +332,13 @@ Emacs buffer are those starting with *."
 (require 'vimrc-mode)
 (add-to-list 'auto-mode-alist '(".vim\\(rc\\)?$" . vimrc-mode))
 
+;;for github wiki
+(autoload 'markdown-mode "markdown-mode"
+   "Major mode for editing Markdown files" t)
+(add-to-list 'auto-mode-alist '("\\.text\\'" . markdown-mode))
+(add-to-list 'auto-mode-alist '("\\.markdown\\'" . markdown-mode))
+(add-to-list 'auto-mode-alist '("\\.md\\'" . markdown-mode))
+
 (provide 'other-setting)
 
 ;;; other-setting.el ends here

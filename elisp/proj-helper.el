7ce4728a34af487ea8fd29d50aa0eb83f0cdfd09
zhoujd <zjd-405@163.com>
2014-02-12 16:42:13 +0800

    add new functions

--- elisp/proj-helper.el
+++ elisp/proj-helper.el
@@ -173,7 +173,19 @@
           "   return -1;\n"
           "}\n"))
 
-
+;;copying lines, not killing
+(defun copy-line (&optional arg)
+  "Do a kill-line but copy rather than kill.  This function directly calls
+kill-line, so see documentation of kill-line for how to use it including prefix
+argument and relevant variables.  This function works by temporarily making the
+buffer read-only, so I suggest setting kill-read-only-ok to t."
+  (interactive "P")
+  (toggle-read-only 1)
+  (kill-line arg)
+  (toggle-read-only 0))
+ 
+(setq-default kill-read-only-ok t)
+(global-set-key "\C-c\C-k" 'copy-line)
 
 (provide 'proj-helper)
 

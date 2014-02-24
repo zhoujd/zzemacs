e8bf5bc8fdb8577b9c8166e3cd1c3f08f4f3b006
zhoujd <zjd-405@163.com>
2014-02-24 12:28:35 +0800

    saving

---
+++ third-party/python/zzpytools/setup.py
@@ -0,0 +1,15 @@
+#### This is py tools setup script for zhoujd
+
+from setuptools import setup, find_packages
+
+setup(
+    name="zzpytools",
+    version="0.1",
+    description="zz python tools",
+    author="zjd-405@163.com",
+    url="https://github.com/zhoujd",
+    license="LGPL",
+    packages= ['pycomplete'],
+    scripts=[],
+    )
+

386cfb50347b0178ef7c2beef7e69ff529bbe192
zhoujd <zjd-405@163.com>
2014-02-24 22:36:13 +0800

    update

--- third-party/python/zzpytools/setup.py
+++ third-party/python/zzpytools/setup.py
@@ -1,15 +1,20 @@
 #### This is py tools setup script for zhoujd
 
+import os
+import sys
+import glob
 from distutils.core import setup
 
 setup(
-    name = "zzpytools",
-    version = "0.1",
-    description = "zz python tools",
-    author = "zjd-405@163.com",
-    url = "https://github.com/zhoujd",
-    license = "LGPL",
-    packages = ['pycomplete'],
-    scripts = [],
+    name             = "zzpytools",
+    version          = "0.1",
+    description      = "python tools for zachary zhou",
+    long_description = "self using script on python",
+    author           = "zachary zhou",
+    author_email     = "zjd-405@163.com",
+    url              = "https://github.com/zhoujd",
+    license          = "LGPL",
+    packages         = ['pycomplete'],
+    scripts          = [],
     )
 

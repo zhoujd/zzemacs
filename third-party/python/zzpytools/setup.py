22f12d6dbbddd9e23d8bc52474e5bee1abd575e7
zhoujd <zjd-405@163.com>
2014-02-24 14:33:15 +0800

    saving

--- third-party/python/zzpytools/setup.py
+++ third-party/python/zzpytools/setup.py
@@ -3,13 +3,13 @@
 from setuptools import setup, find_packages
 
 setup(
-    name="zzpytools",
-    version="0.1",
-    description="zz python tools",
-    author="zjd-405@163.com",
-    url="https://github.com/zhoujd",
-    license="LGPL",
-    packages= ['pycomplete'],
-    scripts=[],
+    name = "zzpytools",
+    version = "0.1",
+    description = "zz python tools",
+    author = "zjd-405@163.com",
+    url = "https://github.com/zhoujd",
+    license = "LGPL",
+    packages = find_packages(),
+    scripts = [],
     )
 

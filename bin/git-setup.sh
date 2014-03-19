0eaeb0a094de25f4fbdc63afcee8ad1699f23b29
zhoujd <zjd-405@163.com>
2014-03-19 17:20:05 +0800

    update for changing

--- bin/git-setup.sh
+++ bin/git-setup.sh
@@ -4,19 +4,6 @@ GIT_SETUP_HOME=`pwd`
 
 echo git diff setup start ...
 
-## setup packages
-if [ "$OS" != "Windows_NT" ] ; then
-    echo -n "Do you need install packages? (y/N): "
-    read answer
-    case "$answer" in
-        "Y" | "y" )
-        sudo apt-get install -y python-nautilus python-configobj python-gtk2 python-glade2 python-svn python-dbus meld
-        sudo apt-get install -y python-meld3
-        sudo apt-get install -y git-core
-        sudo apt-get install -y gitk
-    esac
-fi
-
 ## clear ~/.gitconfig
 echo "remove ~/.gitconfig and setting git configure ..."
 rm -f ~/.gitconfig

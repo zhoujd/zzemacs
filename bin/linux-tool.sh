66411a73e0f051d9cb7005ce5001ddd4a18e5a53
zhoujd <zjd-405@163.com>
2014-03-20 23:01:19 +0800

    more for os select

--- bin/linux-tool.sh
+++ bin/linux-tool.sh
@@ -1,20 +1,44 @@
 #!/bin/sh
 
-echo install linux-tools begin...
-#sudo apt-get install -y cairo-dock cairo-dock-plug-ins
+echo "install linux-tools begin..."
 
-##develop package
-#sudo apt-get install -y gnome-core-devel
-#sudo apt-get install -y acetoneiso
+##package for ubuntu
+function Install_package_suse
+{
+    ##TODO
+}
 
-sudo apt-get install -y libx11-dev
-sudo apt-get install -y xorg-dev
-sudo apt-get install -y texlive
-sudo apt-get install -y texinfo
+##package for ubuntu
+function Install_package_ubuntu
+{
+    #sudo apt-get install -y cairo-dock cairo-dock-plug-ins
 
-sudo apt-get install -y openssh-server
+    ##develop package
+    #sudo apt-get install -y gnome-core-devel
+    #sudo apt-get install -y acetoneiso
 
-##easy split windows for multi-term
-sudo apt-get install -y terminator
+    sudo apt-get install -y libx11-dev
+    sudo apt-get install -y xorg-dev
+    sudo apt-get install -y texlive
+    sudo apt-get install -y texinfo
 
-echo install linux-tools end...
+    sudo apt-get install -y openssh-server
+
+    ##easy split windows for multi-term
+    sudo apt-get install -y terminator
+}
+
+# dectect OS version
+LINUX_DISTRO=`lsb_release -si`
+if [ "$LINUX_DISTRO" == "SUSE LINUX" ]; then
+    LINUX_DISTRO="SuSE"
+    Install_package_suse
+elif [ "$LINUX_DISTRO" == "Ubuntu" ]; then
+    LINUX_DISTRO="Ubuntu"
+    Install_package_ubuntu
+else
+    echo "You are about to install on a non supported linux distribution."
+fi
+
+echo "Install on $LINUX_DISTRO ..."
+echo "install linux-tools end..."

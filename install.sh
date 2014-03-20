75808e635ccba231e323f0695005bda0fe976286
zhoujd <zjd-405@163.com>
2014-03-20 18:07:32 +0800

    saving

--- install.sh
+++ install.sh
@@ -4,33 +4,41 @@ ZZEMACS_ROOT=`pwd`
 
 echo install .emacs to HOME directory begin...
 
+
+function Install_package() {
+    # dectect OS version
+    LINUX_DISTRO=`lsb_release -si`
+    if [ "$LINUX_DISTRO" == "SUSE LINUX" ]; then
+        LINUX_DISTRO="SuSE"
+    else if [ "$LINUX_DISTRO" == "Ubuntu" ]; then
+        LINUX_DISTRO="Ubuntu"
+        
+        sudo apt-get install -y emacs
+        sudo apt-get install -y cscope
+        sudo apt-get install -y texinfo
+        
+        ##install font
+        FONT_HOME=/usr/share/fonts/truetype/
+        sudo cp -f ${ZZEMACS_ROOT}/font/consola.ttf  ${FONT_HOME}
+        sudo cp -f ${ZZEMACS_ROOT}/font/MSYHMONO.ttf ${FONT_HOME}
+    else
+        echo "You are about to install on a non supported linux distribution."
+    fi
+        
+    echo "Install on $LINUX_DISTRO ..."
+}
+
+
 ##install package for emacs
 echo -n "Do you need install packages? (y/N): "
 read answer
 case "$answer" in
     "Y" | "y" )
-        # dectect OS version
-        LINUX_DISTRO=`lsb_release -si`
-        if [ "$LINUX_DISTRO" == "SUSE LINUX" ]; then
-            LINUX_DISTRO="SuSE"
-        else if [ "$LINUX_DISTRO" == "Ubuntu" ]; then
-            LINUX_DISTRO="Ubuntu"
-            
-            sudo apt-get install -y emacs
-            sudo apt-get install -y cscope
-            sudo apt-get install -y texinfo
-            
-            ##install font
-            FONT_HOME=/usr/share/fonts/truetype/
-            sudo cp -f ${ZZEMACS_ROOT}/font/consola.ttf  ${FONT_HOME}
-            sudo cp -f ${ZZEMACS_ROOT}/font/MSYHMONO.ttf ${FONT_HOME}
-        else
-            echo "You are about to install on a non supported linux distribution.\n"
-        fi
-        
-        echo "Install on $LINUX_DISTRO ..."
+        Install_package
         ;;
 esac
+
+exit 0
 
 ##setup .emacs
 rm -f ~/.emacs

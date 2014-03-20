bbfe2d821ffa68a4afdc9944982c13a9b014660c
zhoujd <zjd-405@163.com>
2014-03-20 16:10:25 +0800

    format code

--- configure/lisp-setting.el
+++ configure/lisp-setting.el
@@ -81,13 +81,11 @@
 (require 'ac-slime)
 (add-hook 'slime-mode-hook 'set-up-slime-ac)
 (add-hook 'slime-repl-mode-hook 'set-up-slime-ac)
-(add-hook 'slime-editing-mode-hook 'set-up-slime-ac)
 (eval-after-load "auto-complete"
   '(progn
     (add-to-list 'ac-modes 'slime-repl-mode)
-    (add-to-list 'ac-modes 'slime-editing-mode)
+    (add-to-list 'ac-modes 'slime-mode)
   ))
-
 
 (provide 'lisp-setting)
 

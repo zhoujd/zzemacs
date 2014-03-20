a8bd5dfbd0ebea3e41237b4a040e3e0c8014a4bc
zhoujd <zjd-405@163.com>
2014-03-20 23:16:01 +0800

    add for zhoujd

--- bin/hg-setup.sh
+++ bin/hg-setup.sh
@@ -1,18 +1,32 @@
-#!/bin/sh
+#!/bin/bash
 
-echo hg setup start ...
+echo "hg setup start ..."
 
 ###Mercurial Books
 ##http://mercurial.selenic.com/
 ##http://hginit.com/ (Hg Init: a Mercurial tutorial)
 ##http://hgbook.red-bean.com/ (Mercurial: The Definitive Guide)
 
+function Install_package
+{
+    # dectect OS version
+    LINUX_DISTRO=`lsb_release -si`
+    if [ "$LINUX_DISTRO" == "SUSE LINUX" ]; then
+        echo "Install on suse"
+    elif [ "$LINUX_DISTRO" == "Ubuntu" ]; then
+        sudo apt-get install -y mercurial
+    else
+        echo "You are about to install on a non supported linux distribution."
+    fi
+}
+
 ##setup packages
 echo -n "Do you need install packages? (y/N): "
 read answer
 case "$answer" in
     "Y" | "y" )
-        sudo apt-get install -y mercurial
+        Install_package
+        ;;
 esac
 
 echo "remove ~/.hgrc and setting hg configure ..."
@@ -33,4 +47,4 @@ push_ssl = false
 allow_push = *
 EOF
 
-echo hg setup end ...
+echo "hg setup end ..."

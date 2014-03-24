74712561c931fc5baaef38fca604f22b2f91f5bf
zhoujd <zjd-405@163.com>
2014-03-24 20:01:55 +0800

    saving

--- bin/sample.sh
+++ bin/sample.sh
@@ -5,7 +5,8 @@ export ECHO_PREFIX_INFO="\033[1;32;40mINFO...\033[0;0m"
 export ECHO_PREFIX_ERROR="\033[1;31;40mError...\033[0;0m"
 
 # Try command  for test command result.
-function try_command {
+try_command()
+{
     "$@"
     status=$?
     if [ $status -ne 0 ]; then

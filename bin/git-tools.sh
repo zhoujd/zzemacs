d8f4c5711588520af620fb01a5ccf1f0c81b38df
zhoujd <zjd-405@163.com>
2014-03-24 19:38:23 +0800

    saving

--- bin/git-tools.sh
+++ bin/git-tools.sh
@@ -9,19 +9,18 @@ Install_package()
     LINUX_DISTRO=`lsb_release -si`
     if [ "$LINUX_DISTRO" == "SUSE LINUX" ]; then
         LINUX_DISTRO="SuSE"
-        try_command sudo zypper install git gitk
+        sudo zypper install git gitk
     elif [ "$LINUX_DISTRO" == "Ubuntu" ]; then
         LINUX_DISTRO="Ubuntu"
-        try_command sudo apt-get install -y python-nautilus python-configobj python-gtk2 python-glade2 python-svn python-dbus meld
-        try_command sudo apt-get install -y python-meld3
-        try_command sudo apt-get install -y git-core
-        try_command sudo apt-get install -y gitk
+        sudo apt-get install -y python-nautilus python-configobj python-gtk2 python-glade2 python-svn python-dbus meld
+        sudo apt-get install -y python-meld3
+        sudo apt-get install -y git-core
+        sudo apt-get install -y gitk
     else
         echo "You are about to install on a non supported linux distribution."
     fi
         
-	echo "Install on $LINUX_DISTRO ..."
-
+	echo -e $ECHO_PREFIX_INFO "Install on $LINUX_DISTRO ..."
 }
 
 ## setup packages
@@ -29,7 +28,7 @@ echo -n "Do you need install packages? (y/N): "
 read answer
 case "$answer" in
     "Y" | "y" )
-        Install_package
+        try_command Install_package
         ;;
 esac
 

3b9e2f6d4d5bc789b8229bf32029a3052f932347
zhoujd <zjd-405@163.com>
2014-03-30 02:31:23 +0800

    fixed for pydb install on ubuntu-u1204

--- third-party/python/install.sh
+++ third-party/python/install.sh
@@ -9,31 +9,40 @@ CURRENT_ROOT=`pwd`
 
 echo "for python develop start ..."
 
+echo "==>1 install pymacs"
 cd $CURRENT_ROOT/pymacs
 sudo python pppp -C ppppconfig.py pppp.rst.in pymacs.el.in pymacs.rst.in Pymacs.py.in contrib tests
 sudo python setup.py install
 cd $CURRENT_ROOT
 
+echo "==>2 install rope"
 cd $CURRENT_ROOT/rope
 sudo python setup.py install
 cd $CURRENT_ROOT
 
+echo "==>3 install ropemode"
 cd $CURRENT_ROOT/ropemode
 sudo python setup.py install
 cd $CURRENT_ROOT
 
+echo "==>4 install ropemacs"
 cd $CURRENT_ROOT/ropemacs
 sudo python setup.py install
 cd $CURRENT_ROOT
 
+echo "==>5 install pyreadline"
 cd $CURRENT_ROOT/pyreadline
 sudo python setup.py install
 cd $CURRENT_ROOT
 
+echo "==>6 install pydb"
 cd $CURRENT_ROOT/pydb
-sudo python setup.py install
+./configure
+make
+sudo make install
 cd $CURRENT_ROOT
 
+echo "==>7 install zzpytools"
 cd $CURRENT_ROOT/zzpytools
 sudo python setup.py install
 cd $CURRENT_ROOT

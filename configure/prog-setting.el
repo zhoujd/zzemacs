f099c99fcea6ea07410a68905f52080a8eaf187a
zhoujd <zjd-405@163.com>
2014-04-09 08:49:34 +0800

    format for ssh

--- configure/prog-setting.el
+++ configure/prog-setting.el
@@ -479,11 +479,16 @@ the mru bookmark stack."
 ;(require 'vc-svn)
 
 ;;Git Support
-;;github ssh-keygen
+;;=>github ssh-keygen
 ;;$ssh-keygen -t rsa -C "your_email@example.com"
-;;gitisos ssh-keygen
+;;;=>gitisos ssh-keygen
 ;;$ssh-keygen -t dsa
 ;;$ssh -T git@github.com
+;;;=>SSH authorized each other
+;;$ssh-keygen -t rsa
+;;$touch ~/.ssh/authorized_keys
+;;$chmod 644 ~/.ssh/authorized_keys
+;;cat id_rsa_zachary.pub >> ~/.ssh/authorized_keys
 (zz-load-path "site-lisp/git-emacs")
 (require 'git-emacs)
 (require 'git-show)

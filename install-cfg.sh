50ce3d5199efae48bb782a724711fc5adcad35d8
zhoujd <zjd-405@163.com>
2014-06-14 21:01:56 +0800

    for pure install

---
+++ install-cfg.sh
@@ -0,0 +1,32 @@
+#!/bin/sh
+
+ZZEMACS_ROOT=`pwd`
+
+Install_dot_emacs()
+{
+cat > ~/.emacs <<EOF
+;;;this is .emacs for zhoujd.
+(defvar zzemacs-path "${ZZEMACS_ROOT}")
+(if (file-exists-p (concat zzemacs-path "/.emacs"))
+    (load-file (concat zzemacs-path "/.emacs"))
+    (message "zzemacs has not install"))
+EOF
+}
+
+##setup font setting
+Install_fonts_conf()
+{
+cat > ~/.fonts.conf <<EOF
+<?xml version="1.0"?>
+<!DOCTYPE fontconfig SYSTEM "fonts.dtd">
+<!-- /etc/fonts/fonts.conf file to configure system font access -->
+<fontconfig>
+  <!-- Font directory list -->
+  <dir>${ZZEMACS_ROOT}/font</dir>
+</fontconfig>
+EOF
+}
+
+Install_dot_emacs
+Install_fonts_conf
+

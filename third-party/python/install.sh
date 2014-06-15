ca0843f361bf71c57cc2edaccd0611dbe2997742
zhoujd <zjd-405@163.com>
2014-06-15 12:10:04 +0800

    restore for sudo

--- third-party/python/install.sh
+++ third-party/python/install.sh
@@ -12,39 +12,39 @@ echo "for python develop start ..."
 echo "==>1 install pymacs"
 cd $CURRENT_ROOT/pymacs
 python pppp -C ppppconfig.py pppp.rst.in pymacs.el.in pymacs.rst.in Pymacs.py.in contrib tests
-python setup.py install
+sudo python setup.py install
 cd $CURRENT_ROOT
 
 echo "==>2 install rope"
 cd $CURRENT_ROOT/rope
-python setup.py install
+sudo python setup.py install
 cd $CURRENT_ROOT
 
 echo "==>3 install ropemode"
 cd $CURRENT_ROOT/ropemode
-python setup.py install
+sudo python setup.py install
 cd $CURRENT_ROOT
 
 echo "==>4 install ropemacs"
 cd $CURRENT_ROOT/ropemacs
-python setup.py install
+sudo python setup.py install
 cd $CURRENT_ROOT
 
 echo "==>5 install pyreadline"
 cd $CURRENT_ROOT/pyreadline
-python setup.py install
+sudo python setup.py install
 cd $CURRENT_ROOT
 
 echo "==>6 install pydb"
 cd $CURRENT_ROOT/pydb
 ./configure
 make
-make install
+sudo make install
 cd $CURRENT_ROOT
 
 echo "==>7 install pycomplete"
 cd $CURRENT_ROOT/pycomplete
-python setup.py install
+sudo python setup.py install
 cd $CURRENT_ROOT
 
 echo "for python develop end ..."

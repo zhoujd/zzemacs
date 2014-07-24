b9bf04808121322a704eb6eb3159f861b5a0a33a
zhoujd <zjd-405@163.com>
2014-07-24 17:53:49 +0800

    saving

--- configure/key-setting.el
+++ configure/key-setting.el
@@ -206,6 +206,9 @@
   "3" (when-ms-windows (execute-set-key
                         "git-shell"
                         '("cmd" "/c" "start" "sh" "--login" "-i")))
+  "4" (when-ms-windows (execute-set-key
+                        "console"
+                        (list "console2" "-t" "Git" "-d" default-directory)))
   "t" 'open-with-terminal
   "f" (execute-set-key "firefox"  '("firefox" "http://www.baidu.com"))
   "b" (execute-set-key "bcompare" '("bcompare"))

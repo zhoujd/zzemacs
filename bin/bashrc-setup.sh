0c129790c478eade408c15705133f6c80ecdad29
zhoujd <zjd-405@163.com>
2014-07-29 17:40:32 +0800

    saving

--- bin/bashrc-setup.sh
+++ bin/bashrc-setup.sh
@@ -10,11 +10,17 @@ echo "Setup self .bashrc start ..."
 
 BASHRC_PATH=~/.bashrc
 SELF_BASHRC_PATH=$(cd $SETUP_ROOT/../misc && pwd)
+SELF_BIN_PATH=$(cd $SETUP_ROOT/../bin && pwd)
 
 ##setup .bashrc
 Install_self_bashrc()
 {
 try_command cat >> $BASHRC_PATH <<EOF
+# add zzemacs/bin to PATH for zachary zhou
+if [ -d $SELF_BIN_PATH ] ; then
+    export PATH=$SELF_BIN_PATH:\$PATH
+fi
+
 # source self .bashrc setting for zachary zhou
 if [ -f $SELF_BASHRC_PATH/.bashrc ] ; then
     . ${SELF_BASHRC_PATH}/.bashrc

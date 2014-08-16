2608ce50495c201eb9b42b7c2403fe50edfcb4e2
zhoujd <zjd-405@163.com>
2014-08-16 13:47:51 +0800

    merge from hg

--- bin/git-conf-local.sh
+++ bin/git-conf-local.sh
@@ -1,24 +1,25 @@
 #!/bin/sh
 
-echo -n "please input user.name (Enter for skip): "
+echo -n "Please input user.name (Enter for skip): "
 read name
 if [ ! "$name" = "" ] ; then
     git config user.name $name
 fi
 
-echo -n "please input user.email (Enter for skip): "
+echo -n "Please input user.email (Enter for skip): "
 read email
 if [ ! "$email" = "" ] ; then
     git config user.email $email
 fi
 
-echo -n "please input http.proxy (Enter for skip): "
+echo -n "Please input http.proxy (Enter for skip): "
 read http_proxy
 if [ ! "$http_proxy" = "" ] ; then
     git config http.proxy $http_proxy
 fi
 
-echo -n "please input core.autocrlf (true/auto/input/Enter for skip): "
+echo "Hint core.autocrlf (auto: for window, input: for linux)"
+echo -n "Please input core.autocrlf (Enter for skip): "
 read autocrlf
 if [ ! "$autocrlf" = "" ] ; then
     git config core.autocrlf $autocrlf

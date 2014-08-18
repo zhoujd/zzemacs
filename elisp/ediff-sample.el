b9d2ef0ea9995b667c14603f90e1f8aaafc67f9f
zhoujd <zjd-405@163.com>
2014-08-18 14:35:31 +0800

    saving changed

--- elisp/ediff-sample.el
+++ elisp/ediff-sample.el
@@ -1,6 +1,11 @@
 ;;; ediff sample file for git/hg diff-merge
 ;;;
 
+
+;; Set ediff style
+(setq ediff-window-setup-function 'ediff-setup-windows-plain)
+(setq ediff-split-window-function 'split-window-horizontally)
+
 (defun ediff-sample-files (file-a file-b)
   (setq ediff-quit-hook 'kill-emacs)
   (ediff-files file-a file-b))

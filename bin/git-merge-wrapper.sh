04764ef5bdaa72e06b69f5631545cc79b8568e15
zhoujd <zjd-405@163.com>
2014-08-27 16:20:57 +0800

    saving

--- bin/git-merge-wrapper.sh
+++ bin/git-merge-wrapper.sh
@@ -11,24 +11,46 @@
 ## http://www.perforce.com/perforce/products/merge.html
 ## http://meldmerge.org/
 
-if [ "$OS" = "Windows_NT" ] ; then
-    MERGE_TOOL_0="sh emacs-merge.sh $*"
-    MERGE_TOOL_1="$ZZNIX_HOME/home/zhoujd/zztools/bcompare/bcompare $*"
-    MERGE_TOOL_2="$ZZNIX_HOME/home/zhoujd/zztools/perforce/p4merge $*"
+## use emacs as merge tool
+EMACS_FLAG="n"
 
-    MERGE_SELECT=$MERGE_TOOL_1
-else
-    MERGE_TOOL_0="sh emacs-merge.sh $*"
-    MERGE_TOOL_1="$HOME/zztools/bcompare/bin/bcompare $*"
-    MERGE_TOOL_2="$HOME/zztools/meld/bin/meld $2 $4 $3"
-    MERGE_TOOL_3="$HOME/zztools/p4v/bin/p4merge $*"
+merge_extern()
+{
+    if [ "$OS" = "Windows_NT" ] ; then
+        MERGE_TOOL_0="$ZZNIX_HOME/home/zhoujd/zztools/bcompare/bcompare $*"
+        MERGE_TOOL_1="$ZZNIX_HOME/home/zhoujd/zztools/perforce/p4merge $*"
 
-    if [ "Linux" = "`uname -s`" ] ; then
-        MERGE_SELECT=$MERGE_TOOL_3
-    else
         MERGE_SELECT=$MERGE_TOOL_0
-    fi    
-fi
+    else
+        MERGE_TOOL_0="$HOME/zztools/bcompare/bin/bcompare $*"
+        MERGE_TOOL_1="$HOME/zztools/meld/bin/meld $2 $4 $3"
+        MERGE_TOOL_2="$HOME/zztools/p4v/bin/p4merge $*"
+
+        MERGE_SELECT=$MERGE_TOOL_2
+    fi
+
+    $MERGE_SELECT
+}
+
+merge_emacs()
+{
+    emacs --no-site-file -q \
+		  --eval "(load-file \"~/zzemacs/elisp/ediff-sample.el\")" \
+          --eval "(ediff-merge-files \"$2\" \"$3\" \"$1\" \"$4\")" \
+          --eval "(message \"emacs merge finished.\")"
+}
+
+main()
+{
+    case "$EMACS_FLAG" in
+        "Y" | "y" )
+            merge_emacs $* 
+            ;;
+        * )
+            merge_extern $*
+            ;;
+    esac
+}
 
 ## run merge tools
-$MERGE_SELECT
+main $1 $2 $3 $4

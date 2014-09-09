e028a184c29b3c3342258d4db3e841b3be570b6e
zhoujd <zjd-405@163.com>
2014-09-09 09:48:44 +0800

    saving changed

--- bin/hg-conf-local.py
+++ bin/hg-conf-local.py
@@ -13,23 +13,23 @@ if __name__ == "__main__":
     config.read(hg_rc_path)
 
     # ui.username
-    name = raw_input("please input name (Enter for skip):")
-    email = raw_input("please input email (Enter for skip):")
-    username = name + "<" + email + ">"
-    if username != "" :
+    name = raw_input("please input name (Enter for skip):").rstrip('\n')
+    email = raw_input("please input email (Enter for skip):").rstrip('\n')
+    if name != "" and email != "":
+        username = name + "<" + email + ">"
         if not config.has_section('ui') :
             config.add_section('ui')
         config.set('ui', 'username', username)
 
     # http_proxy.host
-    proxy_host = raw_input("please input http_proxy.host (Enter for skip):")
+    proxy_host = raw_input("please input http_proxy.host (Enter for skip):").rstrip('\n')
     if proxy_host != "" :
         if not config.has_section('http_proxy') :
             config.add_section('http_proxy')
         config.set('http_proxy', 'host', proxy_host)
 
     # paths.default
-    path_url = raw_input("please input paths.default (Enter for skip):")
+    path_url = raw_input("please input paths.default (Enter for skip):").rstrip('\n')
     if path_url != "" :
         if not config.has_section('paths') :
             config.add_section('paths')

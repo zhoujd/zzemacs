15de21a5cdcebcae4bde10be7c5d82121591d5ab
zhoujd <zjd-405@163.com>
2014-09-12 12:30:42 +0800

    add for saving

--- configure/other-setting.el
+++ configure/other-setting.el
@@ -74,6 +74,7 @@
 (require 'redo+)
 (global-set-key [M-S-backspace] 'redo)
 (global-set-key (kbd "C-?") 'redo)
+(add-to-list 'warning-suppress-types '(undo discard-info))
 
 ;; space tab show
 ;(require 'jaspace)

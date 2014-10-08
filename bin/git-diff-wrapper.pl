db37b71a2dea38a6010b4cfee8dcba580ab3e112
zhoujd <zjd-405@163.com>
2014-10-08 13:26:43 +0800

    saving changed

--- bin/git-diff-wrapper.pl
+++ bin/git-diff-wrapper.pl
@@ -5,4 +5,40 @@ use lib "$FindBin::Bin";
 
 use sample;
 
-print "hello, zhoujd\n";
+my $emacs_flag = "y";
+
+sub diff_extern {
+
+}
+
+sub diff_emacs {
+    my $diff_a = shift;
+    my $diff_b = shift;
+    my $zzemacs_path = "";
+
+    if (is_windows() == 1) {
+        $zzemacs_path = "$ENV{'ZZNIX_HOME'}/home/zhoujd/zzemacs";
+    } else {
+        $zzemacs_path = "$ENV{'HOME'}/zzemacs";
+    }
+
+    my $cmd = "emacs --no-site-file -q \
+                     --eval \"(load-file \"$zzemacs_path/elisp/ediff-sample.el\")\" \
+                     --eval \"(ediff-sample-diff \"$diff_a\" \"$diff_b\")\" \
+                     --eval \"(message \"emacs diff finished.\")\"";
+
+
+}
+
+sub main {
+    my $diff_a = shift;
+    my $diff_b = shift;
+
+    if ($emacs_flag eq "y" || $emacs_flag eq "Y") {
+        diff_emacs($diff_a, $diff_b);
+    } else {
+        diff_extern($diff_a, $diff_b);
+    }
+}
+
+main($ARGV[0], $ARGV[1]);

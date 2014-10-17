6b644c4e791bac4d9e8a09ae7178d42068b78903
zhoujd <zjd-405@163.com>
2014-10-17 23:34:12 +0800

    add evil plugin

--- configure/evil-setting.el
+++ configure/evil-setting.el
@@ -4,7 +4,21 @@
 ;;http://emacswiki.org/emacs/Evil
 (zz-load-path "site-lisp/evil")
 (zz-load-path "site-lisp/evil/lib")
+(zz-load-path "site-lisp/evil-plugin")
+
+;;evil-evil
 (require 'evil)
+
+;;evil-surround
+(require 'evil-surround)
+(global-evil-surround-mode 1)
+
+;;evil-leader
+(require 'evil-leader)
+(global-evil-leader-mode 1)
+(evil-leader/set-key "e" 'find-file
+                     "b" 'switch-to-buffer
+                     "k" 'kill-buffer)
 
 ;;key setting using default
 ;(evil-set-toggle-key "<f5>")

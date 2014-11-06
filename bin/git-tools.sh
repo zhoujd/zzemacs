eac80277b3d73a6c7b0141a7a3d6df988e6a070e
zhoujd <zjd-405@163.com>
2014-11-06 17:20:10 +0800

    update for using

--- bin/git-tools.sh
+++ bin/git-tools.sh
@@ -1,7 +1,11 @@
 #!/bin/sh
 
+## Git script path
+GIT_TOOLS_ROOT="$(dirname $0)"
+ZZEMACS_ROOT="$(cd $GIT_TOOLS_ROOT/.. && pwd)"
+
 ##Import vars and functions
-. sample.sh
+. $GIT_TOOLS_ROOT/sample.sh
 
 ## Install package for git
 Install_package()
@@ -22,6 +26,19 @@ Install_package()
     fi        
 }
 
+
+## Install git self tool
+Install_tools()
+{
+    INS_PATH=/usr/libexec/git-core/
+    
+    if [ -d $INS_PATH ] ; then
+        sudo ln -sf $ZZEMACS_ROOT/libexec/git-ediff   $INS_PATH
+        sudo ln -sf $ZZEMACS_ROOT/libexec/git-flow/*  $INS_PATH
+    fi
+}
+
+
 ## setup packages
 echo -n "Do you need install packages? (y/N): "
 read answer
@@ -31,5 +48,13 @@ case "$answer" in
         ;;
 esac
 
-echo "git diff tools setup end ..."
 
+echo -n "Do you need install git self tool? (y/N): "
+read answer
+case "$answer" in
+    "Y" | "y" )
+        try_command Install_tools
+        ;;
+esac
+
+echo "git diff tools setup end ..."

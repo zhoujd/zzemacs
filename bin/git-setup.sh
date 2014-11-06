f382b881012a75ab8257cc487d44f5254e905eaf
zhoujd <zjd-405@163.com>
2014-11-07 01:41:45 +0800

    clean code

--- bin/git-setup.sh
+++ bin/git-setup.sh
@@ -51,7 +51,6 @@ git config --global alias.lola  "log --graph --decorate --pretty=oneline --abbre
 git config --global alias.ls    "ls-files"
 git config --global alias.ign   "ls-files -o -i --exclude-standard"
 git config --global alias.glog  "log --graph --pretty=format:'%Cred%h%Creset %C(cyan)%an%Creset -%C(yellow)%d%Creset %s %Cgreen(%cr)%Creset' --abbrev-commit --date=relative"
-git config --global alias.dt    "diff-tree --no-commit-id --name-only -r"
 git config --global alias.so    "show --pretty=format: --name-only"
 
 ## set http proxy

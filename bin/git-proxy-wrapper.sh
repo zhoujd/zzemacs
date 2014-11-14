26f0d506b8a5e62d1a6fd3793ad0e70c04c788de
zhoujd <zjd-405@163.com>
2014-11-14 21:42:18 +0800

    format code

--- bin/git-proxy-wrapper.sh
+++ bin/git-proxy-wrapper.sh
@@ -14,7 +14,7 @@ _proxyport=1080
 
 _connect_flag="y"
 
-echo $1 | grep "\.intel\.com$" > /dev/null 2>&1
+echo $1 | grep -E "\.intel\.com$|\.kernel\.org$" > /dev/null 2>&1
 if [ $? -eq 0 ] ; then
     exec connect $@
 else

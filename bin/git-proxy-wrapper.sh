e30406a10f1877ec04fa50aed590d118425de774
zhoujd <zjd-405@163.com>
2014-11-14 19:03:26 +0800

    add for git proxy

---
+++ bin/git-proxy-wrapper.sh
@@ -0,0 +1,12 @@
+#!/bin/sh
+#
+# Use socat to proxy git through a SOCKS proxy
+# to access git repositories outside Intel.
+#
+
+# Configuration.
+#_proxy=proxy-ir.intel.com
+#_proxy=proxy-shz.intel.com
+_proxy=10.7.211.16
+_proxyport=1080
+exec socat STDIO SOCKS4:$_proxy:$1:$2,socksport=$_proxyport

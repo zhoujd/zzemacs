8fb38afa1a6b72984e8bae39b77ced87461bef43
zhoujd <zjd-405@163.com>
2014-11-19 09:28:39 +0800

    more for hg-src-install

--- bin/hg-src-install.sh
+++ bin/hg-src-install.sh
@@ -1,2 +1,22 @@
-#!/bin/sh
+#!/bin/bash
 
+##Install from source on linux
+sudo yum install -y  python-docutils python-devel
+
+##Init install temp directory
+INS_TEMP_DIR=$HOME/Downloads/hg-src-install
+rm -rf $INS_TEMP_DIR
+mkdir -p $INS_TEMP_DIR
+
+pushd $INS_TEMP_DIR
+
+wget http://selenic.com/hg/archive/tip.tar.gz
+tar xf tip.tar.gz
+mv Mercurial-* Mercurail-latest
+cd Mercurail-latest
+make all
+sudo make install #default to /usr/local
+
+popd
+
+echo "Install hg from source finished"

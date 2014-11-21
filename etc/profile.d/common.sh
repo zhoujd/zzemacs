2fc2197cd10e1a53460e9574069d0aefd2e1af80
zhoujd <zjd-405@163.com>
2014-11-22 00:26:24 +0800

    more for putty

--- etc/profile.d/common.sh
+++ etc/profile.d/common.sh
@@ -24,6 +24,7 @@ alias ec='emacsclient -c'
 alias ET="SUDO_EDITOR=\"emacsclient -t\" sudo -e"
 alias EC="SUDO_EDITOR=\"emacsclient -c\" sudo -e"
 alias lcsh="csh -l"
+alias putty='putty -X'
 
 # shell directory bookmark setting
 SHELLBM_FILE=~/.sdirs

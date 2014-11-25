c81d4c7a9730efad7043f4318856faaac65f780b
zhoujd <zjd-405@163.com>
2014-11-25 23:44:14 +0800

    format code

--- bin/apply-patches.py
+++ bin/apply-patches.py
@@ -2,6 +2,10 @@
 
 import sys, os
 
+if len(sys.argv) < 2:
+    print "Using example: %s <patches folder>" % sys.argv[0]
+    sys.exit(1)
+
 patchdir = sys.argv[1]
 f = os.popen("ls -1 %s | sort -n" % (patchdir))
 

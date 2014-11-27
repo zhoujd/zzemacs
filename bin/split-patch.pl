0883c29d653f58c78d8a41ba13bc24566c8be2d0
zhoujd <zjd-405@163.com>
2014-11-28 00:02:52 +0800

    clean code

--- bin/split-patch.pl
+++ bin/split-patch.pl
@@ -20,28 +20,28 @@ sub main {
     print "hello, zhoujd\n";
 
     help() if (@ARGV != 2);
-    
+
     my $diff_a = $ARGV[0];
     my $diff_b = $ARGV[1];
     my $index  = 0;
-    
+
     foreach  my $item (@diff_file_list) {
         $index += 1;
-        
+
         my $file_diff_name = $item;
         $file_diff_name =~ s|.*/||;
 
         my $index_prefix = sprintf("%04d", $index);
         my $cmd = "git diff $diff_a -- $item $diff_b -- $item > $split_dir/${index_prefix}_${file_diff_name}_diff";
-        
-        
+
+
         print "$cmd\n";
 
         (system("mkdir -p $split_dir") == 0) || die "can`t run $cmd $!";
         (system($cmd) == 0) || die "can`t run $cmd $!";
     }
 
-    print "patches to $index files finished.\n";    
+    print "patches to $index files finished.\n";
 }
 
 main;

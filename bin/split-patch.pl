0b3ce2a54d5034a97ed22492cf6cdc801e5a05a3
zhoujd <zjd-405@163.com>
2014-12-03 03:00:30 +0800

    clean code

--- bin/split-patch.pl
+++ bin/split-patch.pl
@@ -20,26 +20,22 @@ sub main {
     my $diff_b = $ARGV[1];
 
     my @diff_stat = `git diff --stat $diff_a $diff_b`;
-    my $index  = 0;
+    my $index     = 0;
 
-    foreach my $i (0 .. @diff_stat - 2) {
-        my $line = $diff_stat[$i];
-        chomp($line);
-        $line =~ s/\|.*$//;
-        $line =~ s/^\s+//;
-        $line =~ s/\s+$//;
-
-        my $file_diff_name = $line;
-        $file_diff_name =~ s|.*/||;  # remove folder only file
+    foreach my $i (0 .. $#diff_stat - 1) {
+        my $file_diff_name = $diff_stat[$i];
+        chomp($file_diff_name);
+        $file_diff_name =~ s/\|.*$//;     # trim file diff status
+        $file_diff_name =~ s/^\s+//;      # trim head space
+        $file_diff_name =~ s/\s+$//;      # trim tail space
+        $file_diff_name =~ s|.*/||;       # remove folder only file
 
         my $index_prefix = sprintf("%04d", ++$index);
-        my $cmd = "git diff $diff_a -- $file_diff_name $diff_b -- $file_diff_name > $split_dir/${index_prefix}-${file_diff_name}.diff";
+        my $cmd = "git diff $diff_a -- $file_diff_name $diff_b -- $file_diff_name > $split_dir/$index_prefix-$file_diff_name.diff";
         print "$cmd\n";
 
         (system("mkdir -p $split_dir") == 0) || die "can`t run $cmd $!";
         (system($cmd) == 0) || die "can`t run $cmd $!";
-
-        print "== $line\n";
     }
 
     printf "patches to %d files finished.\n", $index;

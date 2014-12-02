dba1c9aa674cfe46772184461ad03ec6f622ad87
zhoujd <zjd-405@163.com>
2014-12-03 02:49:56 +0800

    clean code

--- bin/split-patch.pl
+++ bin/split-patch.pl
@@ -20,36 +20,29 @@ sub main {
     my $diff_b = $ARGV[1];
 
     my @diff_stat = `git diff --stat $diff_a $diff_b`;
-    my @diff_file_list = ();
+    my $index  = 0;
+
     foreach my $i (0 .. @diff_stat - 2) {
         my $line = $diff_stat[$i];
         chomp($line);
         $line =~ s/\|.*$//;
         $line =~ s/^\s+//;
         $line =~ s/\s+$//;
-        $diff_file_list[$i] = $line;
+
+        my $file_diff_name = $line;
+        $file_diff_name =~ s|.*/||;  # remove folder only file
+
+        my $index_prefix = sprintf("%04d", ++$index);
+        my $cmd = "git diff $diff_a -- $file_diff_name $diff_b -- $file_diff_name > $split_dir/${index_prefix}-${file_diff_name}.diff";
+        print "$cmd\n";
+
+        (system("mkdir -p $split_dir") == 0) || die "can`t run $cmd $!";
+        (system($cmd) == 0) || die "can`t run $cmd $!";
 
         print "== $line\n";
     }
 
-    if (@diff_file_list != 0) {
-        my $index  = 0;
-        foreach  my $index (0 .. $#diff_file_list) {
-            my $file_diff_name = $diff_file_list[$index];
-            $file_diff_name =~ s|.*/||;
-
-            my $index_prefix = sprintf("%04d", $index + 1);
-            my $cmd = "git diff $diff_a -- $file_diff_name $diff_b -- $file_diff_name > $split_dir/${index_prefix}-${file_diff_name}.diff";
-            print "$cmd\n";
-
-            (system("mkdir -p $split_dir") == 0) || die "can`t run $cmd $!";
-            (system($cmd) == 0) || die "can`t run $cmd $!";
-        }
-
-        printf "patches to %d files finished.\n", $#diff_file_list + 1;
-    } else {
-        print "no file chaned between $diff_a and $diff_b\n";
-    }
+    printf "patches to %d files finished.\n", $index;
 }
 
 main;

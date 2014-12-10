05afd07fc2d3821eb9bf5d71cbfc929df2d7a32c
zhoujd <zjd-405@163.com>
2014-12-10 12:13:48 +0800

    more for git

--- install.sh
+++ install.sh
@@ -26,8 +26,8 @@ Install_fonts_conf()
     FONT_TARGET=~/.fonts
     mkdir -p $FONT_TARGET
 
-    ln -sf ${ZZEMACS_ROOT}/font/consola/*   $FONT_TARGET
-    ln -sf ${ZZEMACS_ROOT}/font/MSYHMONO/*  $FONT_TARGET
+    ln -sf ${ZZEMACS_ROOT}/font/consola/*.ttf  $FONT_TARGET
+    ln -sf ${ZZEMACS_ROOT}/font/*.ttf          $FONT_TARGET
 }
 
 ##setup runemacs.sh

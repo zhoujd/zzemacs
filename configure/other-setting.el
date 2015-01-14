36ee992f300502dd630f710d1690b5c18e5bfbaa
zhoujd <zjd-405@163.com>
2015-01-14 19:42:34 +0800

    more saving

--- configure/other-setting.el
+++ configure/other-setting.el
@@ -306,9 +306,9 @@
 
 ;;powerline
 (require 'powerline)
-(custom-set-faces
- '(mode-line ((t (:foreground "#030303" :background "#bdbdbd" :box nil))))
- '(mode-line-inactive ((t (:foreground "#f9f9f9" :background "#666666" :box nil)))))
+(set-face-attribute 'mode-line nil
+                    :background "#bdbdbd"
+                    :box nil)
 
 
 (provide 'other-setting)

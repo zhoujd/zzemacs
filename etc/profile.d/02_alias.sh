df3a3d7f9f5132a83e0d4d2dcb0afdeb04eaf785
zhoujd <zjd-405@163.com>
2015-01-21 17:12:25 +0800

    chmod and for grep color

--- etc/profile.d/02_alias.sh
+++ etc/profile.d/02_alias.sh
@@ -22,6 +22,7 @@ alias ec='emacsclient -c'
 alias ET="SUDO_EDITOR=\"emacsclient -t\" sudo -e"
 alias EC="SUDO_EDITOR=\"emacsclient -c\" sudo -e"
 alias lcsh="csh -l"
+alias grep='grep --color=always'
 
 #alias ..='cd ..'
 #alias ...='cd ../..'

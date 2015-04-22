e9dc92257a8eaf14204658c7d9feebe15e445bbf
zhoujd <zjd-405@163.com>
2015-04-22 23:45:23 +0800

    clean code

--- etc/profile.d/04_function.sh
+++ etc/profile.d/04_function.sh
@@ -3,14 +3,15 @@
 ## useful function like alias
 ipaddr()  { ifconfig $1 | grep inet | awk '{print $2}' | sed 's/^addr://g'; }
 cdl()     { cd "$@";  l; }
+xrpm()    { [ "$1" != "" ] && (rpm2cpio "$1" | cpio -idmv); }
 
 ## chomod directory recursivly
 chmoddr() 
 {
     if [ -d "$1" ]; then 
-        echo "error: please use the mode first, then the directory";
-        return 1;
+        echo "error: please use the mode first, then the directory"
+        return 1
     elif [ -d "$2" ]; then
-        find $2 -type d -print0 | xargs -0 chmod $1;
+        find $2 -type d -print0 | xargs -0 chmod $1
     fi
 }

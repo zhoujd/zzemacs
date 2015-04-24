f658a28741e67d950687f4c417888b5629bf8c46
zhoujd <zjd-405@163.com>
2015-04-24 15:23:45 +0800

    format code for prompt

--- etc/profile.d/01_common.sh
+++ etc/profile.d/01_common.sh
@@ -6,25 +6,30 @@
 # ls colors setting
 export LS_COLORS='di=01;35:ln=04'
 
-norm='\[\033[m\]'
-bold='\[\033[1m\]'
-underline='\[\033[4m\]'
-bold='\[\033[5m\]'
-inverted='\[\033[7m\]'
+color_prompt()
+{
+    local norm='\[\033[m\]'
+    local bold='\[\033[1m\]'
+    local underline='\[\033[4m\]'
+    local bold='\[\033[5m\]'
+    local inverted='\[\033[7m\]'
 
-red='\[\033[0;31m\]'
-lred='\[\033[1;31m\]'
-green='\[\033[0;32m\]'
-lgreen='\[\033[1;32m\]'
-orange='\[\033[0;33m\]'
-yellow='\[\033[1;33m\]'
-blue='\[\033[0;34m\]'
-lblue='\[\033[1;34m\]'
-mangenta='\[\033[1;35m\]'
-white='\[\033[1;37m\]'
+    local red='\[\033[0;31m\]'
+    local lred='\[\033[1;31m\]'
+    local green='\[\033[0;32m\]'
+    local lgreen='\[\033[1;32m\]'
+    local orange='\[\033[0;33m\]'
+    local yellow='\[\033[1;33m\]'
+    local blue='\[\033[0;34m\]'
+    local lblue='\[\033[1;34m\]'
+    local mangenta='\[\033[1;35m\]'
+    local white='\[\033[1;37m\]'
 
-if [ $(whoami) = 'root' ]; then
-    PS1="${yellow}\u@\h${white}:${norm}\w${norm}${lred}#${norm} "
-else
-    PS1="${yellow}\u@\h${white}:${norm}\w${norm}\$ "
-fi
+    if [ $(whoami) = 'root' ]; then
+        PS1="${yellow}\u@\h${white}:${norm}\w${norm}${lred}#${norm} "
+    else
+        PS1="${yellow}\u@\h${white}:${norm}\w${norm}\$ "
+    fi
+}
+
+color_prompt

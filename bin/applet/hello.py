71b0188f3651caa28c8a3aa1a08535775319379b
zhoujd <zjd-405@163.com>
2015-07-08 14:10:43 +0800

    add sample code for hello.py

---
+++ bin/applet/hello.py
@@ -0,0 +1,10 @@
+## This a test for zz.py
+
+def main():
+    print "Hello, Zachary"
+
+if __name__ == '__main__':
+    try:
+        main()
+    except KeyboardInterrupt:
+        print "\nUser Press Ctrl+C, exit\n"

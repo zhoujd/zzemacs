f77f15a7bbfe910a8d46751a117ea22e8a269786
zhoujd <zjd-405@163.com>
2015-07-08 15:14:22 +0800

    clean code

--- libexec/glibc-check.sh
+++ libexec/glibc-check.sh
@@ -1,5 +1,11 @@
 #!/bin/sh
 
+##Check run OS
+if [ "$OS" = "Windows_NT" ] ; then
+    echo "This script is not support on windows."
+    exit 0
+fi
+
 GLIBC_FOLDER=/lib64
 GLIBC_PATH=$GLIBC_FOLDER/libc.so.6
 

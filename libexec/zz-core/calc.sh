334f814aa674cc5a53203237d36c221ea8d384e1
zhoujd <zjd-405@163.com>
2015-07-15 09:38:19 +0800

    format code

--- libexec/zz-core/calc.sh
+++ libexec/zz-core/calc.sh
@@ -1,12 +1,13 @@
 #!/bin/bash
 
+# variable setting
 input_num=1
+cpu_mem_file=cpu_mem.txt
+
+# set channel number if input by command line
 if [ ! $1 = "" ]; then
     input_num=$2 
 fi
-
-cpu_mem_file=cpu_mem.txt
-
 
 cpu_core_num=`cat /proc/cpuinfo | grep -c processor`
 eval $(awk '{a+=$1;b+=$2} END {printf("avgcpu=%.1f\navgmem=%.1f", (a/(NR*'"$cpu_core_num"'))*'"$input_num"',(b/NR));}' $cpu_mem_file)
--- libexec/zz-core/top.sh
+++ libexec/zz-core/top.sh
@@ -18,7 +18,7 @@ rm -f $LogFile
 while true
 do
     # collect cpu and mem usage
-    top -d 1 -bn 1 -c | grep $AppName |grep -v grep|awk '{print $9"\t"$10}'| grep -v 0.0 >> $LogFile
+    top -d 1 -bn 1 -c | grep $AppName | grep -v grep | awk '{print $9"\t"$10}' | grep -v 0.0 >> $LogFile
 
     # sleep for next
     sleep $Interval

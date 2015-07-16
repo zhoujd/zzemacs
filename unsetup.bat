5e81797580738410d5d3f305aefe8c56b1fd8340
zhoujd <zjd-405@163.com>
2015-07-16 16:50:56 +0800

    more setting

--- unsetup.bat
+++ unsetup.bat
@@ -1,12 +1,12 @@
 @echo off
-echo setup to %OS% start ...
+echo unsetup to %OS% start ...
 
 set MYCFG_PATH="%APPDATA%\.emacs"
 set CURRENT_DIR=%CD%
 
 if exist %MYCFG_PATH% del %MYCFG_PATH%
 
-echo setup python environment ...
+echo unsetup python environment ...
 @cd third-party\python
 @if exist unsetup.bat call unsetup.bat
 @echo off & cd %CURRENT_DIR%
@@ -14,7 +14,7 @@ echo setup python environment ...
 set MYCFG_PATH=
 set CURRENT_DIR=
 
-echo setup to %OS% end ...
+echo unsetup to %OS% end ...
 pause
 
 @echo on

970549630f3da4b9cd11a261ec258de1ed808485
zhoujd <zjd-405@163.com>
2015-07-17 12:41:33 +0800

    more setting

--- bin/bashrc-proxy-setup.sh
+++ bin/bashrc-proxy-setup.sh
@@ -1,6 +1,21 @@
 #!/bin/sh
 
-cat > $HOME/.bash_proxy <<EOF
+echo "Setup self .bashrc start ..."
+echo "Usage: `basename $0` [host:port]"
+
+##Check run OS
+if [ "$OS" = "Windows_NT" ] ; then
+    SETUP_ROOT=$(cd $(dirname $0) && pwd -W)
+else
+    SETUP_ROOT=$(cd $(dirname $0) && pwd)
+fi
+
+##Import vars and functions
+. $SETUP_ROOT/sample.sh
+
+ZZEMACS_ROOT=$(cd $SETUP_ROOT/.. && pwd)
+
+cat > $ZZEMACS_ROOT/etc/profile.d/99_proxy.sh <<EOF
 ## This is for bash proxy
 export http_proxy=$1
 export https_proxy=\$http_proxy

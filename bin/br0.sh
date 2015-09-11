7dbec369a388bb75dbbe8348d26bcacdc0aab9ff
zhoujd <zjd-405@163.com>
2015-09-11 21:19:25 +0800

    more for br0

--- bin/br0.sh
+++ bin/br0.sh
@@ -13,14 +13,14 @@ fi
 eth0=$1
 br0=$2
 
-ifconfig $eth0 down                  # 先关闭eth0接口
-brctl addbr $br0                     # 增加一个虚拟网桥br0
-brctl addif $br0 $eth0               # 在br0中添加一个接口eth0
-brctl stp $br0 off                   # 只有一个网桥，所以关闭生成树协议
-brctl setfd $br0 1                   # 设置br0的转发延迟
-brctl sethello $br0 1                # 设置br0的hello时间
-ifconfig $br0 0.0.0.0 promisc up     # 打开br0接口
-ifconfig $eth0 0.0.0.0 promisc up    # 打开eth0接口
-dhclient $br0                        # 从dhcp服务器获得br0的IP地址
-brctl show $br0                      # 查看虚拟网桥列表
-brctl showstp $br0                   # 查看br0的各接口信息
+ifconfig $eth0 down                  # close eth0 first
+brctl addbr $br0                     # add virtual bridge br0
+brctl addif $br0 $eth0               # add interface eth0 on br0
+brctl stp $br0 off                   # only on bridge, so close generate tree protocol
+brctl setfd $br0 1                   # set br0 delaytime of forward
+brctl sethello $br0 1                # set br0 hello time
+ifconfig $br0 0.0.0.0 promisc up     # open br0
+ifconfig $eth0 0.0.0.0 promisc up    # open eth0
+dhclient $br0                        # br0 get IP address from DHCP server
+brctl show $br0                      # show virtual bridge list
+brctl showstp $br0                   # show interface information on br0

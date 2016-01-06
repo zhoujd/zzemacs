ec46fa4a7a03c2d20bcc3053929d921e02dfea43
zhoujd <zjd-405@163.com>
2016-01-06 15:42:05 +0800

    temp saving

--- configure/env-setting.el
+++ configure/env-setting.el
@@ -2,43 +2,46 @@
 ;;
 
 ;;add path for excute files
-(when-ms-windows
- (setq my-env-path (list
-                    (concat zzemacs-path "bin")
-                    "C:/Git/bin"
-                    "C:/Git/libexec/git-core"
-                    "C:/zznix/bin"
-                    (format "%s/bin" (getenv "EMACS_DIR"))
-                    (format "%s/zztools/bcompare" (getenv "HOME"))
-                    (format "%s/zztools/perforce" (getenv "HOME"))
-                    "C:/strawberry/perl/bin"
-                    "C:/strawberry/c/bin"
-                    "C:/Vim/vim73"
-                    "C:/Program Files (x86)/CMake 2.8/bin"
-                    "C:/Program Files (x86)/Mozilla Firefox"
-                    "C:/Program Files (x86)/Microsoft Visual Studio 10.0/VC/bin"
-                    "C:/Program Files (x86)/Microsoft Visual Studio 10.0/VC"
-                    "C:/Program Files (x86)/Microsoft Visual Studio 10.0/Common7/IDE"
-                    "C:/Program Files/Windows Kits/8.0/Debuggers/x64"
-                    "C:/Scheme/Gambit-C/v4.6.7/bin"
-                    "C:/Scheme/Racket"
-                    "C:/Python27/Scripts"
-                    "C:/Python27"
-                    "C:/Lisp/ecl"
-                    "C:/Lisp/SBCL"
-                    "C:/Lisp/clisp"
-                    )))
-(unless-ms-windows
- (setq my-env-path (list
-                    (concat zzemacs-path "bin")
-                    "/sbin"
-                    "/usr/sbin"
-                    "/usr/local/racket/bin"
-                    "/usr/local/Gambit-C/bin"
-                    (format "%s/zztools/bcompare/bin" (getenv "HOME"))
-                    (format "%s/zztools/meld/bin" (getenv "HOME"))
-                    (format "%s/zztools/p4v/bin" (getenv "HOME"))
-                    )))
+(defvar my-env-path
+  (if-ms-windows
+   (progn
+    (list
+     (concat zzemacs-path "bin")
+     "C:/Git/bin"
+     "C:/Git/libexec/git-core"
+     "C:/zznix/bin"
+     (format "%s/bin" (getenv "EMACS_DIR"))
+     (format "%s/zztools/bcompare" (getenv "HOME"))
+     (format "%s/zztools/perforce" (getenv "HOME"))
+     "C:/strawberry/perl/bin"
+     "C:/strawberry/c/bin"
+     "C:/Vim/vim73"
+     "C:/Program Files (x86)/CMake 2.8/bin"
+     "C:/Program Files (x86)/Mozilla Firefox"
+     "C:/Program Files (x86)/Microsoft Visual Studio 10.0/VC/bin"
+     "C:/Program Files (x86)/Microsoft Visual Studio 10.0/VC"
+     "C:/Program Files (x86)/Microsoft Visual Studio 10.0/Common7/IDE"
+     "C:/Program Files/Windows Kits/8.0/Debuggers/x64"
+     "C:/Scheme/Gambit-C/v4.6.7/bin"
+     "C:/Scheme/Racket"
+     "C:/Python27/Scripts"
+     "C:/Python27"
+     "C:/Lisp/ecl"
+     "C:/Lisp/SBCL"
+     "C:/Lisp/clisp"
+     ))
+   (progn
+    (list
+     (concat zzemacs-path "bin")
+     "/sbin"
+     "/usr/sbin"
+     "/usr/local/racket/bin"
+     "/usr/local/Gambit-C/bin"
+     (format "%s/zztools/bcompare/bin" (getenv "HOME"))
+     (format "%s/zztools/meld/bin" (getenv "HOME"))
+     (format "%s/zztools/p4v/bin" (getenv "HOME"))
+     )))
+  "add to path and exec-path")
 
 (mapcar 'zz-add-os-path (reverse my-env-path))
 

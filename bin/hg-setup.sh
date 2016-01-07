e07856e953c6453e6d8f733ca130ef708d5de13b
zhoujd <zjd-405@163.com>
2016-01-07 16:33:54 +0800

    format code

--- bin/hg-setup.sh
+++ bin/hg-setup.sh
@@ -1,8 +1,5 @@
 #!/bin/sh
 
-##Import vars and functions
-. sample.sh
-
 echo "hg setup start ..."
 
 echo "remove ~/.hgrc and setting hg configure ..."
@@ -10,6 +7,20 @@ cat > ~/.hgrc <<EOF
 [ui]
 username = zhoujd<zjd-405@163.com>
 verbose = True
+merge = p4
+
+[merge-tools]
+p4.executable = p4merge
+p4.args = $base $local $other $output
+p4.priority = 1
+p4.premerge = True
+p4.gui = True
+
+bcomp.executable = bcomp
+bcomp.args = $local $other $base $output
+bcomp.priority = 1
+bcomp.premerge = True
+bcomp.gui = True
 
 [extensions]
 hgext.extdiff =
@@ -17,7 +28,7 @@ bookmarks =
 
 [extdiff]
 cmd.bcomp = bcomp
-opts.bcomp = /ro
+cmd.p4diff = p4merge
 
 [web] 
 push_ssl = false

6e065980e72edd703d3db401e298b7691976118e
zhoujd <zjd-405@163.com>
2016-01-08 13:54:14 +0800

    merge from hg

--- configure/perl-setting.el
+++ configure/perl-setting.el
@@ -1,33 +1,32 @@
 ;;;perl programme setting
 
 ;;conflict with rgrep find-grep
-;(zz-load-path "site-lisp/pde/lisp")
-;(require 'pde-load)
+(zz-load-path "site-lisp/pde/lisp")
+(require 'pde-load)
 
-;;;;perl code style
-;(defun pde-perl-mode-hook ()
-;  (abbrev-mode t)
-;  (hs-minor-mode t)
-;  (setq cperl-lazy-help-time 2)
-;  (cperl-lazy-install)
-;  (add-to-list 'cperl-style-alist
-;               '("MYPDE"
-;                 (cperl-auto-newline                         . nil)
-;                 (cperl-brace-offset                         . 0)
-;                 (cperl-close-paren-offset                   . -4)
-;                 (cperl-continued-brace-offset               . 0)
-;                 (cperl-continued-statement-offset           . 0)
-;                 (cperl-extra-newline-before-brace           . nil)
-;                 (cperl-extra-newline-before-brace-multiline . nil)
-;                 (cperl-indent-level                         . 4)
-;                 (cperl-indent-parens-as-block               . t)
-;                 (cperl-label-offset                         . -4)
-;                 (cperl-merge-trailing-else                  . t)
-;                 (cperl-tab-always-indent                    . t)))
-;  (cperl-set-style "MYPDE"))
+;;;perl code style
+(defun pde-perl-mode-hook ()
+  (abbrev-mode t)
+  (hs-minor-mode t)
+  (setq cperl-lazy-help-time 2)
+  (cperl-lazy-install)
+  (add-to-list 'cperl-style-alist
+               '("MYPDE"
+                 (cperl-auto-newline                         . nil)
+                 (cperl-brace-offset                         . 0)
+                 (cperl-close-paren-offset                   . -4)
+                 (cperl-continued-brace-offset               . 0)
+                 (cperl-continued-statement-offset           . 0)
+                 (cperl-extra-newline-before-brace           . nil)
+                 (cperl-extra-newline-before-brace-multiline . nil)
+                 (cperl-indent-level                         . 4)
+                 (cperl-indent-parens-as-block               . t)
+                 (cperl-label-offset                         . -4)
+                 (cperl-merge-trailing-else                  . t)
+                 (cperl-tab-always-indent                    . t)))
+  (cperl-set-style "MYPDE"))
 
-;(add-hook 'cperl-mode-hook 'pde-perl-mode-hook)
-
+(add-hook 'cperl-mode-hook 'pde-perl-mode-hook)
 
 (defalias 'perl-mode 'cperl-mode)
 

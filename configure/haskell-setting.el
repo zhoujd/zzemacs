5abe2ceb76ff48dd67d3af39524ba943c442a3d3
zhoujd <zjd-405@163.com>
2016-01-13 15:45:36 +0800

    update for configure/haskell-setting.el

--- configure/haskell-setting.el
+++ configure/haskell-setting.el
@@ -6,6 +6,17 @@
 (require 'haskell-mode-autoloads)
 (add-to-list 'Info-default-directory-list (concat zzemacs-path "/site-lisp/haskell-mode/"))
 
+(add-hook 'haskell-mode-hook 'turn-on-haskell-doc-mode)
+(add-hook 'haskell-mode-hook 'turn-on-haskell-indent)
+(add-hook 'haskell-mode-hook 'font-lock-mode)
+(add-hook 'haskell-mode-hook 'imenu-add-menubar-index)
+(add-hook 'haskell-mode-hook 'outline-minor-mode)
+
+(defun my-haskell-mode-hook()
+  (make-local-variable 'outline-regexp)
+  (setq outline-regexp "^//w+[^/n/r]*::[^/n/r]*"))
+
+(add-hook 'haskell-mode-hook 'my-haskell-mode-hook)
 
 (provide 'haskell-setting)
 

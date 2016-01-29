365a0c480e9847f34b621f0faa91f381d479a34f
zhoujd <zjd-405@163.com>
2016-01-29 18:14:32 +0800

    format code

--- bin/hg-merge-wrapper.py
+++ bin/hg-merge-wrapper.py
@@ -5,7 +5,17 @@
 
 import os
 import sys
+import platform
 
-merge_tool = "p4merge" ##bcompare
-os.system(merge_tool + " " + " ".join(sys.argv[1:]))
+merge_tool = ["p4merge", "meld", "bcompare"]
+merge_select = merge_tool[0]
 
+sysstr = platform.system()
+if sysstr =="Windows":
+    merge_select = merge_tool[2]
+elif sysstr == "Linux":
+    merge_select = merge_tool[0]
+else:
+    print "Other System tasks"
+
+os.system(merge_select + " " + " ".join(sys.argv[1:]))

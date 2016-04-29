cfd33896fd565ce3aa9851d13614c4ca62438763
zhoujd <zjd-405@163.com>
2016-04-29 18:45:08 +0800

    add gnuplot-mode

---
+++ configure/gnuplot-setting.el
@@ -0,0 +1,15 @@
+;;;; gnuplot-setting.el --- gnuplot config file
+;;
+
+;;; gnuplot-mode
+(autoload 'gnuplot-mode "gnuplot"
+  "gnuplot major mode" t)
+(autoload 'gnuplot-make-buffer "gnuplot"
+  "open a buffer in gnuplot mode" t)
+(setq auto-mode-alist
+      (append '(("\\.gp$" . gnuplot-mode))
+	      auto-mode-alist))
+
+(global-set-key [M-f9] 'gnuplot-make-buffer)
+
+;;; gnuplot-setting.el ends here

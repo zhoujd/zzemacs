3115844befa835f26603625d1cf09ba59011e2b8
zhoujd <zjd-405@163.com>
2018-04-28 18:23:54 +0800

    update markdown on Linux

--- site-lisp/markdown-mode/webpage.sh
+++ site-lisp/markdown-mode/webpage.sh
@@ -1,37 +1,21 @@
 #!/bin/sh
 
-STARTRE='^;;; Commentary:$'
-STOPRE='^$'
 DATE=$(date +"%B %e, %Y %H:%M %Z")
 
-echo "title: Emacs Markdown Mode
+echo "title: Markdown Mode for Emacs
 description: A major mode for GNU Emacs for editing Markdown-formatted text files.
 markup: markdown
-icon: emacs
 city: Columbus
 created: May 24, 2007 23:47 GMT
-modified: $DATE" > index.text
+modified: $DATE
+style: #badges { margin-bottom: 1.5rem; }
 
-cat markdown-mode.el |\
-    # Keep only the Commentary section
-    awk "/$STARTRE/,/$STOPRE/" |\
-    # Remove the start and step regexps
-    grep -v "$STARTRE" | grep -v "$STOPRE" |\
-    # Convert headers
-    sed -e 's/^;;; \(.*\):$/## \1/' |\
-    # Remove leading spaces (but don't disturb pre blocks)
-    sed -e 's/^;;[ ]\{0,1\}//' |\
-    # Escape wiki links
-    #sed -e 's/\(\[\[[^]\n]*\]\]\)/\\\1/g' |\
-    # Use Markdown-style backticks for single-quoted lisp code
-    sed -e 's/`\([^'\'']*\)'\''/`\1`/g' |\
-    # Use <kbd> tags for single character, unprefixed keybindings
-    sed -e 's/`\([^`]\)`/<kbd>\1<\/kbd>/g' |\
-    # Use <kbd> tags for TAB and RET keys
-    sed -e 's/`TAB`/<kbd>TAB<\/kbd>/g' |\
-    sed -e 's/`RET`/<kbd>RET<\/kbd>/g' |\
-    # Use <kbd> tags for keybindings prefixed by C, M, orS
-    sed -e 's/`\([CMS]-[^`]*\)`/<kbd>\1<\/kbd>/g' |\
-    # Remove email addresses
-    sed -e 's/ <.*@.*> / /g' \
-    >> index.text
+<div id=\"badges\">
+<a href=\"https://github.com/jrblevin/markdown-mode\"><img src=\"https://img.shields.io/github/stars/jrblevin/markdown-mode.svg?style=social&label=GitHub\" alt=\"GitHub\"/></a>
+<a href=\"https://melpa.org/#/markdown-mode\"><img src=\"https://melpa.org/packages/markdown-mode-badge.svg\" alt=\"MELPA badge\"/></a>
+<a href=\"https://stable.melpa.org/#/markdown-mode\"><img src=\"https://stable.melpa.org/packages/markdown-mode-badge.svg\" alt=\"MELPA stable badge\"/></a>
+<a href=\"https://travis-ci.org/jrblevin/markdown-mode\"><img src=\"https://travis-ci.org/jrblevin/markdown-mode.svg?branch=master\" alt=\"Travis CI Build Status\"/></a>
+<a href=\"https://leanpub.com/markdown-mode\"><img src=\"https://img.shields.io/badge/leanpub-guide-orange.svg\" alt=\"Guide to Markdown Mode for Emacs\"/></a>
+</div>" > index.text
+
+cat README.md >> index.text

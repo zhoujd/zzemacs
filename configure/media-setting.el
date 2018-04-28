a879cb151b5ac4d71dff5cd6167cce1104a75e8c
zhoujd <zjd-405@163.com>
2018-04-28 22:34:33 +0800

    format code

--- configure/media-setting.el
+++ configure/media-setting.el
@@ -4,10 +4,11 @@
 (zz-load-path "site-lisp")
 
 ;; https://www.mpg123.de/download.shtml
-(defvar mpg123-startup-volume nil)
 (require 'mpg123)
 (setq mpg123-lang 0)
-(add-hook 'mpg123-hook (lambda () (local-unset-key (kbd "<down-mouse-1>"))))
+(add-hook 'mpg123-hook
+          (lambda ()
+            (local-unset-key (kbd "<down-mouse-1>"))))
 
 (require 'mpg123-remote)
 (setq mpg123-show-help t)
@@ -17,11 +18,11 @@
 
 (defun my-mpg123-format-name-function (artist album title tracknum filename)
   (concat (if (or mpg123-omit-id3-artist
-		  (string= artist ""))
-	      ""
-	    (concat artist " - "))
-	  (if (string< "" title) title
-	    filename)))
+                  (string= artist ""))
+              ""
+              (concat artist " - "))
+          (if (string< "" title) title
+              filename)))
 
 (provide 'media-setting)
 

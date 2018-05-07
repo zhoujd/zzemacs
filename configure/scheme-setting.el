d2b263448d538ad631e8e4bd6471ed7e03af0eb7
zhoujd <zjd-405@163.com>
2018-05-07 12:22:19 +0800

    clean code

--- configure/scheme-setting.el
+++ configure/scheme-setting.el
@@ -75,11 +75,6 @@
 ;;http://docs.racket-lang.org/guide/Emacs.html
 (zz-load-path "site-lisp/racket-mode")
 (require 'racket-mode)
-(add-hook 'racket-mode-hook
-          (lambda ()
-            (define-key racket-mode-map (kbd "C-c r") 'racket-run)))
-(add-hook 'racket-mode-hook      #'racket-unicode-input-method-enable)
-(add-hook 'racket-repl-mode-hook #'racket-unicode-input-method-enable)
 (setq tab-always-indent 'complete)
 
 

c9a4f3f8a1b196694e80d6aad4d7b21350118800
Zachary Zhou <zachary.zhou@hotmail.com>
2018-05-14 21:58:12 +0800

    update for script

--- bin/build-emacs.sh
+++ bin/build-emacs.sh
@@ -54,7 +54,7 @@ Install_package()
     elif [ "$OS_DISTRO" = "Ubuntu" ]; then
         sudo apt-get build-dep -y emacs24
         sudo apt-get install -y build-essential
-        sudo apt-get install -y libxpm-dev libpng12-dev libjpeg-dev libtiff4-dev libgif-dev
+        sudo apt-get install -y libxpm-dev libpng12-dev libjpeg-dev libtiff5-dev libgif-dev
         sudo apt-get install -y libxaw7-dev libncurses5-dev libgtk2.0-dev librsvg2-dev libdbus-1-dev libgconf2-dev
         sudo apt-get install -y libotf-dev libgpm-dev libm17n-dev libgnutls-dev libselinux1-dev imagemagick
     elif [ "$OS_DISTRO" = "CentOS" ]; then

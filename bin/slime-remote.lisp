6f5ae93700f90fb81890e07574826a787a6c086f
Zachary Zhou <zachary.zhou@hotmail.com>
2018-05-18 23:41:58 +0800

    add more setting

--- bin/slime-remote.lisp
+++ bin/slime-remote.lisp
@@ -2,9 +2,30 @@
 
 (write-line "Start slime at default port: 7070")
 
+(defun my-command-line ()
+  (or
+   #+CLISP *args*
+   #+SBCL *posix-argv*  
+   #+LISPWORKS system:*line-arguments-list*
+   #+CMU extensions:*command-line-words*
+   nil))
+
+(defun my-getenv (name &optional default)
+  #+CMU
+  (let ((x (assoc name ext:*environment-list*
+                  :test #'string=)))
+    (if x (cdr x) default))
+  #-CMU
+  (or
+   #+Allegro (sys:getenv name)
+   #+CLISP (ext:getenv name)
+   #+ECL (si:getenv name)
+   #+SBCL (sb-unix::posix-getenv name)
+   #+LISPWORKS (lispworks:environment-variable name)
+   default))
+
 #+sbcl
 (require 'swank)
-
 #+clisp
 (asdf:operate 'asdf:load-op 'swank)
 

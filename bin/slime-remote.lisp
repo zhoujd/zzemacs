e8be52d8edf4355f8d299ce716f66ea48bf57f49
Zachary Zhou <zachary.zhou@hotmail.com>
2018-05-18 22:41:58 +0800

    add more for slime remote

--- bin/slime-remote.lisp
+++ bin/slime-remote.lisp
@@ -1,12 +1,17 @@
 ;;; sbcl --load slime-remote.lisp
 
-(write-line "start slime at port:7070")
+(write-line "start slime at default port:7070")
 
 ;; auto start swank server
+#+sbcl
 (require 'swank)
-(defvar *swank-server-p* nil "swant server flag")
 
+#+clisp
+(asdf:operate 'asdf:load-op 'swank)
+
+(defvar *swank-server-p* nil "swant server flag")
+(defvar *swant-server-port* 7070  "swant server port")
 (unless *swank-server-p*
-  (swank:create-server :dont-close t :port 7070)
-  (setf swank:*use-dedicated-output-stream* nil)
+  (swank:create-server :dont-close t
+                       :port *swant-server-port*)
   (setf *swank-server-p* t))

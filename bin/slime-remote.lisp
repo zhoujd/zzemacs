c1cecaa1eb32d1c3a3caf7d296590f4dfabffbb7
Zachary Zhou <zachary.zhou@hotmail.com>
2018-05-18 23:22:18 +0800

    format code

--- bin/slime-remote.lisp
+++ bin/slime-remote.lisp
@@ -1,8 +1,7 @@
 ;;; sbcl --load slime-remote.lisp
 
-(write-line "start slime at default port:7070")
+(write-line "Start slime at default port: 7070")
 
-;; auto start swank server
 #+sbcl
 (require 'swank)
 
@@ -11,6 +10,8 @@
 
 (defvar *swank-server-p* nil "swant server flag")
 (defvar *swant-server-port* 7070  "swant server port")
+
+;; start swank server
 (unless *swank-server-p*
   (swank:create-server :dont-close t
                        :port *swant-server-port*)

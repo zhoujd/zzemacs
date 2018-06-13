4e0f4aed526c9e48cc9ac0e6396805f0cf45d626
Zachary Zhou <zachary.zhou@hotmail.com>
2018-06-13 20:29:18 +0800

    add package-setting.el

---
+++ configure/package-setting.el
@@ -0,0 +1,10 @@
+;;;; package-setting.el
+
+(require 'package)
+(add-to-list 'package-archives '("melpa" . "http://melpa.org/packages/"))
+(package-initialize)
+
+
+(provide 'package-setting)
+
+;;; package-setting.el ends here

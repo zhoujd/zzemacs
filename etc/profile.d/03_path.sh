27f7d6ac193104a4c1a9fc7bb742a8c844fe4343
Zachary Zhou <zachary.zhou@hotmail.com>
2018-07-20 16:48:54 +0800

    format code

--- etc/profile.d/03_path.sh
+++ etc/profile.d/03_path.sh
@@ -1,4 +1,9 @@
-### path
+### PATH
+
+SCRIPT_ROOT=$(cd $(dirname "${BASH_SOURCE[0]}") && pwd)
+ZZEMACS_HOME=$(cd $SCRIPT_ROOT/../.. && pwd)
+
+export PATH=${ZZEMACS_HOME}/bin:${ZZEMACS_HOME}/libexec:$PATH
 
 if [ -d ~/local/bin ]; then
     export PATH=~/local/bin:$PATH

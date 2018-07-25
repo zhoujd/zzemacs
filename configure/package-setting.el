07c79b5e3057daf8b9b3811698797a936b28d3e0
Zachary Zhou <zachary.zhou@hotmail.com>
2018-07-25 17:05:31 +0800

    add more for package

--- configure/package-setting.el
+++ configure/package-setting.el
@@ -2,6 +2,8 @@
 
 (require 'package)
 (add-to-list 'package-archives '("melpa" . "http://melpa.org/packages/"))
+(add-to-list 'package-archives '("melpa-stable" . "https://stable.melpa.org/packages/"))
+
 (package-initialize)
 
 

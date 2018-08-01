725a8e1791376cc6a247c4cebc62807bdbfdc3d1
Zachary Zhou <zachary.zhou@hotmail.com>
2018-08-01 16:05:33 +0800

    format code

--- etc/profile.d/03_path.sh
+++ etc/profile.d/03_path.sh
@@ -4,9 +4,9 @@ SCRIPT_ROOT=$(cd $(dirname "${BASH_SOURCE[0]}") && pwd)
 ZZEMACS_HOME=$(cd $SCRIPT_ROOT/../.. && pwd)
 
 ADD_PATH=(
+    $HOME/local/bin
     $ZZEMACS_HOME/bin
     $ZZEMACS_HOME/libexec
-    $HOME/local/bin
 )
 
 add-to-path() {

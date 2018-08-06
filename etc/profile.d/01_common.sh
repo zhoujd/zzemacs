f605afdc1dee57f1dab21046165739db2b71d3dd
Zachary Zhou <zachary.zhou@hotmail.com>
2018-08-06 13:11:56 +0800

    add cecho

--- etc/profile.d/01_common.sh
+++ etc/profile.d/01_common.sh
@@ -6,6 +6,28 @@
 # ls colors setting
 export LS_COLORS='di=01;35:ln=04'
 
+cecho() {
+    local exp=$1;
+    local color=$2;
+    
+    if ! [[ $color =~ '^[0-9]$' ]] ; then
+        case $(echo $color | tr '[:upper:]' '[:lower:]') in
+            black) color=0 ;;
+            red) color=1 ;;
+            green) color=2 ;;
+            yellow) color=3 ;;
+            blue) color=4 ;;
+            magenta) color=5 ;;
+            cyan) color=6 ;;
+            white|*) color=7 ;; # white or invalid color
+        esac
+    fi
+    
+    tput setaf $color;
+    echo $exp;
+    tput sgr0;
+}
+
 color_prompt() {
     local norm='\[\033[m\]'
     local bold='\[\033[1m\]'

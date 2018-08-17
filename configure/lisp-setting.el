fc7321b8ff968c59cd22974f5b7ab8088db71214
Zachary Zhou <zachary.zhou@hotmail.com>
2018-08-17 16:55:19 +0800

    add lisp indent style

--- configure/lisp-setting.el
+++ configure/lisp-setting.el
@@ -6,7 +6,13 @@
 (autoload 'common-lisp-indent-function "cl-indent")
 (add-hook 'lisp-mode-hook
           (lambda ()
-            (setq lisp-indent-function 'common-lisp-indent-function)))
+            (setq lisp-indent-function 'common-lisp-indent-function)
+            (put 'if 'lisp-indent-function nil)
+            (put 'when 'lisp-indent-function 1)
+            (put 'unless 'lisp-indent-function 1)
+            (put 'do 'lisp-indent-function 2)
+            (put 'do* 'lisp-indent-function 2)
+            ))
 
 ;;add common lisp configure file mode alias
 (setq auto-mode-alist

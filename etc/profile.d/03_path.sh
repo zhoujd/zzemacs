b4e356511408fdccd5b8d8bb8eec12ad02bba0cb
Zachary Zhou <zachary.zhou@hotmail.com>
2018-09-10 22:04:33 +0800

    format code

--- etc/profile.d/03_path.sh
+++ etc/profile.d/03_path.sh
@@ -11,8 +11,8 @@ ADD_PATH=(
 
 add-to-path() {
     case ":$PATH:" in
-        *":$1:"*) :;; # already there
-        *) PATH="$1:$PATH";; # or PATH="$PATH:$1"
+        *":$1:"*) :;;           # already there
+        *) PATH="$1:$PATH";;    # or PATH="$PATH:$1"
     esac
 }
 

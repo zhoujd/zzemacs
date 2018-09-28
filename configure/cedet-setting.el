4eee565944e890a881c644e920b037142ff704d8
Zachary Zhou <zachary.zhou@hotmail.com>
2018-09-28 10:23:26 +0800

    more for cedet setting

--- configure/cedet-setting.el
+++ configure/cedet-setting.el
@@ -3,38 +3,24 @@
 
 (zz-load-path "site-lisp")
 
+(setq semantic-idle-work-update-headers-flag  t)
+(setq semantic-idle-scheduler-idle-time       10)
+(setq semantic-idle-scheduler-work-idle-time  60)
+
 (setq semantic-default-submodes
-      '(;; Perform semantic actions during idle time
-        global-semantic-idle-scheduler-mode
-        ;; Use a database of parsed tags
+      '(global-semantic-idle-scheduler-mode
         global-semanticdb-minor-mode
-        ;; Decorate buffers with additional semantic information
-        global-semantic-decoration-mode
-        ;; Highlight the name of the function you're currently in
-        global-semantic-highlight-func-mode
-        ;; show the name of the function at the top in a sticky
-        global-semantic-stickyfunc-mode
-        ;; Generate a summary of the current tag when idle
         global-semantic-idle-summary-mode
-        ;; Show a breadcrumb of location during idle time
         global-semantic-idle-breadcrumbs-mode
-        ;; Switch to recently changed tags with `semantic-mrub-switch-tags',
-        ;; or `C-x B'
         global-semantic-mru-bookmark-mode))
-
-(semantic-mode t)
-(global-ede-mode t)
-(global-semantic-idle-completions-mode)
-
-(defun my:add-semantic-to-autocomplete() 
-  (add-to-list 'ac-sources 'ac-source-semantic))
 
 (mapc
  (lambda (mode)
-   (add-hook mode 'my:add-semantic-to-autocomplete))
- '(c-mode-common-hook
+   (add-hook mode 'semantic-mode))
+ '(c-mode-hook
+   c++-mode-hook
+   python-mode-hook
    ))
-
 
 (provide 'cedet-setting)
 

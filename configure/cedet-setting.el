e83f61d71ee9036bed3ea314a4e33d88547e4fb4
Zachary Zhou <zachary.zhou@hotmail.com>
2018-09-28 08:57:08 +0800

    add more for cedet

--- configure/cedet-setting.el
+++ configure/cedet-setting.el
@@ -3,15 +3,28 @@
 
 (zz-load-path "site-lisp")
 
-;; cedet
-(mapc (lambda (mode) (add-to-list 'semantic-default-submodes mode))
-      '(global-semantic-idle-scheduler-mode
+(setq semantic-default-submodes
+      '(;; Perform semantic actions during idle time
+        global-semantic-idle-scheduler-mode
+        ;; Use a database of parsed tags
         global-semanticdb-minor-mode
+        ;; Decorate buffers with additional semantic information
+        global-semantic-decoration-mode
+        ;; Highlight the name of the function you're currently in
+        global-semantic-highlight-func-mode
+        ;; show the name of the function at the top in a sticky
+        global-semantic-stickyfunc-mode
+        ;; Generate a summary of the current tag when idle
         global-semantic-idle-summary-mode
+        ;; Show a breadcrumb of location during idle time
+        global-semantic-idle-breadcrumbs-mode
+        ;; Switch to recently changed tags with `semantic-mrub-switch-tags',
+        ;; or `C-x B'
         global-semantic-mru-bookmark-mode))
 
-(global-ede-mode t)
 (semantic-mode t)
+(global-ede-mode t)
+(global-semantic-idle-completions-mode)
 
 (defun my:add-semantic-to-autocomplete() 
   (add-to-list 'ac-sources 'ac-source-semantic))
@@ -19,8 +32,7 @@
 (mapc
  (lambda (mode)
    (add-hook mode 'my:add-semantic-to-autocomplete))
- '(
-   c-mode-common-hook
+ '(c-mode-common-hook
    ))
 
 

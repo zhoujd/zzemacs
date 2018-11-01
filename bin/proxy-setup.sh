b81847a06f161cefaa07d562b2e7aeb7133c602b
Zachary Zhou <zachary.zhou@hotmail.com>
2018-11-01 10:17:32 +0800

    clean code

--- bin/proxy-setup.sh
+++ bin/proxy-setup.sh
@@ -3,7 +3,7 @@
 ## /etc/yum.conf
 # proxy=http://username:password@yourproxy:8080
 
-## /etc/apt/apt.conf
+## /etc/apt/apt.conf on Ubuntu 16.04
 # Acquire::http::proxy "http://127.0.0.1:8000";  
 # Acquire::ftp::proxy "ftp://127.0.0.1:8000";  
 # Acquire::https::proxy "https://127.0.0.1:8000";
@@ -20,27 +20,19 @@ echo "Setup proxy start ..."
 if [ "$OS" = "Windows_NT" ] ; then
     PROXY_SCRIPT=$HOME/.bashrc.d/99-proxy.sh
 else
-    ## 1: system, 0: user
-    SETUP_SYS=1
-    
-    if [ $SETUP_SYS -ne 0 ]; then
-        if [ $EUID -ne 0 ]; then
-            echo "You must be a root user" 2>&1
-            exit 1
-        fi
-        PROXY_SCRIPT=/etc/profile.d/zz-proxy.sh
-    else
-        PROXY_SCRIPT=$HOME/.bashrc
+    if [ $EUID -ne 0 ]; then
+        echo "You must be a root user" 2>&1
+        exit 1
     fi
+    PROXY_SCRIPT=/etc/profile.d/zz-proxy.sh
 fi
 
-cat <<EOF >> $PROXY_SCRIPT
+cat <<EOF > $PROXY_SCRIPT
 ## This is for bash proxy
 export http_proxy=http://$1/
 export https_proxy=\$http_proxy
 export ftp_proxy=\$http_proxy
 export no_proxy="localhost,127.0.0.0/8,::1,10.0.0.0/8,192.168.0.0/16"
-
 EOF
 
 echo "Setup proxy end ..."

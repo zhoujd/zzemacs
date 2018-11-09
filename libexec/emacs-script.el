f2dd00b0dc137438b19a5e2732f65fcd6d16af30
Zachary Zhou <zachary.zhou@hotmail.com>
2018-11-09 17:36:20 +0800

    more for emacs script

---
+++ libexec/emacs-script.el
@@ -0,0 +1,12 @@
+#!/usr/bin/emacs --script
+;;;http://ergoemacs.org/emacs/emacs.html
+
+(message "zzemacs setup script start ...")
+(message "zzemacs system-type:%s system-name:%s" system-type system-name)
+
+;;print argument is setting
+(when argv
+  (message "input argument are %s" argv))
+
+
+(message "zzemacs setup script end ...")

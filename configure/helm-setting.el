e79fad8a7f9897de61a89c80ed4236979656b967
Zachary Zhou <zachary.zhou@hotmail.com>
2018-11-11 11:31:49 +0800

    format code

--- configure/helm-setting.el
+++ configure/helm-setting.el
@@ -15,32 +15,32 @@
 (add-to-list 'helm-mode-no-completion-in-region-in-modes 'shell-mode)
 
 ;;https://writequit.org/eos/eos-helm.html
-(setq helm-display-header-line nil
-      helm-always-two-windows nil
-      helm-split-window-in-side-p t
-      helm-candidate-number-limit 100
-      helm-input-idle-delay 0
-      helm-tramp-verbose 0
-      helm-move-to-line-cycle-in-source t)
-
-(setq helm-mini-default-sources '(helm-source-buffers-list
-                                  helm-source-recentf
-                                  helm-source-bookmarks
-                                  helm-source-buffer-not-found))
-(setq helm-apropos-function-list '(helm-def-source--emacs-commands
-                                   helm-def-source--emacs-functions
-                                   helm-def-source--emacs-variables
-                                   helm-def-source--emacs-faces))
-;; List of times to show in helm-world-time
-(setq display-time-world-list '(("Asia/Shanghai" "Shanghai")
-                                ("PST8PDT" "Mountain View")
-                                ("Europe/London" "London")
-                                ("Australia/Sydney" "Sydney")))
+(setq helm-display-header-line          nil
+      helm-echo-input-in-header-line    nil
+      helm-always-two-windows           nil
+      helm-split-window-in-side-p       t
+      helm-candidate-number-limit       100
+      helm-input-idle-delay             0
+      helm-tramp-verbose                0
+      helm-move-to-line-cycle-in-source t
+      helm-mini-default-sources         '(helm-source-buffers-list
+                                          helm-source-recentf
+                                          helm-source-bookmarks
+                                          helm-source-buffer-not-found)
+      helm-apropos-function-list        '(helm-def-source--emacs-commands
+                                          helm-def-source--emacs-functions
+                                          helm-def-source--emacs-variables
+                                          helm-def-source--emacs-faces)
+      display-time-world-list           '(("Asia/Shanghai" "Shanghai")
+                                          ("PST8PDT" "Mountain View")
+                                          ("Europe/London" "London")
+                                          ("Australia/Sydney" "Sydney")))
 
 ;;https://github.com/ptrv/helm-smex
 (require 'helm-smex)
 (require 'helm-etags-plus)
 
+;;https://github.com/emacs-helm/helm-descbinds
 (require 'helm-descbinds)
 (helm-descbinds-mode)
 

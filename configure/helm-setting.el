1e87fbe1774acfe677be53a663a7976606a183f2
Zachary Zhou <zachary.zhou@hotmail.com>
2018-11-14 08:28:30 +0800

    more for helm swoop setting

--- configure/helm-setting.el
+++ configure/helm-setting.el
@@ -43,6 +43,14 @@
 
 ;;https://github.com/ShingoFukuyama/helm-swoop
 (require 'helm-swoop)
+(setq helm-multi-swoop-edit-save t
+      helm-swoop-split-with-multiple-windows t
+      helm-swoop-split-direction 'split-window-horizontally
+      helm-swoop-pre-input-function (lambda () "")
+      helm-swoop-speed-or-color nil)
+
+(define-key isearch-mode-map (kbd "M-i") 'helm-swoop-from-isearch)
+(define-key helm-swoop-map (kbd "M-i") 'helm-multi-swoop-all-from-helm-swoop)
 (define-key helm-swoop-map (kbd "C-r") 'helm-previous-line)
 (define-key helm-swoop-map (kbd "C-s") 'helm-next-line)
 (define-key helm-multi-swoop-map (kbd "C-r") 'helm-previous-line)

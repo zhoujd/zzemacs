da95e2ca0bcf4e64e0688dcbfe51ae459c5bf5fe
Zachary Zhou <zachary.zhou@hotmail.com>
2018-11-28 15:45:44 +0800

    more for calendar

--- configure/common-setting.el
+++ configure/common-setting.el
@@ -273,6 +273,7 @@
 (setq appt-display-format 'window)
 
 ;;chinese-calendar
+(setq calendar-week-start-day 7)
 (setq chinese-calendar-celestial-stem
       ["甲" "乙" "丙" "丁" "戊" "己" "庚" "辛" "壬" "癸"])
 (setq chinese-calendar-terrestrial-branch

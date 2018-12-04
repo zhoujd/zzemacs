4ed5ee2ad9d253e47ab9266350f208606082799b
Zachary Zhou <zachary.zhou@hotmail.com>
2018-12-04 09:21:43 +0800

    more script for apt reset

--- libexec/emacs-script.el
+++ libexec/emacs-script.el
@@ -1,12 +1,12 @@
 #!/usr/bin/emacs --script
 ;;;http://ergoemacs.org/emacs/emacs.html
 
-(message "zzemacs setup script start ...")
-(message "zzemacs system-type:%s system-name:%s" system-type system-name)
+(message "emacs script start ...")
+(message "system-type:%s system-name:%s" system-type system-name)
 
 ;;print argument is setting
 (when argv
   (message "input argument are %s" argv))
 
 
-(message "zzemacs setup script end ...")
+(message "emacs script end ...")

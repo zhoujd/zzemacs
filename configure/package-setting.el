019271e75d539e0fbfc2dbed153fed138c1e39b5
Zachary Zhou <zachary.zhou@hotmail.com>
2018-12-12 20:34:18 +0800

    add for emacs proxy

--- configure/package-setting.el
+++ configure/package-setting.el
@@ -1,10 +1,16 @@
 ;;;; package-setting.el
 
 (require 'package)
-(add-to-list 'package-archives '("melpa" . "http://melpa.org/packages/"))
-(add-to-list 'package-archives '("melpa-stable" . "https://stable.melpa.org/packages/"))
+(add-to-list 'package-archives
+             '("melpa" . "http://melpa.org/packages/"))
+(add-to-list 'package-archives
+             '("melpa-stable" . "https://stable.melpa.org/packages/"))
 
-(package-initialize)
+;; emacs proxy
+;(setq url-proxy-services
+;   '(("no_proxy" . "^\\(localhost\\|10\\..*\\|192\\.168\\..*\\)")
+;     ("http" . "proxy.com:8080")
+;     ("https" . "proxy.com:8080")))
 
 
 (provide 'package-setting)

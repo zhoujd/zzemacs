03adbee7c5e6b3bdf0ec57c681b801a3893d0b12
Zachary Zhou <zachary.zhou@hotmail.com>
2018-12-30 14:25:11 +0800

    more for path

--- etc/profile.d/03-path.sh
+++ etc/profile.d/03-path.sh
@@ -3,7 +3,7 @@
 SCRIPT_ROOT=$(cd $(dirname "${BASH_SOURCE[0]}") && pwd)
 ZZEMACS_HOME=$(cd $SCRIPT_ROOT/../.. && pwd)
 
-add-to-path() {
+add-path() {
     for ARG in "$@"
     do
         if [ -d "$ARG" ] && [[ ":$PATH:" != *":$ARG:"* ]]; then
@@ -12,7 +12,25 @@ add-to-path() {
     done
 }
 
-add-to-path \
+add-ldpath() {
+    for ARG in "$@"
+    do
+        if [ -d "$ARG" ] && [[ ":$LD_LIBRARY_PATH:" != *":$ARG:"* ]]; then
+            LD_LIBRARY_PATH="$ARG${LD_LIBRARY_PATH:+":$LD_LIBRARY_PATH"}"
+        fi
+    done
+}
+
+add-pkgpath() {
+    for ARG in "$@"
+    do
+        if [ -d "$ARG" ] && [[ ":$PKG_CONFIG_PATH:" != *":$ARG:"* ]]; then
+            PKG_CONFIG_PATH="$ARG${PKG_CONFIG_PATH:+":$PKG_CONFIG_PATH"}"
+        fi
+    done
+}
+
+add-path \
     $HOME/local/bin       \
     $ZZEMACS_HOME/bin     \
     $ZZEMACS_HOME/libexec

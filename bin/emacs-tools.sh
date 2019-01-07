85b2acb2aeb2c7ab6118c11340692836bc2b4079
Zachary Zhou <zachary.zhou@hotmail.com>
2019-01-07 17:37:37 +0800

    more for Arch/Manjaro install

--- bin/emacs-tools.sh
+++ bin/emacs-tools.sh
@@ -7,32 +7,39 @@ EMACS_TOOLS_ROOT=`pwd`
 
 
 install_package() {
-    # dectect OS version
-    if [ "$OS_DISTRO" = "SUSE" ]; then
-        sudo zypper install -y cscope
-        sudo zypper install -y texinfo
-    elif [ "$OS_DISTRO" = "Ubuntu" ]; then
-        sudo apt-get install -y cscope
-        sudo apt-get install -y texinfo
-        sudo apt-get install -y markdown
-        sudo apt-get install -y w3m
-        sudo apt-get install -y silversearcher-ag
-    elif [ "$OS_DISTRO" = "CentOS" ]; then
-        sudo yum install -y cscope
-        sudo yum install -y texinfo
-    elif [ "$OS_DISTRO" = "Fedora" ]; then
-        sudo dnf install -y cscope
-        sudo dnf install -y texinfo
-        ## If want to install emacs with dnf tool
-        #sudo dnf install -y emacs emacs-auto-complete emacs-auto-complete-el
-    elif [ "$OS_DISTRO" = "FreeBSD" ]; then
-        sudo pkg_add -r w3m
-        sudo pkg_add -r cscope
-    elif [ "$OS_DISTRO" = "Arch" ]; then
-        sudo pacman -S tk cscope w3m
-    else
-        echo "You are about to install on a non supported linux distribution."
-    fi
+    case "$OS_DISTRO" in
+        "SUSE" )
+            sudo zypper install -y cscope
+            sudo zypper install -y texinfo
+            ;;
+        "Ubuntu" )
+            sudo apt-get install -y cscope
+            sudo apt-get install -y texinfo
+            sudo apt-get install -y markdown
+            sudo apt-get install -y w3m
+            sudo apt-get install -y silversearcher-ag
+            ;;
+        "CentOS" )
+            sudo yum install -y cscope
+            sudo yum install -y texinfo
+            ;;
+        "Fedora" )
+            sudo dnf install -y cscope
+            sudo dnf install -y texinfo
+            ## If want to install emacs with dnf tool
+            #sudo dnf install -y emacs emacs-auto-complete emacs-auto-complete-el
+            ;;
+        "FreeBSD" )
+            sudo pkg_add -r w3m
+            sudo pkg_add -r cscope
+            ;;
+        "Arch" | "Manjaro" )
+            sudo pacman -S tk cscope w3m
+            ;;
+        t )
+            echo "You are about to install on a non supported linux distribution."
+            ;;
+    esac
 }
 
 echo "Install package for emacs on $OS_DISTRO"

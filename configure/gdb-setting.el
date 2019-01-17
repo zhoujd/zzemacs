31a2b98c7b627cf209822c122bbc23b464e687ab
Zachary Zhou <zachary.zhou@hotmail.com>
2019-01-17 16:11:17 +0800

    more for tramp gdb

--- configure/gdb-setting.el
+++ configure/gdb-setting.el
@@ -27,21 +27,26 @@
  '(lambda ()
     (gud-def gud-break-main "break main" nil "Set breakpoint at main.")))
 
-;;for cdb.exe debug on windows
-;;Use kd -k <connection string> instead of cdb <your program> e.g. M-x cdb kd -k com:port=com1
-;;http://www.microsoft.com/whdc/devtools/debugging/default.mspx
-;;A word for WinDbg
-;;http://mtaulty.com/communityserver/blogs/mike_taultys_blog/archive/2004/08/03/4656.aspx
-;;http://www.windbg.org/
-;;http://www.windbg.info/
-;;command line: "windbg.exe" -y "Debug;for-windbg\Debug" -srcpath "for-windbg" Debug\for-windbg.exe args
-;;start folder: D:\work\for-windbg
-;;Symbol Server (Microsoft): srv*c:\mss*http://msdl.microsoft.com/download/symbols
-;(when-ms-windows  
-; (load-library "cdb-gud.el")
-; (defun zz:cdb-mode-hook ()
-;   (gud-def cdb-bc  "bc * "  nil "Breakpoint clean all."))
-; (add-hook 'cdb-mode-hook 'zz:cdb-mode-hook))
+;;GDB through TRAMP
+(defadvice find-file-noselect (before ad-find-file-noselect)
+  (set 'filename (if (file-exists-p filename) (expand-file-name filename)
+                   (gud-file-name filename))))
+
+(defadvice gdb-get-location (around ad-gdb-get-location activate)
+  (let ()
+    (ad-activate 'find-file-noselect) ad-do-it
+    (ad-deactivate 'find-file-noselect)))
+
+(defadvice gdb-info-breakpoints-custom
+  (around ad-gdb-info-breakpoints-custom activate)
+  (let ()
+    (ad-activate 'find-file-noselect) ad-do-it
+    (ad-deactivate 'find-file-noselect)))
+
+(defadvice gdb-goto-breakpoint (around ad-gdb-goto-breakpoint activate)
+  (let ()
+    (ad-activate 'find-file-noselect) ad-do-it
+    (ad-deactivate 'find-file-noselect)))
 
 
 (provide 'gdb-setting)

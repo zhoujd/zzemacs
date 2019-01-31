689f213d6b75c725a662b186576108853a198051
Zachary Zhou <zachary.zhou@hotmail.com>
2019-01-31 12:30:39 +0800

    add git http proxy setup script

---
+++ bin/git-proxy-setup.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+if [ $# != 2 ] ; then
+    echo "Usage: `basename $0` host port"
+    exit 1
+fi
+
+git config --global http.proxy http://$1:$2
+git config --global https.proxy https://$1:$2
+
+

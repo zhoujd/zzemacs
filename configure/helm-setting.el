f63687e5359d9e3331683c9352f57054778ce856
Zachary Zhou <zachary.zhou@hotmail.com>
2019-02-13 23:04:39 +0800

    more for shell buffer helm

--- configure/helm-setting.el
+++ configure/helm-setting.el
@@ -83,15 +83,21 @@
 
 (setq helm-source-shell-list
       (helm-build-sync-source "Shell buffers"
-        :candidates
-        (lambda ()
-          (mapcar #'buffer-name
-                  (cl-remove-if-not
-                   (lambda (buf)
-                     (with-current-buffer buf
-                       (eq major-mode 'shell-mode)))
-                   (buffer-list))))
-        :action '(("Switch to buffer" . switch-to-buffer))))
+        :candidates (lambda ()
+                      (mapcar 
+                       (lambda (buf)
+                         (format "%-20s %s"
+                                 (buffer-name buf)
+                                 (with-current-buffer (buffer-name buf)
+                                   default-directory)))
+                       (cl-remove-if-not
+                        (lambda (buf)
+                          (with-current-buffer buf
+                            (eq major-mode 'shell-mode)))
+                        (buffer-list))))
+        :action '(("Switch to buffer" . (lambda (select)
+                                          (switch-to-buffer (car (split-string select)))))
+                  )))
 
 (setq helm-source-term-list
       (helm-build-sync-source "Multi-term buffers"

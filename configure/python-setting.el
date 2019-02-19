9bebc2b5e463ece912951b1f2d9c279f13c8974f
Zachary Zhou <zachary.zhou@hotmail.com>
2019-02-19 14:25:42 +0800

    more for ropemacs and bookmarkplus

--- configure/python-setting.el
+++ configure/python-setting.el
@@ -3,6 +3,27 @@
 
 (zz-load-path "site-lisp/python-mode")
 
+;;pymacs & ropemacs
+(require 'pymacs)
+(autoload 'pymacs-apply "pymacs")
+(autoload 'pymacs-call  "pymacs")
+(autoload 'pymacs-eval  "pymacs" nil t)
+(autoload 'pymacs-exec  "pymacs" nil t)
+(autoload 'pymacs-load  "pymacs" nil t)
+
+;;ropemacs conflicts with bookmarkplus on 'C-x p'
+(global-unset-key (kbd "C-x p"))
+(pymacs-load "ropemacs" "rope-")
+(setq ropemacs-enable-autoimport t)
+
+;;pymacs with auto complete
+(ac-ropemacs-initialize)
+(ac-ropemacs-setup)
+
+(add-hook 'python-mode-hook
+          (lambda ()
+            (add-to-list 'ac-sources 'ac-source-ropemacs)))
+
 (require 'pycomplete)
 (setq auto-mode-alist (cons '("\\.py$" . python-mode) auto-mode-alist))
 

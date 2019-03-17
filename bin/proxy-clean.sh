016b994953d49a9fa08870c2f1dbe968f0b3b0a1
Zachary Zhou <zachary.zhou@hotmail.com>
2019-03-18 07:10:57 +0800

    add proxy clean script

---
+++ bin/proxy-clean.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+#set -x
+
+## check os
+case "$OS" in
+    "Windows_NT" )
+        echo "Setup proxy on Windows"
+        rm -f $HOME/.bashrc.d/99-proxy.sh
+        ;;
+    * )
+        echo "Setup proxy on Linux"
+        if [ $EUID -ne 0 ]; then
+            echo "You must be a root user" 2>&1
+            exit 1
+        fi
+
+        rm -f /etc/profile.d/zz-proxy.sh
+        ;;
+esac

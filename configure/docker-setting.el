232b97108d8f266f47b4f6bcd944c2a435c309f7
Zachary Zhou <zachary.zhou@hotmail.com>
2019-04-29 19:56:24 +0800

    format perl code

--- configure/docker-setting.el
+++ configure/docker-setting.el
@@ -22,7 +22,19 @@
   "(tramp-completion-handle-file-name-all-completions \"\" \"/docker:\" returns
     a list of active Docker container names, followed by colons."
   (if (equal (ad-get-arg 1) "/docker:")
-      (let* ((dockernames-raw (shell-command-to-string "docker ps | perl -we 'use strict; $_ = <>; m/^(.*)NAMES/ or die; my $offset = length($1); while(<>) {substr($_, 0, $offset, q()); chomp; for(split m/\\W+/) {print qq($_:\n)} }'"))
+      (let* ((dockernames-raw (shell-command-to-string "docker ps | perl -we '
+use strict;
+$_ = <>;
+m/^(.*)NAMES/ or die;
+my $offset = length($1);
+while(<>) {
+  substr($_, 0, $offset, q());
+  chomp;
+  for(split m/\\W+/) {
+    print qq($_:\n)
+  }
+}
+'"))
              (dockernames (cl-remove-if-not
                            #'(lambda (dockerline) (string-match ":$" dockerline))
                            (split-string dockernames-raw "\n"))))

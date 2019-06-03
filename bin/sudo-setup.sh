5f7a4af8203c8e621d571ef90015be0d87608544
Zachary Zhou <zachary.zhou@hotmail.com>
2019-06-03 15:11:14 +0800

    more for sudo

--- bin/sudo-setup.sh
+++ bin/sudo-setup.sh
@@ -10,7 +10,7 @@ USER_NAME=zhoujd
 
 cat > /etc/sudoers.d/${USER_NAME} <<EOF
 ## ${USER_NAME}
-Defaults env_keep+="http_proxy https_proxy ftp_proxy no_proxy socks_proxy"
+Defaults env_keep+="http_proxy https_proxy ftp_proxy no_proxy socks_proxy all_proxy"
 ${USER_NAME} ALL=(ALL) ALL
 EOF
 

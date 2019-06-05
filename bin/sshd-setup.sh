7af9b50886a6818901431bed888e46e3e4d95d50
Zachary Zhou <zachary.zhou@hotmail.com>
2019-06-05 08:49:25 +0800

    bin: more for sshd setup

--- bin/sshd-setup.sh
+++ bin/sshd-setup.sh
@@ -6,11 +6,22 @@
 ## X11DisplayOffset 10
 ## X11UseLocalhost yes
 
-sudo sed -i 's/#AllowTcpForwarding yes/AllowTcpForwarding yes/g' /etc/ssh/sshd_config
-sudo sed -i 's/#X11Forwarding yes/X11Forwarding yes/g' /etc/ssh/sshd_config
-sudo sed -i 's/#X11DisplayOffset 10/X11DisplayOffset 10/g' /etc/ssh/sshd_config
-sudo sed -i 's/#X11UseLocalhost yes/X11UseLocalhost yes/g' /etc/ssh/sshd_config
+sshd_x11() {
+    sudo sed -i 's/#AllowTcpForwarding yes/AllowTcpForwarding yes/g' /etc/ssh/sshd_config
+    sudo sed -i 's/#X11Forwarding yes/X11Forwarding yes/g' /etc/ssh/sshd_config
+    sudo sed -i 's/#X11DisplayOffset 10/X11DisplayOffset 10/g' /etc/ssh/sshd_config
+    sudo sed -i 's/#X11UseLocalhost yes/X11UseLocalhost yes/g' /etc/ssh/sshd_config
+}
 
-sudo systemctl restart sshd
+sshd_root() {
+    sudo sed -i 's/#PermitRootLogin prohibit-password/PermitRootLogin yes/g' /etc/ssh/sshd_config
+}
+
+sshd_restart() {
+    sudo systemctl restart sshd
+}
+
+sshd_x11
+sshd_restart
 
 echo "setup /etc/ssh/sshd_config done"

f76e44f1eba0a5df42f495382201e913de045d97
Zachary Zhou <zachary.zhou@hotmail.com>
2019-06-12 22:47:38 +0800

    format code

--- bin/meson-setup.sh
+++ bin/meson-setup.sh
@@ -2,9 +2,17 @@
 
 ## https://mesonbuild.com/
 ## https://mesonbuild.com/Tutorial.html
-# $ meson builddir
-# $ cd builddir
+# $ cd /path/to/source/root
+# $ meson builddir && cd builddir
 # $ ninja
+# $ ninja test
+
+## https://mesonbuild.com/Quick-guide.html
+# $ cd /path/to/source/root
+# $ meson --prefix /usr --buildtype=plain builddir -Dc_args=... -Dcpp_args=... -Dc_link_args=... -Dcpp_link_args=...
+# $ ninja -v -C builddir
+# $ ninja -C builddir test
+# $ DESTDIR=/path/to/staging/root ninja -C builddir install
 
 sudo apt-get install python3 python3-pip ninja-build
 pip3 install --user meson

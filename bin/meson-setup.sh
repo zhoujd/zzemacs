a2867632823d1bd40603ca864e703de97ce05aa7
Zachary Zhou <zachary.zhou@hotmail.com>
2019-06-12 22:42:00 +0800

    more for meson build

--- bin/meson-setup.sh
+++ bin/meson-setup.sh
@@ -1,5 +1,11 @@
 #!/bin/bash
 
+## https://mesonbuild.com/
+## https://mesonbuild.com/Tutorial.html
+# $ meson builddir
+# $ cd builddir
+# $ ninja
+
 sudo apt-get install python3 python3-pip ninja-build
 pip3 install --user meson
 

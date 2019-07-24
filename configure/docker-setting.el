0bb4da8a6c7995e28bd6dfaaa1d85aa3a70c4127
Zachary Zhou <zachary.zhou@hotmail.com>
2019-07-24 22:04:05 +0800

    more for docker tramp

--- configure/docker-setting.el
+++ configure/docker-setting.el
@@ -1,26 +1,27 @@
 ;;;; docker-setting.el
 
 ;;docker-tramp
-;;C-x C-f /ssh:user@host|docker:
+;;https://www.emacswiki.org/emacs/TrampAndDocker
+;;https://afsmnghr.github.io/emacs/tramp-with-ssh-and-docker/
+;;sudo apt install docker-runc
+;;C-x C-f /docker:container:/etc/passwd RET ;; simple use
+;;C-x C-f /ssh:user@remotehost|docker:user@container:/etc/passwd RET
 (require 'docker-tramp)
+(push (cons "docker"
+            '((tramp-login-program "docker")
+              (tramp-login-args (("exec" "-it") ("%h") ("/bin/bash")))
+              (tramp-remote-shell "/bin/sh")
+              (tramp-remote-shell-args ("-i") ("-c"))))
+      tramp-methods)
+
 (defadvice tramp-completion-handle-file-name-all-completions
     (around dotemacs-completion-docker activate)
-  "(tramp-completion-handle-file-name-all-completions \"\" \"/docker:\" returns
-    a list of active Docker container names, followed by colons."
+  "(tramp-completion-handle-file-name-all-completions \"\" \"/docker:\"
+    returns a list of active Docker container names, followed by colons."
   (if (equal (ad-get-arg 1) "/docker:")
-      (let* ((dockernames-raw (shell-command-to-string "docker ps | perl -we '
-use strict;
-$_ = <>;
-m/^(.*)NAMES/ or die;
-my $offset = length($1);
-while(<>) {
-  substr($_, 0, $offset, q());
-  chomp;
-  for(split m/\\W+/) {
-    print qq($_:\n)
-  }
-}
-'"))
+      (let* ((dockernames-raw
+              (shell-command-to-string
+               "docker ps | awk '$NF != \"NAMES\" { print $NF \":\" }'"))
              (dockernames (cl-remove-if-not
                            #'(lambda (dockerline) (string-match ":$" dockerline))
                            (split-string dockernames-raw "\n"))))

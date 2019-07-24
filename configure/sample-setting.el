316924787ef3a31149eef86d1db65786be575437
Zachary Zhou <zachary.zhou@hotmail.com>
2019-07-24 22:54:06 +0800

    add sudo edit current file

--- configure/sample-setting.el
+++ configure/sample-setting.el
@@ -297,6 +297,34 @@
   (iimage-mode t)
   (message "Refreshed images"))
 
+;;edit current file with sudo
+(defun sudo-edit-current-file ()
+  (interactive)
+  (let ((my-file-name) ; fill this with the file to open
+        (position))    ; if the file is already open save position
+    (if (equal major-mode 'dired-mode) ; test if we are in dired-mode
+        (progn
+          (setq my-file-name (dired-get-file-for-visit))
+          (find-alternate-file (prepare-tramp-sudo-string my-file-name)))
+        (setq my-file-name (buffer-file-name)
+              position (point))
+        (find-alternate-file (prepare-tramp-sudo-string my-file-name))
+        (goto-char position))))
+
+(defun prepare-tramp-sudo-string (tempfile)
+  (if (file-remote-p tempfile)
+      (let ((vec (tramp-dissect-file-name tempfile)))
+
+        (tramp-make-tramp-file-name
+         "sudo"
+         (tramp-file-name-user nil)
+         (tramp-file-name-host vec)
+         (tramp-file-name-localname vec)
+         (format "ssh:%s@%s|"
+                 (tramp-file-name-user vec)
+                 (tramp-file-name-host vec))))
+      (concat "/sudo:root@localhost:" tempfile)))
+
 
 (provide 'sample-setting)
 

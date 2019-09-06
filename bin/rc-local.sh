3c3e71188492d3b6f18de2f958b708e0fa50c6f6
Zachary Zhou <zachary.zhou@hotmail.com>
2019-09-06 19:42:59 +0800

    more for rc.local

--- bin/rc-local.sh
+++ bin/rc-local.sh
@@ -2,20 +2,59 @@
 
 rc_local_install() {
     sudo tee /etc/rc.local <<EOF
-#!/bin/sh -e
-## rc.local run as root during boot up
-# run script in /etc/rc.local.d
+#!/bin/sh
+### BEGIN INIT INFO
+# Provides:          rc.local
+# Required-Start:    $all
+# Required-Stop:     $all
+# Default-Start:     2 3 4 5
+# Default-Stop:      0 1 6
+# Short-Description: Run /etc/rc.local if it exist
+### END INIT INFO
 
-rc_local_log=/var/log/zz-rc.log
-rm -f \$rc_local_log
-for i in /etc/rc.local.d/* ; do
-    if [ -x "\$i" ]; then
-       "\$i"
-       echo "\$(date) \$i [\$?]" >> \$rc_local_log
-    fi
-done
+PATH=/sbin:/usr/sbin:/bin:/usr/bin
 
-exit 0
+. /lib/init/vars.sh
+. /lib/lsb/init-functions
+
+do_start() {
+    rc_local_log=/var/log/zz-rc.log
+    rm -f \$rc_local_log
+    for i in /etc/rc.local.d/* ; do
+        if [ -x "\$i" ]; then
+           "\$i start"
+           echo "\$(date) \$i $* [\$?]" >> \$rc_local_log
+        fi
+    done
+}
+
+do_stop() {
+    rc_local_log=/var/log/zz-rc.log
+    rm -f \$rc_local_log
+    for i in /etc/rc.local.d/* ; do
+        if [ -x "\$i" ]; then
+           "\$i stop"
+           echo "\$(date) \$i $* [\$?]" >> \$rc_local_log
+        fi
+    done
+}
+
+case "$1" in
+    start)
+	do_start
+        ;;
+    restart|reload|force-reload)
+        echo "Error: argument '$1' not supported" >&2
+        exit 3
+        ;;
+    stop)
+        do_stop
+        ;;
+    *)
+        echo "Usage: $0 start|stop" >&2
+        exit 3
+        ;;
+esac
 EOF
     sudo chmod +x /etc/rc.local
     sudo mkdir -p /etc/rc.local.d

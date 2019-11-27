80250747f0002cbec3d8cca9bbc294138fd1e86f
Zachary Zhou <zachary.zhou@hotmail.com>
2019-11-27 21:24:27 +0800

    add more for complete

--- configure/complete-setting.el
+++ configure/complete-setting.el
@@ -32,7 +32,31 @@
                                             try-complete-file-name)))
     (call-interactively 'hippie-expand)))
 
+;; https://www.emacswiki.org/emacs/InsertFileName
+(defun zz:insert-file-name (filename &optional args)
+  "Insert name of file FILENAME into buffer after point.
+
+  Prefixed with \\[universal-argument], expand the file name to
+  its fully canocalized path.  See `expand-file-name'.
+
+  Prefixed with \\[negative-argument], use relative path to file
+  name from current directory, `default-directory'.  See
+  `file-relative-name'.
+
+  The default with no prefix is to insert the file name exactly as
+  it appears in the minibuffer prompt."
+  ;; Based on insert-file in Emacs -- ashawley 20080926
+  (interactive "*fInsert file name: \nP")
+  (cond ((eq '- args)
+         (insert (expand-file-name filename)))
+        ((not (null args))
+         (insert filename))
+        (t
+         (insert (file-relative-name filename)))))
+
+;; bind it
 (defkeys-map global-map
+  ((kbd "C-z /") 'zz:insert-file-name)
   ((kbd "C-M-/") 'zz:expand-file-name-at-point))
 
 (defun zz:indent-or-complete ()

64d2898c62effaebbf5c6711f3ca368317fd8c3f
Zachary Zhou <zachary.zhou@hotmail.com>
2019-12-12 09:32:45 +0800

    add more for ubuntu tools

--- bin/linux-tools.sh
+++ bin/linux-tools.sh
@@ -24,6 +24,9 @@ install_package_ubuntu() {
     sudo apt install -y connect-proxy
     sudo apt install -y cups-pdf
     sudo apt install -y git-email
+    sudo apt install -y meld
+    sudo apt install -y evolution-ews
+    sudo apt install -y dos2unix
 }
 
 ##package for centos

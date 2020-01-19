aacf874373543643dee00a8ab0de6b3ad720741e
Zachary Zhou <zchrzhou@gmail.com>
2020-01-19 09:18:37 +0800

    thirdparty: more for plsense init

--- third-party/perl/install.sh
+++ third-party/perl/install.sh
@@ -5,6 +5,6 @@ echo "For perl develop start ..."
 
 echo "==>1 install plsense"
 sudo apt install -y plsense
-echo "==>Please run 'plsense' to initalize it"
+echo "==>Please run 'plsense' or 'plsense config' to initalize it"
 
 echo "For perl develop end ..."

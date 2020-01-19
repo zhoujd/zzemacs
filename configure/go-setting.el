b8fd57116e90f61d7ef8e2ae3f7f1187ea9b446f
Zachary Zhou <zchrzhou@gmail.com>
2020-01-19 09:56:34 +0800

    add company-go support

--- configure/go-setting.el
+++ configure/go-setting.el
@@ -1,9 +1,14 @@
 ;;;;go programe setting
 ;;;
 
+;; go-mode
 (zz-load-path "site-lisp/go-mode")
 (autoload 'go-mode "go-mode" nil t)
 (add-to-list 'auto-mode-alist '("\\.go\\'" . go-mode))
+
+;; company-go
+(require 'company-go)
+
 
 (provide 'go-setting)
 

9e2d926143751f5b7fd925c6c73619254031fa63
Zachary Zhou <zchrzhou@gmail.com>
2020-01-22 10:41:15 +0800

    add helm-company

--- configure/helm-setting.el
+++ configure/helm-setting.el
@@ -112,6 +112,12 @@
               ((kbd "M-s M-f") 'helm-comint-prompts)
               )))
 
+(autoload 'helm-company "helm-company")
+(eval-after-load 'company
+  '(progn
+     (define-key company-mode-map (kbd "C-:") 'helm-company)
+     (define-key company-active-map (kbd "C-:") 'helm-company)))
+
 
 (provide 'helm-setting)
 

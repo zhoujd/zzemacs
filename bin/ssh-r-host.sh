63a1968f2c782420fff64ff46bd88961bb4ef860
Zachary Zhou <zchrzhou@gmail.com>
2020-04-26 13:59:39 +0800

    add more for ssh remove host

---
+++ bin/ssh-r-host.sh
@@ -0,0 +1,10 @@
+#!/bin/bash 
+
+if [ $# != 1 ]; then
+    echo "Usage: $0 <host_ip|host_name>"
+    exit 1
+fi
+
+ssh-keygen -f $HOME/.ssh/known_hosts -R $1
+
+echo "Remove $1 from $HOME/.ssh/known_hosts done."

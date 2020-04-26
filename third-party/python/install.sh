79167b576e65f4a15701c9ea525943fe84fcd39e
Zachary Zhou <zchrzhou@gmail.com>
2020-04-26 09:24:28 +0800

    add more for pip3

--- third-party/python/install.sh
+++ third-party/python/install.sh
@@ -7,12 +7,15 @@ SCRIPT_ROOT=$(cd $(dirname "${BASH_SOURCE[0]}") && pwd)
 ##http://archive.ipython.org/release/
 ##https://pypi.python.org/pypi/pyreadline
 
+#pip install pip -U
+#pip config set global.index-url https://pypi.tuna.tsinghua.edu.cn/simple
+
 PIP_PAR="--timeout 60"
 
 echo "For python develop start ..."
 
 py2_deps() {
-    pip install $PIP_PAR -r $SCRIPT_ROOT/py2.txt
+    pip2 install $PIP_PAR -r $SCRIPT_ROOT/py2.txt
 }
 
 py3_deps() {

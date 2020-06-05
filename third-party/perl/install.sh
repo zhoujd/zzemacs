5dfe10a6ebbd1b99dcc855f64a76ae2acc68fae0
Zachary Zhou <zchrzhou@gmail.com>
2020-06-05 11:42:29 +0800

    more for plsense in emacs

--- third-party/perl/install.sh
+++ third-party/perl/install.sh
@@ -7,6 +7,21 @@ echo "For perl develop start ..."
 
 echo "==>1 install plsense"
 sudo apt install -y plsense
-echo "==>Please run 'plsense' or 'plsense config' to initalize it"
+
+echo "==>2 install \$HOME/.plsense"
+cat > $HOME/.plsense <<EOF
+cachedir=$HOME/.plsense.d
+clean-env=0
+logfile=
+loglevel=
+maxtasks=20
+perl=perl
+perldoc=perldoc
+port1=33333
+port2=33334
+port3=33335
+EOF
+
+echo "==>Run 'plsense' or 'plsense config' to initalize it"
 
 echo "For perl develop end ..."

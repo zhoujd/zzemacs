e3a3b5ae45f13ae9ded94301a1da220d7108b05d
Zachary Zhou <zchrzhou@gmail.com>
2020-07-15 22:27:02 +0800

    more for keymap

--- bin/keymap-setup.sh
+++ bin/keymap-setup.sh
@@ -1,6 +1,8 @@
 #!/bin/bash
 
 echo "Set capslk to ctrl"
+
 sudo sed -i 's/XKBOPTIONS=.*/XKBOPTIONS="ctrl:nocaps"/' /etc/default/keyboard 
+setxkbmap -option "ctrl:nocaps"
 
 echo "Set capslk to ctrl done"

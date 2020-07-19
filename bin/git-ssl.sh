a108f9e8bd864fc893a1aed5a79adc7ad4fb1085
Zachary Zhou <zchrzhou@gmail.com>
2020-07-19 18:42:37 +0800

    clean code

--- bin/git-ssl.sh
+++ bin/git-ssl.sh
@@ -5,7 +5,7 @@ install_ca_org() {
     echo "install cacert.org"
     local ca_target=/usr/local/share/ca-certificates/cacert.org
     sudo rm -rf $ca_target
-    sudo mkdir $ca_target
+    sudo mkdir -p $ca_target
     sudo wget -P $ca_target \
          http://www.cacert.org/certs/root.crt \
          http://www.cacert.org/certs/class3.crt
@@ -13,9 +13,8 @@ install_ca_org() {
 
 install_ca_bundle() {
     echo "install bundle"
-    local ca_bundle=/usr/local/share/ca-certificates/cabundle
-    sudo rm -rf $ca_bundle
-    sudo mkdir $ca_bundle
+    local ca_bundle=/usr/local/share/ca-certificates
+    sudo mkdir -p $ca_bundle
     sudo cp ~/zzemacs/misc/ca-bundle.crt $ca_bundle
 }
 

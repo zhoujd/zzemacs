4bd815836b1757f5791b060b2ce5c7ade177b531
Zachary Zhou <zchrzhou@gmail.com>
2020-07-19 18:53:15 +0800

    format code

--- bin/git-ssl.sh
+++ bin/git-ssl.sh
@@ -5,19 +5,20 @@ MISC_ROOT=$(cd $SCRIPT_ROOT/../misc && pwd)
 
 install_ca_org() {
     echo "install cacert.org"
-    local ca_target=/usr/local/share/ca-certificates/cacert.org
-    sudo rm -rf $ca_target
-    sudo mkdir -p $ca_target
-    sudo wget -P $ca_target \
+    local ca_cert=/usr/local/share/ca-certificates/cacert.org
+    sudo rm -rf $ca_cert
+    sudo mkdir -p $ca_cert
+    sudo wget -P $ca_cert \
          http://www.cacert.org/certs/root.crt \
          http://www.cacert.org/certs/class3.crt
 }
 
-install_ca_bundle() {
+install_ca_git() {
     echo "install bundle"
-    local ca_bundle=/usr/local/share/ca-certificates
-    sudo mkdir -p $ca_bundle
-    sudo cp $MISC_ROOT/ca-bundle.crt $ca_bundle
+    local ca_git=/usr/local/share/ca-certificates/git-certs
+    sudo rm -rf $ca_git
+    sudo mkdir -p $ca_git
+    sudo cp $MISC_ROOT/ca-bundle.crt $ca_git
 }
 
 update_ca() {
@@ -29,7 +30,7 @@ update_ca() {
 }
 
 install_ca_org
-install_ca_bundle
+install_ca_git
 update_ca
 
 echo "ssl update done"

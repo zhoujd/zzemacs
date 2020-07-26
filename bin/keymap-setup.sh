ae02d92ea57e0b5bb050d1aa8364113278c657b5
Zachary Zhou <zchrzhou@gmail.com>
2020-07-26 23:30:10 +0800

    more for keyboard

--- bin/keymap-setup.sh
+++ bin/keymap-setup.sh
@@ -2,6 +2,9 @@
 
 echo "Set capslk to ctrl"
 
+### https://unix.stackexchange.com/questions/9098/map-windows-key-on-keyboard-to-ctrl
+## grep ctrl /usr/share/X11/xkb/rules/evdev.lst
+
 keymap_ubuntu() {
     echo "on Ubuntu"
     sudo sed -i 's/XKBOPTIONS=.*/XKBOPTIONS="ctrl:nocaps"/' /etc/default/keyboard

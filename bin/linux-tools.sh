af4ba1c21fa4277d3f65f7b43ab74677ff937f1b
Zachary Zhou <zchrzhou@gmail.com>
2020-08-04 13:41:30 +0800

    more for package on Centos

--- bin/linux-tools.sh
+++ bin/linux-tools.sh
@@ -44,6 +44,10 @@ install_package_centos() {
     sudo yum install -y vim
     sudo yum install -y nano
     sudo yum install -y git gitk
+    sudo yum install -y wireless-tools
+    sudo yum install -y python3 python3-pip python2-pip
+    sudo yum install -y rxvt-unicode sakura
+    sudo yum install -y alsa-tools alsa-utils
 }
 
 ##package for fedora

3908269ea0b33e7cf30890bdaefc9e26ad2fa836
Zachary Zhou <zachary.zhou@intel.com>
2021-03-31 08:25:04 +0800

    add more for kube ac

--- libexec/kube-ac-setup.sh
+++ libexec/kube-ac-setup.sh
@@ -3,8 +3,21 @@
 kube_ac() {
     mkdir -p ~/.bashrc.d
     cat > ~/.bashrc.d/zz-kube.sh <<EOF
-alias k=kubectl
-source <(kubectl completion bash | sed s/kubectl/k/g)
+### zz-kube.sh
+
+## kubectl
+if command -v kubectl &>/dev/null; then
+    alias k=kubectl
+    source <(kubectl completion bash)
+    complete -F __start_kubectl k
+fi
+
+## minikube
+if command -v minikube &>/dev/null; then
+    alias m=minikube
+    source <(minikube completion bash)
+    complete -F __start_minikube m
+fi
 EOF
 }
 

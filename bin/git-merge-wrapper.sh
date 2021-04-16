4a9ca854e01bde52380072e38df0b9ce0577cf2a
Zachary Zhou <zachary.zhou@intel.com>
2021-04-16 10:33:25 +0800

    more for merge with meld

--- bin/git-merge-wrapper.sh
+++ bin/git-merge-wrapper.sh
@@ -8,21 +8,21 @@
 
 ## mergetool selects
 ## http://www.scootersoftware.com/support.php?c=kb_vcs.php
+## https://lukas.zapletalovi.com/2012/09/three-way-git-merging-with-meld.html
 ## http://www.perforce.com/perforce/products/merge.html
 ## http://meldmerge.org/
 
 main() {
     if [ "$OS" = "Windows_NT" ] ; then
         MERGE_TOOL_0="bcompare $*"
-        MERGE_TOOL_1="p4merge $*"
-        MERGE_TOOL_2="vimmerge $*"
+        MERGE_TOOL_1="meld $2 $1 $3 -o $4 --diff $1 $2 --diff $1 $3 --auto-merge"
+        MERGE_TOOL_2="p4merge $*"
 
         MERGE_SELECT=$MERGE_TOOL_0
     else
         MERGE_TOOL_0="bcompare $*"
-        MERGE_TOOL_1="meld $2 $4 $3"
+        MERGE_TOOL_1="meld $2 $1 $3 -o $4 --diff $1 $2 --diff $1 $3 --auto-merge"
         MERGE_TOOL_2="p4merge $*"
-        MERGE_TOOL_3="vimmerge $*"
 
         MERGE_SELECT=$MERGE_TOOL_2
     fi

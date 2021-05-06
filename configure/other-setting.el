27b382eb6452f220a7222ef9509ebce148d2b62d
Zachary Zhou <zchrzhou@gmail.com>
2021-05-06 20:45:16 +0800

    add more for anzu.el

--- configure/other-setting.el
+++ configure/other-setting.el
@@ -336,6 +336,23 @@
           ))
   (add-hook hook 'whitespace-cleanup-mode))
 
+;;https://github.com/emacsorphanage/anzu
+(require 'anzu)
+(global-anzu-mode +1)
+
+(set-face-attribute 'anzu-mode-line nil
+                    :foreground "yellow" :weight 'bold)
+
+(custom-set-variables
+ '(anzu-mode-lighter "")
+ '(anzu-deactivate-region t)
+ '(anzu-search-threshold 1000)
+ '(anzu-replace-threshold 50)
+ '(anzu-replace-to-string-separator " => "))
+
+(global-set-key [remap query-replace] 'anzu-query-replace)
+(global-set-key [remap query-replace-regexp] 'anzu-query-replace-regexp)
+
 
 (provide 'other-setting)
 

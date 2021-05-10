92b9a1cb0872f1807c8c8d8b998a5a1d35f90c84
Zachary Zhou <zchrzhou@gmail.com>
2021-05-10 21:12:35 +0800

    add more for apply keys

--- elisp/apply-keys.el
+++ elisp/apply-keys.el
@@ -1,6 +1,6 @@
 ;;; define keys
 
-;;multi key setting
+;;multi key setting for one map
 (defun apply-keys-to-map (map key-pairs)
   "apply multi key defines"
   (let ((i 0))
@@ -10,6 +10,18 @@
         (when fn
           (define-key map key fn)))
       (setq i (+ i 2)))))
+
+;;multi key setting for multi maps
+(defun apply-keys-to-maps (maps key-pairs)
+  "apply multi key defines"
+  (dolist (map maps)
+    (let ((i 0))
+      (while (< i (length key-pairs))
+        (let ((key (nth i key-pairs))
+              (fn (nth (1+ i) key-pairs)))
+          (when fn
+            (define-key map key fn)))
+        (setq i (+ i 2))))))
 
 
 (provide 'apply-keys)

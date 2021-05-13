a511e26ce2e2beb482c1e145c750e9f329cd757a
Zachary Zhou <zchrzhou@gmail.com>
2021-05-13 20:33:59 +0800

    format and clean code for PS1

--- etc/profile.d/01-common.sh
+++ etc/profile.d/01-common.sh
@@ -6,6 +6,7 @@
 # ls colors setting
 export LS_COLORS=$LS_COLORS:'di=01;33:ln=36'
 
+### color prompt
 ## norm='\[\033[m\]'
 ## bold='\[\033[1m\]'
 ## underline='\[\033[4m\]'
@@ -21,34 +22,24 @@ export LS_COLORS=$LS_COLORS:'di=01;33:ln=36'
 ## lblue='\[\033[1;34m\]'
 ## mangenta='\[\033[1;35m\]'
 ## white='\[\033[1;37m\]'
-
-color_prompt() {
-    local lred='\[\033[1;31m\]'
-    local green='\[\033[0;32m\]'
-    local title='\[\033]0;\w\007\]'
-
-    if [ $(whoami) = 'root' ]; then
-        pmt_str='# '
-        pmt=${lred}${pmt_str}${norm}
-    else
-        pmt_str='$ '
-        pmt=${green}${pmt_str}${norm}
-    fi
-
-    # depend on term type
-    case "$TERM" in
+prompt() {
+    local lgreen='\[\033[1;32m\]'
+    local lblue='\[\033[1;34m\]'
+    local norm='\[\033[m\]'
+    case $TERM in
         xterm* | rxvt* )
-            PS1="${green}\u@\h \W${norm}${pmt}"
+            local title='\[\033]0;\w\007\]'
+            PS1="${lgreen}\u@\h ${lblue}\W${norm}\$ "
             PS1="${title}${PS1}"
             ;;
         eterm* | emacs* )
-            PS1="${green}\u@\h \W${norm}${pmt}"
+            PS1="${lgreen}\u@\h ${lblue}\W${norm}\$ "
             ;;
         dumb* )
-            PS1="\u@\h \W${pmt_str}"
+            PS1="\u@\h \W\$ "
             ;;
     esac
     export PS1
 }
 
-color_prompt
+prompt

7e16b3b0b2c7f4676d304a5ce38ab339cf350112
Zachary Zhou <zchrzhou@gmail.com>
2021-05-14 10:39:23 +0800

    more for custom.el

--- configure/common-setting.el
+++ configure/common-setting.el
@@ -459,7 +459,7 @@
 (defalias 'c 'toggle-case-fold-search)
 
 ;;Configure saved from menu "Save Options"
-(setq-default custom-file "~/.emacs.d/custom.el")
+(setq custom-file "~/.emacs.d/custom.el")
 
 ;;auto refresh remote files
 (custom-set-variables

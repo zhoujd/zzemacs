ecd88ccfed6c221733611073e11d6aa00e95ddbd
Zachary Zhou <zchrzhou@gmail.com>
2021-05-14 22:08:23 +0800

    more for console font

--- configure/common-setting.el
+++ configure/common-setting.el
@@ -82,7 +82,7 @@
     (zz:frame-font (nth 0 zz:en-font-list))
     (if window-system
         (zz:frame-font (nth 0 zz:en-font-list) (nth 0 zz:cn-font-list))
-        (zz:frame-font (nth 0 zz:en-font-list))))
+        (zz:frame-font (nth 3 zz:en-font-list))))
 
 ;;color theme
 (zz:load-path "site-lisp/emacs-color-themes")

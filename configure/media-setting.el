114e4f73b8dded65005fe0cc872117b87e4e089a
Zachary Zhou <zchrzhou@gmail.com>
2021-05-19 10:17:33 +0800

    add more for mpa

--- configure/media-setting.el
+++ configure/media-setting.el
@@ -3,34 +3,14 @@
 
 (zz:load-path "site-lisp")
 
-;; https://www.mpg123.de/download.shtml
-(require 'mpg123)
-(setq mpg123-lang 0)
-(add-hook 'mpg123-hook
-          (lambda ()
-            (local-unset-key (kbd "<down-mouse-1>"))))
-
-(require 'mpg123-remote)
-(setq mpg123-show-help t)
-(setq mpg123-lazy-slider t)
-(setq mpg123-display-slider nil)
-
-(defun zz:mpg123-format-name-function (artist album title tracknum filename)
-  (concat (if (or mpg123-omit-id3-artist
-                  (string= artist ""))
-              ""
-              (concat artist " - "))
-          (if (string< "" title)
-              title
-              filename)))
-
-(setq mpg123-format-name-function 'zz:mpg123-format-name-function)
-
 ;;mpv
 (require 'mpv)
 
 ;;bingo
 (require 'bongo)
+(setq bongo-logo nil)
+(setq bongo-mode-line-indicator-mode nil)
+(setq bongo-enabled-backends '(mpv vlc))
 
 
 (provide 'media-setting)

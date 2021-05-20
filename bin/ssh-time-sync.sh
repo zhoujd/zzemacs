d409a63f8099e5df71e60a356404c879cb22d57c
Zachary Zhou <zchrzhou@gmail.com>
2021-05-20 21:36:20 +0800

    more format code

--- bin/ssh-time-sync.sh
+++ bin/ssh-time-sync.sh
@@ -1,21 +1,41 @@
 #!/bin/bash
 #set -x
 
-if [ $# != 1 ]; then
-    echo "Usage: $0 <user@host>"
+if [ $# != 2 ]; then
+    echo "Usage: $0 <user@host> <set|get>"
     exit 1
 fi
 
 HOST=$1
+TYPE=$2
 
 NOW=$(date +"%Y-%m-%d %T")
 echo "Now: $NOW"
 
-ssh -T $HOST << EOSSH
-echo "DATE: Set $HOST $NOW"
+date_set() {
+    echo "DATE: Set $HOST $NOW"
+    ssh -T $HOST << EOSSH
 date --set \"$NOW\""
 EOSSH
+}
 
-ssh -T $HOST << 'EOSSH'
+date_get() {
+    ssh -T $HOST << 'EOSSH'
 echo "DATE: $(date) [$(hostname)]"
 EOSSH
+}
+
+case "$TYPE" in
+    "set" )
+        date_set
+        ;;
+    "get" )
+        date_get
+        ;;
+    * )
+        echo "Unknown type: $TYPE"
+        exit 1
+        ;;
+esac
+
+echo "ssh time sync done"

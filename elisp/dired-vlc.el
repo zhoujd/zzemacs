8ac538e92cc48254130e1cc6ad40c4eed2b733e0
Zachary Zhou <zchrzhou@gmail.com>
2021-05-31 22:12:53 +0800

    add dired vlc play temp

---
+++ elisp/dired-vlc.el
@@ -0,0 +1,31 @@
+;;; dired-vlc.el
+
+(require 'dired-aux)
+(defvar dired-filelist-cmd
+  '(("vlc" "-L")))
+(defun dired-start-process (cmd &optional file-list)
+  (interactive
+   (let ((files (dired-get-marked-files
+                 t current-prefix-arg)))
+     (list
+      (dired-read-shell-command "& on %s: "
+                                current-prefix-arg files)
+      files)))
+  (let (list-switch)
+    (start-process
+     cmd nil shell-file-name
+     shell-command-switch
+     (format
+      "nohup 1>/dev/null 2>/dev/null %s \"%s\""
+      (if (and (> (length file-list) 1)
+               (setq list-switch
+                     (cadr (assoc cmd dired-filelist-cmd))))
+          (format "%s %s" cmd list-switch)
+          cmd)
+      (mapconcat #'expand-file-name file-list "\" \"")))))
+(define-key dired-mode-map "r" 'dired-start-process)
+
+
+(provide 'dired-vlc)
+
+;;; end of dired-vlc.el

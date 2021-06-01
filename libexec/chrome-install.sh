d8fda2b20819a31b97695c1f4f28e9a2e9e573d3
Zachary Zhou <zchrzhou@gmail.com>
2021-06-01 16:17:49 +0800

    add more for chrome install

--- libexec/chrome-install.sh
+++ libexec/chrome-install.sh
@@ -5,8 +5,8 @@ VERSION_STRING="89.0.4389.82-1"
 install() {
     pushd ~/Downloads/
     wget "https://dl.google.com/linux/chrome/deb/pool/main/g/google-chrome-stable/google-chrome-stable_${VERSION_STRING}_amd64.deb"
-    sudo dpkg -i "google-chrome-stable_${VERSION_STRING}"
-    rm -i "google-chrome-stable_${VERSION_STRING}"
+    sudo dpkg -i "google-chrome-stable_${VERSION_STRING}_amd64.deb"
+    rm -i "google-chrome-stable_${VERSION_STRING}_amd64.deb"
     popd
 }
 

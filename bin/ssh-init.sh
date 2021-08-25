e78413868cb5b390d8ae08a68982d7cada1dc4dd
Zachary Zhou <zchrzhou@gmail.com>
2021-08-25 09:53:17 +0800

    add more ssh init script

--- bin/ssh-init.sh
+++ bin/ssh-init.sh
@@ -1,34 +1,26 @@
 #!/bin/bash
 
 SCRIPT_ROOT=$(cd $(dirname "${BASH_SOURCE[0]}") && pwd)
-MCI_HOME=$(cd $SCRIPT_ROOT/.. && pwd)
 
 ssh_x11() {
+    echo "ssh x11 config"
     sudo sed -i 's/#AllowTcpForwarding yes/AllowTcpForwarding yes/g' /etc/ssh/sshd_config
     sudo sed -i 's/#X11Forwarding yes/X11Forwarding yes/g' /etc/ssh/sshd_config
     sudo sed -i 's/#X11DisplayOffset 10/X11DisplayOffset 10/g' /etc/ssh/sshd_config
-    sudo sed -i 's/#X11UseLocalhost yes/X11UseLocalhost yes/g' /etc/ssh/sshd_config
+    sudo sed -i 's/#X11UseLocalhost yes/X11UseLocalhost yes/g' /etc/ssh/sshd_config    
 }
 
 ssh_root() {
+    echo "ssh root config"
     sudo sed -i 's/#PermitRootLogin prohibit-password/PermitRootLogin yes/g' /etc/ssh/sshd_config
 }
 
 ssh_restart() {
+    echo "ssh restart"
     sudo systemctl restart sshd
 }
 
-ssh_keys() {
-    local TARGET="$HOME/.ssh"
-
-    rm -rf $TARGET
-    mkdir -p $TARGET
-
-    echo "Install keys to $TARGET/authorized_keys"
-    cat $MCI_HOME/misc/ssh/keys/id_rsa_intel.pub >> $TARGET/authorized_keys
-}
-
-ssh_keys
+ssh_x11
 ssh_root
 ssh_restart 
 

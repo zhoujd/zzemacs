c5545e01f1f183079fc59a81b431a1fe45dad589
Zachary Zhou <zchrzhou@gmail.com>
2021-08-30 21:29:48 +0800

    format code

--- configure/kube-setting.el
+++ configure/kube-setting.el
@@ -1,9 +1,17 @@
 ;;;; kube-setting.el
 
 ;;https://github.com/chrisbarrett/kubernetes-el
+;;magit-style interface to the Kubernetes
+;M-x kubernetes-overview
 (zz:load-path "site-lisp/kubernetes-el")
 (require 'kubernetes)
+
+;;kubel
+;M-x kubel-set-namespace
+;M-x kubel-set-context
 (require 'kubel)
+
+;;kubernetes helm
 (require 'kubernetes-helm)
 
 

cc5beb32a8907b8c2904b9542fdc95bcd3b92893
Zachary Zhou <zchrzhou@gmail.com>
2021-10-10 21:57:16 +0800

    add more for http

--- configure/http-setting.el
+++ configure/http-setting.el
@@ -7,6 +7,54 @@
 (zz:load-path "site-lisp/know-your-http-well")
 (require 'company-restclient)
 
+;;eg: (princ (url-http-get "http://httpbin.org/get" nil))
+(defun url-http-post (url args)
+  "Send ARGS to URL as a POST request."
+  (let (
+        (response-string nil)
+        (url-request-method "POST")
+        (url-request-extra-headers
+         '(("Content-Type" . "application/x-www-form-urlencoded")))
+        (url-request-data
+         (mapconcat (lambda (arg)
+                      (concat (url-hexify-string (car arg))
+                              "="
+                              (url-hexify-string (cdr arg))))
+                    args
+                    "&")))
+    (switch-to-buffer
+     (url-retrieve-synchronously url))
+    (goto-char (point-min))
+    (re-search-forward "\n\n")
+    (setq response-string
+          (buffer-substring-no-properties (point) (point-max)))
+    (kill-buffer (current-buffer))
+    response-string))
+
+;;eg: (princ (url-http-post "http://httpbin.org/post" '(("use" . "dummy")  ("pass" . "something else") ("code" . "pxyz0011213"))))
+(defun url-http-get (url args)
+  "Send ARGS to URL as a GET request."
+  (let (
+        (response-string nil)
+        (url-request-method "GET")
+        (url-request-data
+         (mapconcat (lambda (arg)
+                      (concat (url-hexify-string (car arg))
+                              "="
+                              (url-hexify-string (cdr arg))))
+                    args
+                    "&")))
+    (switch-to-buffer
+     (url-retrieve-synchronously
+      (concat url "?" url-request-data)))
+    (goto-char (point-min))
+    (re-search-forward "\n\n")
+    (setq response-string
+          (buffer-substring-no-properties
+           (point) (point-max)))
+    (kill-buffer (current-buffer))
+    response-string))
+
 
 (provide 'http-setting)
 

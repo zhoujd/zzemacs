5da25f233c8ccc651db4643253b0ac0c0ff7a183
Zachary Zhou <zchrzhou@gmail.com>
2021-10-31 13:13:20 +0800

    add more comments for ack

--- libexec/ack-install.sh
+++ libexec/ack-install.sh
@@ -1,5 +1,8 @@
 #!/bin/bash
 
+## https://beyondgrep.com/install/
+## https://beyondgrep.com/more-tools/
+
 VER=v3.5.0
 sudo wget -O /usr/local/bin/ack https://beyondgrep.com/ack-$VER
 sudo chmod +x /usr/local/bin/ack

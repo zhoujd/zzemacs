db2a3401b1271308796f4bec8ffcb6cb42f731b5
Zachary Zhou <zchrzhou@gmail.com>
2021-10-31 13:08:51 +0800

    clean code

--- libexec/ack-install.sh
+++ libexec/ack-install.sh
@@ -1,7 +1,6 @@
 #!/bin/bash
 
 VER=v3.5.0
-#sudo curl https://beyondgrep.com/ack-$VER > /usr/local/bin/ack
 sudo wget -O /usr/local/bin/ack https://beyondgrep.com/ack-$VER
 sudo chmod +x /usr/local/bin/ack
 

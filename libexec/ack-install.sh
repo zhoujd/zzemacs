0ac0f08b890197755cb91242bdbfef9cdb366738
Zachary Zhou <zchrzhou@gmail.com>
2021-10-31 13:45:46 +0800

    add more for pss

--- libexec/ack-install.sh
+++ libexec/ack-install.sh
@@ -3,6 +3,7 @@
 ## https://beyondgrep.com/install/
 ## https://beyondgrep.com/more-tools/
 ## Ubuntu: sudo apt install ack-grep
+## Python: pip install pss
 
 VER=v3.5.0
 sudo wget -O /usr/local/bin/ack https://beyondgrep.com/ack-$VER

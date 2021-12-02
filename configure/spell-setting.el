f124f6a649983d75bc2f5f5aa2696a12ee00be20
Zachary Zhou <zchrzhou@gmail.com>
2021-12-02 13:08:18 +0800

    more for flyspell check

--- configure/spell-setting.el
+++ configure/spell-setting.el
@@ -11,6 +11,35 @@
   (setq-default ispell-program-name "aspell")
   (setq ispell-really-hunspell t))
 
+(defun flyspell-on-for-buffer-type ()
+  "Enable Flyspell appropriately for the major mode of the current buffer.
+Uses `flyspell-prog-mode' for modes derived from `prog-mode', so only strings and comments get checked.
+All other buffers get `flyspell-mode' to check all text.  If flyspell is already enabled, does nothing."
+  (interactive)
+  (if (not (symbol-value flyspell-mode)) ; if not already on
+      (progn
+        (if (derived-mode-p 'prog-mode)
+            (progn
+              (message "Flyspell on (code)")
+              (flyspell-prog-mode))
+            ;; else
+            (progn
+              (message "Flyspell on (text)")
+              (flyspell-mode 1)))
+        ;; I tried putting (flyspell-buffer) here but it didn't seem to work
+        )))
+
+(defun flyspell-toggle ()
+  "Turn Flyspell on if it is off, or off if it is on.
+When turning on, it uses `flyspell-on-for-buffer-type' so code-vs-text is handled appropriately."
+  (interactive)
+  (if (symbol-value flyspell-mode)
+      (progn ;; flyspell is on, turn it off
+        (message "Flyspell off")
+        (flyspell-mode -1))
+      ;; else - flyspell is off, turn it on
+      (flyspell-on-for-buffer-type)))
+
 
 (provide 'spell-setting)
 

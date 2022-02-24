1bf70e1d5d891c79624175a3c0127f9b90272674
Zachary Zhou <zchrzhou@gmail.com>
2022-02-24 22:29:54 +0800

    more for git prompt

--- etc/profile.d/06-gitprompt.sh
+++ etc/profile.d/06-gitprompt.sh
@@ -1,13 +1,17 @@
-# we pass 2 parameters to __git_ps1 as pre and post which are strings.
-export PROMPT_COMMAND='__git_ps1 "\u@\h:\W" "\\\$ "'
+### Git Prompt
 
-# if .git-prompt.sh exists, set options and execute it
-if [ -f ~/.git-prompt.sh ]; then
-    GIT_PS1_SHOWDIRTYSTATE=true
-    GIT_PS1_SHOWSTASHSTATE=true
-    GIT_PS1_SHOWUNTRACKEDFILES=true
-    GIT_PS1_SHOWUPSTREAM="auto"
-    GIT_PS1_HIDE_IF_PWD_IGNORED=true
-    GIT_PS1_SHOWCOLORHINTS=true
-    . ~/.git-prompt.sh
-fi
+gitprompt() {
+    # we pass 2 parameters to __git_ps1 as pre and post which are strings.
+    export PROMPT_COMMAND='__git_ps1 "\u@\h:\W" "\\\$ "'
+
+    # if .git-prompt.sh exists, set options and execute it
+    if [ -f ~/.git-prompt.sh ]; then
+        GIT_PS1_SHOWDIRTYSTATE=true
+        GIT_PS1_SHOWSTASHSTATE=true
+        GIT_PS1_SHOWUNTRACKEDFILES=true
+        GIT_PS1_SHOWUPSTREAM="auto"
+        GIT_PS1_HIDE_IF_PWD_IGNORED=true
+        GIT_PS1_SHOWCOLORHINTS=true
+        . ~/.git-prompt.sh
+    fi
+}

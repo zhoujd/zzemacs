4d23616f3650af90922bea666127367b13f34b06
Zachary Zhou <zchrzhou@gmail.com>
2022-04-29 10:09:03 +0800

    clean script for evolution

--- libexec/evolution-tool.sh
+++ libexec/evolution-tool.sh
@@ -5,13 +5,29 @@ if [ -z $(command -v evolution) ] ; then
     exit 1
 fi
 
+force_down() {
+    echo "evolution down"
+    evolution --force-shutdown > /dev/null 2>&1
+    sleep 2s
+}
+
 disable_alarm() {
+    echo "evolution noalarm"
     echo "kill evolution-alarm-notify"
     sudo killall evolution-alarm
     echo "chmod -x evolution-alarm-notify"
     sudo chmod -x /usr/libexec/evolution-data-server/evolution-alarm-notify
 }
 
-disable_alarm
+case $1 in
+    force )
+        force_down
+        ;;
+    noalarm )
+        disable_alarm
+        ;;
 
-echo "evolution disble evolution-alarm-notify done"
+    * )
+        echo "Usage: $(basename $0) {force|noalarm}"
+        ;;
+esac

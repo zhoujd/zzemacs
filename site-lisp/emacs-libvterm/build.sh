c4555ae3e936338ab0ff3755ad8c1d2930a5d5c6
Zachary Zhou <zchrzhou@gmail.com>
2022-07-14 22:45:53 +0800

    more for build

--- site-lisp/emacs-libvterm/build.sh
+++ site-lisp/emacs-libvterm/build.sh
@@ -1,12 +1,26 @@
 #!/bin/bash
 
-install_deps() {
+SCRIPT_ROOT=$(cd $(dirname $0) && pwd)
+
+deps() {
     echo "Install deps"
-    sudo apt install cmake
-    sudo apt install libtool
-    sudo apt install libtool-bin
+    sudo apt install -y cmake libtool libtool-bin
 }
 
-install_deps
+build() {
+    echo "build emacs-libvterm"
+    git clone https://github.com/akermu/emacs-libvterm
+    pushd emacs-libvterm
+    cmake .
+    make
+    cp -vf vterm-module.so $SCRIPT_ROOT
+    popd
+
+    echo "clean libvterm"
+    rm -rf emacs-libvterm
+}
+
+deps
+build
 
 echo "Build done"

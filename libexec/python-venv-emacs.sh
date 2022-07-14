859cbbb4b3f094a1d0d3e5a3707bf82260b2df6d
Zachary Zhou <zchrzhou@gmail.com>
2022-07-14 11:21:54 +0800

    more for emacs venv

--- libexec/python-venv-emacs.sh
+++ libexec/python-venv-emacs.sh
@@ -1,6 +1,16 @@
 #!/bin/bash
 
-sudo apt install python3.8-venv
-python3 -m venv ~/.venv/emacs
+install_deps() {
+    echo "Install deps"
+    sudo apt install python3.8-venv
+}
+
+install_venv() {
+    echo "Install emacs venv"
+    python3 -m venv ~/.venv/emacs
+}
+
+install_deps
+install_venv
 
 echo "Setup Python venv emacs done"

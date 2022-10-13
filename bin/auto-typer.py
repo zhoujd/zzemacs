ba0de7fdca87ce3e0a3d848488487dcf1c8e0c09
Zachary Zhou <zchrzhou@gmail.com>
2022-10-13 20:40:42 +0800

    more for auto-typer

---
+++ bin/auto-typer.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python3
+
+# https://github.com/Parveshdhull/AutoTyper
+# apt install python3-tk python3-dev
+
+import pyautogui
+import time
+import sys
+
+delay = 2 # Inital Delay in Seconds
+time.sleep(delay)
+
+interval = 0.07   # In Seconds
+cmd=' '.join(sys.argv[1:]) + "\n"
+pyautogui.write(cmd, interval=interval)

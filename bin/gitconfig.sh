0abbb121665efd3a9a620648cdf5522a2ce5b13a
Zachary Zhou <zchrzhou@gmail.com>
2022-10-16 09:50:37 +0800

    format code

--- bin/gitconfig.sh
+++ bin/gitconfig.sh
@@ -4,12 +4,38 @@ SCRIPT_ROOT=$(cd $(dirname $0) && pwd)
 GITCONIF_ROOT=$(cd $SCRIPT_ROOT/../misc/gitconfig.d && pwd)
 GITCONFI_TGT=~/.gitconfig.d
 
-echo "Install $GITCONIF_ROOT to $GITCONFI_TGT"
-ln -sfvT $GITCONIF_ROOT $GITCONFI_TGT
+install() {
+    echo "Install $GITCONIF_ROOT to $GITCONFI_TGT"
+    ln -sfvT $GITCONIF_ROOT $GITCONFI_TGT
 
-echo "Install gitconfig"
-pushd $GITCONFI_TGT
-./install.sh
-popd
+    echo "Install gitconfig"
+    pushd $GITCONFI_TGT
+    ./install.sh
+    popd
 
-echo "Gitconfig done"
+    echo "Install done"
+}
+
+uninstall() {
+    echo "Uninstall $GITCONFI_TGT"
+    rm -f $GITCONFI_TGT
+    rm -f ~/.gitconfig
+    
+    echo "Uninstall done"
+}
+
+usage() {
+    echo "Usage: $(basename $0) {install|uninstall}"
+}
+
+case $1 in
+    install )
+        install
+        ;;
+    uninstall )
+        uninstall
+        ;;
+    * )
+        usage
+        ;;
+esac

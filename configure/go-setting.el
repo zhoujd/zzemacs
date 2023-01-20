01b4c7b8db082c7e8afed121093ce4a593303f65
Zachary Zhou <zchrzhou@gmail.com>
2023-01-20 14:18:48 +0800

    format code

--- configure/go-setting.el
+++ configure/go-setting.el
@@ -53,9 +53,11 @@
   (setq tab-width 2)
   (setq standard-indent 2)
   (setq indent-tabs-mode nil))
+
 (add-hook 'go-mode-hook 'zz:go-indent4)
 
 ;; enable lsp-mode
+(require 'lsp-go)
 (add-hook 'go-mode-hook 'lsp-deferred)
 
 

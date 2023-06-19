3dd26142338373a58918bfad6dde2507a8805827
Zachary Zhou <zchrzhou@gmail.com>
2023-06-19 16:20:28 +0800

    More for disable bongo

--- configure/media-setting.el
+++ configure/media-setting.el
@@ -14,14 +14,14 @@
 ;;bingo
 ;;amixer: Mixer attach default error: No such file or directory
 ;;(= (shell-command "amixer > /dev/null") 0)
-(require 'bongo)
-(setq bongo-logo nil)
-(setq bongo-enabled-backends '(vlc mpv))
-(setq bongo-mode-line-indicator-mode nil)
-(setq bongo-mode-line-indicator-format nil)
-(setq bongo-display-track-icons nil)
-(setq bongo-track-mark-icon-file-name nil)
-(setq bongo-vlc-program-name "cvlc")
+;(require 'bongo)
+;(setq bongo-logo nil)
+;(setq bongo-enabled-backends '(vlc mpv))
+;(setq bongo-mode-line-indicator-mode nil)
+;(setq bongo-mode-line-indicator-format nil)
+;(setq bongo-display-track-icons nil)
+;(setq bongo-track-mark-icon-file-name nil)
+;(setq bongo-vlc-program-name "cvlc")
 
 
 (provide 'media-setting)

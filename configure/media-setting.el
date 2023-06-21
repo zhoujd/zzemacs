851b6efc9847a4c8ab50cac2b19375bf258d796f
Zachary Zhou <zchrzhou@gmail.com>
2023-06-21 18:09:13 +0800

    more for bongo

--- configure/media-setting.el
+++ configure/media-setting.el
@@ -13,15 +13,15 @@
 
 ;;bingo
 ;;amixer: Mixer attach default error: No such file or directory
-;;(= (shell-command "amixer > /dev/null") 0)
-;(require 'bongo)
-;(setq bongo-logo nil)
-;(setq bongo-enabled-backends '(vlc mpv))
-;(setq bongo-mode-line-indicator-mode nil)
-;(setq bongo-mode-line-indicator-format nil)
-;(setq bongo-display-track-icons nil)
-;(setq bongo-track-mark-icon-file-name nil)
-;(setq bongo-vlc-program-name "cvlc")
+(when (= (shell-command "amixer") 0)
+  (require 'bongo)
+  (setq bongo-logo nil)
+  (setq bongo-enabled-backends '(vlc mpv))
+  (setq bongo-mode-line-indicator-mode nil)
+  (setq bongo-mode-line-indicator-format nil)
+  (setq bongo-display-track-icons nil)
+  (setq bongo-track-mark-icon-file-name nil)
+  (setq bongo-vlc-program-name "cvlc"))
 
 
 (provide 'media-setting)

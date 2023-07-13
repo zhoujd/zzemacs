0e5a2dee4563ba0da1a4348e13ab98fbf98f9e9e
Zachary Zhou <zchrzhou@gmail.com>
2023-07-13 16:03:27 +0800

    Update for ag installation

--- libexec/zz-core/ag-install.sh
+++ libexec/zz-core/ag-install.sh
@@ -1,5 +1,9 @@
 #!/bin/bash
 
+### https://github.com/ggreer/the_silver_searcher
+## sudo apt install silversearcher-ag
+## sudo pacman -S the_silver_searcher
+
 ## sudo yum install -y pcre-devel xz-devel
 ## sudo yum install automake
 

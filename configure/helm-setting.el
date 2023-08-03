4d22fa8e1efc39e8a369c77c0de060988332bc61
Zachary Zhou <zchrzhou@gmail.com>
2023-08-03 17:52:06 +0800

    Update for cscope keys

--- configure/helm-setting.el
+++ configure/helm-setting.el
@@ -207,8 +207,10 @@
 (add-hook 'c-mode-common-hook 'helm-cscope-mode)
 (add-hook 'helm-cscope-mode-hook
           (lambda ()
+            (local-set-key (kbd "M-s M-.") 'helm-cscope-find-global-definition)
             (local-set-key (kbd "M-s M-d") 'helm-cscope-find-global-definition)
             (local-set-key (kbd "M-s M-c") 'helm-cscope-find-calling-this-funtcion)
+            (local-set-key (kbd "M-s M-@") 'helm-cscope-find-calling-this-function)
             (local-set-key (kbd "M-s M-C") 'helm-cscope-find-called-function)
             (local-set-key (kbd "M-s M-s") 'helm-cscope-find-this-symbol)
             (local-set-key (kbd "M-s M-f") 'helm-cscope-find-this-file)

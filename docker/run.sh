77542110ab7eddea3aeb7d8ba8c0d10c9b6bf541
Zachary Zhou <zchrzhou@gmail.com>
2023-09-01 16:16:29 +0800

    Update folder setting

--- docker/run.sh
+++ docker/run.sh
@@ -1,12 +1,16 @@
 #!/bin/bash
 
+SCRIPT_ROOT=$(cd $(dirname "${BASH_SOURCE[0]}") && pwd)
+ZZEMACS_ROOT=$(cd $SCRIPT_ROOT/.. && pwd)
+
 RUN_PARAM=(
     -e DISPLAY=$DISPLAY
     -h $HOSTNAME
     -v /tmp/.X11-unix:/tmp/.X11-unix
     -v $HOME/.Xauthority:/home/zach/.Xauthority
-    -v $HOME/.emacs:/home/zach/.emacs
-    -v $HOME/zzemacs:/home/zach/zzemacs
+    -v $ZZEMACS_ROOT/.emacs:/home/zach/.emacs
+    -v $ZZEMACS_ROOT/font:/home/zach/.fonts
+    -v $ZZEMACS_ROOT:/home/zach/zzemacs
 )
 
 docker run ${RUN_PARAM[@]} ubuntu:zach emacs

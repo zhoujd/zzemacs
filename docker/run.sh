0f6478b55b63bfbe1ab187d9a19269b530d8807d
Zachary Zhou <zchrzhou@gmail.com>
2023-09-01 16:29:55 +0800

    Update run script

--- docker/run.sh
+++ docker/run.sh
@@ -3,14 +3,20 @@
 SCRIPT_ROOT=$(cd $(dirname "${BASH_SOURCE[0]}") && pwd)
 ZZEMACS_ROOT=$(cd $SCRIPT_ROOT/.. && pwd)
 
+REMOTE_HOST=$HOSTNAME
+REMOTE_USER=zach
+REMOTE_HOME=/home/$REMOTE_USER
+
 RUN_PARAM=(
     -e DISPLAY=$DISPLAY
-    -h $HOSTNAME
+    -e SHELL=/bin/bash
+    -h $REMOTE_HOST
+    -u $REMOTE_USER
     -v /tmp/.X11-unix:/tmp/.X11-unix
-    -v $HOME/.Xauthority:/home/zach/.Xauthority
-    -v $ZZEMACS_ROOT/.emacs:/home/zach/.emacs
-    -v $ZZEMACS_ROOT/font:/home/zach/.fonts
-    -v $ZZEMACS_ROOT:/home/zach/zzemacs
+    -v $HOME/.Xauthority:$REMOTE_HOME/.Xauthority
+    -v $ZZEMACS_ROOT:$REMOTE_HOME/zzemacs
+    -v $ZZEMACS_ROOT/font:$REMOTE_HOME/.fonts
+    -v $ZZEMACS_ROOT/.emacs:$REMOTE_HOME/.emacs
 )
 
 docker run ${RUN_PARAM[@]} ubuntu:zach emacs

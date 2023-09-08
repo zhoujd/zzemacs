9341f3316b8f121a1a8a9396911741f6c33b8a50
Zachary Zhou <zchrzhou@gmail.com>
2023-09-08 10:46:25 +0800

    Update for run script

--- docker/run.sh
+++ docker/run.sh
@@ -2,81 +2,22 @@
 #set -x
 
 SCRIPT_ROOT=$(cd $(dirname "${BASH_SOURCE[0]}") && pwd)
-ZZEMACS_ROOT=$(cd $SCRIPT_ROOT/.. && pwd)
-ZZEMACS_TOP=$(cd $ZZEMACS_ROOT/.. && pwd)
-
-IMG=${IMG:-ubuntu-2004-zzemacs}
-TAG=${TAG:-base}
-CTN=${CTN:-zzemacs-$TAG}
-REMOTE_HOST=${REMOTE_HOST:-$IMG}
-REMOTE_USER=${REMOTE_USER:-$USER}
-REMOTE_HOME=${REMOTE_HOME:-/home/$REMOTE_USER}
-SSH_HOST=${SSH_HOST:-localhost}
-SSH_PORT=${SSH_PORT:-10022}
-SSH_USER=${REMOTE_USER}
-HOST_NAME=${HOST_NAME:-myhost}
-HOST_IP=${HOST_IP:-host-gateway}
-
-## Use local X11 Server
-X11_PARAM=(
-    -e DISPLAY=$DISPLAY
-    -v /tmp/.X11-unix:/tmp/.X11-unix
-    -v $HOME/.Xauthority:$REMOTE_HOME/.Xauthority
-)
-
-RUN_PARAM=(
-    --privileged=true
-    --cap-add=ALL
-    --add-host=$HOST_NAME:$HOST_IP
-    -h $REMOTE_HOST
-    -u $REMOTE_USER
-    -p $SSH_PORT:22
-    -v /dev:/dev
-    -v /var/run/docker.sock:/var/run/docker.sock
-    -v /etc/security/limits.conf:/etc/security/limits.conf
-    -v $ZZEMACS_TOP/lab:$REMOTE_HOME/lab
-    -v $ZZEMACS_ROOT:$REMOTE_HOME/zzemacs
-)
-
-EXEC_PARAM=(
-    -e DISPLAY=$DISPLAY
-    -e SHELL=/bin/bash
-    -u $REMOTE_USER
-)
-
-EMACS_PARAM=(
-    emacs
-    -nw
-)
-
-SHELL_PARAM=(
-    bash
-    -l
-)
+TOOL_ROOT=$SCRIPT_ROOT/script
 
 case $1 in
-    start )
-        docker run -d --name=${CTN} ${RUN_PARAM[@]} ${IMG}:${TAG}
+    base )
+        shift
+        export TAG=base
+        export SSH_PORT=10022
+        $TOOL_ROOT/tool.sh $@
         ;;
-    stop )
-        docker stop ${CTN} && docker rm ${CTN}
-        ;;
-    status )
-        docker ps | grep ${CTN}
-        ;;
-    emacs )
-        docker exec -it ${EXEC_PARAM[@]} ${CTN} ${EMACS_PARAM[@]}
-        ;;
-    shell )
-        docker exec -it ${EXEC_PARAM[@]} ${CTN} ${SHELL_PARAM[@]}
-        ;;
-    ssh )
-        ssh -X -l ${SSH_USER} ${SSH_HOST} -p ${SSH_PORT}
-        ;;
-    build )
-        make -C dockerfiles
+    dev )
+        shift
+        export TAG=dev
+        export SSH_PORT=11022
+        $TOOL_ROOT/tool.sh $@
         ;;
     * )
-        echo "Usage: $(basename $0) {start|stop|status|emacs|shell|ssh|build}"
+        echo "Usage: $(basename $0) {base|dev}"
         ;;
 esac

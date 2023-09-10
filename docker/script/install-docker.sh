dd449f3a8164e4b5a4317dd705d96508e566198a
Zachary Zhou <zchrzhou@gmail.com>
2023-09-10 18:28:06 +0800

    Add script for docker install

---
+++ docker/script/install-docker.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+mkdir -p /etc/apt/keyrings
+curl -fsSL https://download.docker.com/linux/ubuntu/gpg | gpg --dearmor -o /etc/apt/keyrings/docker.gpg
+echo "deb [arch=$(dpkg --print-architecture) signed-by=/etc/apt/keyrings/docker.gpg] https://download.docker.com/linux/ubuntu $(lsb_release -cs) stable" \
+    | sudo tee /etc/apt/sources.list.d/docker.list
+apt-get update \
+    && apt-get install -y --no-install-recommends \
+    docker-ce docker-ce-cli containerd.io docker-compose-plugin \
+    && apt-get autoremove \
+    && apt-get clean
+
+echo "Docker Install Done"

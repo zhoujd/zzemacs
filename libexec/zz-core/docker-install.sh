470de275824130acb209c9257233f39c1b3b5c85
Zachary Zhou <zchrzhou@gmail.com>
2023-09-19 10:59:36 +0800

    Docker install

---
+++ libexec/zz-core/docker-install.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+#set -x
+
+Install_docker() {
+    echo "Install Docker CE CLI"
+    sudo apt-get update \
+         sudo apt-get install -y apt-transport-https ca-certificates curl gnupg2 lsb-release \
+         curl -fsSL https://download.docker.com/linux/$(lsb_release -is | tr '[:upper:]' '[:lower:]')/gpg | sudo apt-key add - 2>/dev/null
+    echo "deb [arch=$(dpkg --print-architecture)] https://download.docker.com/linux/$(lsb_release -is | tr '[:upper:]' '[:lower:]') $(lsb_release -cs) stable" | sudo tee /etc/apt/sources.list.d/docker.list
+    sudo apt-get update
+    sudo apt-get install -y docker-ce-cli
+}
+
+Install_compos() {
+    echo "Install Docker Compose"
+    export LATEST_COMPOSE_VERSION=$(curl -sSL "https://api.github.com/repos/docker/compose/releases/latest" | grep -o -P '(?<="tag_name": ").+(?=")')
+    sudo curl -sSL "https://github.com/docker/compose/releases/download/${LATEST_COMPOSE_VERSION}/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
+    sudo chmod +x /usr/local/bin/docker-compose
+}
+
+Install_docker
+Install_compose
+
+echo "Docker install done"

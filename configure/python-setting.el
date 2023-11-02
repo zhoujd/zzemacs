6bbb768cca22e9344b25b6c8afe6cfd9a7fee256
Zachary Zhou <zchrzhou@gmail.com>
2023-11-02 10:49:05 +0800

    Remove elpy

--- configure/python-setting.el
+++ configure/python-setting.el
@@ -8,36 +8,11 @@
 ;;git clone https://github.com/jorgenschaefer/elpy
 ;;M-x elpy-config
 (zz:load-path "site-lisp/python-mode")
-(zz:load-path "site-lisp/elpy")
-(load "elpy")
-(load "elpy-rpc")
-(load "elpy-shell")
-(load "elpy-profile")
-(load "elpy-refactor")
-(load "elpy-django")
-(elpy-enable)
-
-;;disable highlight-indentation-mode
-(add-hook 'elpy-mode-hook
-          (lambda ()
-            (highlight-indentation-mode -1)))
-
-(zz:load-path "site-lisp/python-environment")
-(zz:load-path "site-lisp/epc")
-
-;;remove warning
-(setq python-shell-completion-native-enable nil)
-;;remove warning: "Can't guess python-indent-offset"
-(setq python-indent-guess-indent-offset-verbose nil)
 
 ;;pdb setup, note the python version
 ;;run pdb.py (like this): python3 -i -m pdb <file-name.py>
 ;;M-x pdb
 (setq gud-pdb-command-name "python3 -i -m pdb")
-
-;;scons file setting
-(setq auto-mode-alist (cons '("SConstruct" . python-mode) auto-mode-alist))
-(setq auto-mode-alist (cons '("SConscript" . python-mode) auto-mode-alist))
 
 ;;python3
 (setq py-python-command "python3")
@@ -45,24 +20,6 @@
 ;;ipython or python3
 (setq python-shell-interpreter "python3"
       python-shell-interpreter-args "-i")
-
-;;flycheck
-(setq flycheck-python-pycompile-executable "python3"
-      flycheck-python-pylint-executable "python3"
-      flycheck-python-flake8-executable "python3")
-
-;;virtualenv
-;;python3 -m venv ~/.venv/emacs
-;;vim ~/.venv/emacs/pyvenv.cfg
-;;include-system-site-packages = true
-(setq elpy-rpc-python-command "python3")
-(setq elpy-rpc-virtualenv-path "~/.venv/emacs")
-
-(defun zz:venv ()
-  (interactive)
-  (let ((venv (expand-file-name elpy-rpc-virtualenv-path)))
-    (when (file-exists-p venv)
-      (pyvenv-activate venv))))
 
 ;;custom indent
 (defun zz:python-indent (num)

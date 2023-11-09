62d03858675cadf438e19514e1eabbd4ec51d620
Zachary Zhou <zchrzhou@gmail.com>
2023-11-09 11:41:45 +0800

    Refactor  proj-setting.el

--- configure/prog-setting.el
+++ configure/prog-setting.el
@@ -7,80 +7,6 @@
 ;;;$find . -iregex .*\.el$ | xargs etags
 (setq grep-find-use-xargs t)
 
-;;develop setting for tags path etc.
-(defvar zz:dev-set-file (format "%s/.emacs.d/temp-setting.el"
-                                (getenv "HOME"))
-  "temp project setting")
-
-;;generate temp-setting.el
-(defun zz:create-file (fpath content)
-  "Process the file at path FPATH ..."
-  (let ((tmp-buf-name (file-name-nondirectory fpath)))
-    (set-buffer (get-buffer-create tmp-buf-name))
-    (goto-char 0)
-    (dolist (item content)
-      (insert item)
-      (insert "\n"))
-    (write-file fpath)
-    (kill-buffer tmp-buf-name)))
-
-(defvar zz:proj-list (list zzemacs-path)
-  "project directory list")
-(defvar zz:tag-root (format "%s/.emacs.d/tags/"
-                            (getenv "HOME"))
-  "tag directory root")
-
-(unless (file-exists-p zz:tag-root)
-  (make-directory zz:tag-root))
-
-;;temp setting template
-(defconst zz:temp-template
-  '(
-    ";;;; temp-setting.el --- program temp file"
-    ""
-    ";; set project direcitory list"
-    "(setq zz:proj-list '("
-    "                     \"/usr/include\""
-    "                     \"~/work/project-1\""
-    "                     \"~/work/project-2\""
-    "                     ))"
-    ""
-     "(mapc #'zz:add-os-path"
-    "      '("
-    "        \"~/work/script\""
-    "        ))"
-    ""
-    "(mapc #'zz:add-lib-path"
-    "      '("
-    "        \"~/work/lib\""
-    "        ))"
-    ""
-    ";; project key setting"
-    ";(zz:exec-key f4-p-map \"f\" zz:firefox '(\"firefox\" \"http://www.baidu.com\"))"
-    ))
-
-(defun zz:temp-setting ()
-  "Create ~/.emacs.d/temp-setting.el"
-  (interactive)
-  (let ((path zz:dev-set-file))
-    (if (file-exists-p path)
-        (progn
-         (find-file path)
-         (message "open %s successful." path))
-        (progn
-         (zz:create-file path zz:temp-template)
-         (message "create %s successful." path))
-      )))
-
-(defun zz:temp-delete ()
-  "delete ~/.emacs.d/temp-setting.el"
-  (interactive)
-  (let ((path zz:dev-set-file))
-    (when (file-exists-p path)
-      (delete-file path)
-      (message "delete %s successful." path))
-      ))
-
 ;; holding
 (require 'hideshow)
 
@@ -111,14 +37,6 @@
       sr-speedbar-width  30
       sr-speedbar-max-width 60
       sr-speedbar-right-side nil)
-
-;;load the etags-select.el source code
-(require 'etags-select)
-(require 'etags-table)
-(require 'etags-stack)
-(let ((tag-path (concat zz:tag-root "TAGS")))
-  (when (not (member tag-path  tags-table-list))
-    (push tag-path tags-table-list)))
 
 (defun zz:gen-ctags-cmd (dir-name)
   (format "ctags %s -f %s/TAGS -e -R %s"
@@ -195,60 +113,6 @@
   (if (executable-find "cscope")
       (zz:run-command (zz:gen-cscope-cmd dir-name))
       (message "no cscope, please install it")))
-
-;;creast etags/cscope for multi project
-(defun zz:gen-proj-find-path (proj-list)
-  (let ((proj-path-parts ""))
-    (dolist (cell proj-list)
-      (setq proj-path-parts (concat proj-path-parts cell " ")))
-    (substring proj-path-parts 0 -1)))
-
-(defun zz:create-proj-etags ()
-  (interactive)
-  (zz:create-etags (zz:gen-proj-find-path zz:proj-list)))
-
-(defun zz:create-proj-cscope ()
-  (interactive)
-  (zz:create-cscope (zz:gen-proj-find-path zz:proj-list)))
-
-;; create etags to zz:tag-root
-(defun zz:create-root-etags ()
-  (interactive)
-  (let ((default-directory zz:tag-root))
-    (unless (file-exists-p zz:tag-root)
-      (make-directory zz:tag-root))
-    (zz:create-proj-etags)))
-
-;; create cscope to zz:tag-root
-(defun zz:create-root-cscope ()
-  (interactive)
-  (let ((default-directory zz:tag-root))
-    (unless (file-exists-p zz:tag-root)
-      (make-directory zz:tag-root))
-    (zz:create-proj-cscope)))
-
-(defun zz:rscope-autoinit-path (buffer)
-  "Look the directory from zz:tag-root"
-  (when (file-readable-p (concat zz:tag-root rscope-database-name))
-    zz:tag-root))
-
-;; set cscope tag root
-(defun zz:set-root-cscope ()
-  (interactive)
-  (cscope-set-initial-directory zz:tag-root)
-  (setq helm-cscope-search-dir-init (list (list zz:tag-root)))
-  (add-hook 'rscope-autoinit-cscope-dir-hooks
-            (function zz:rscope-autoinit-path))
-  (message "Set cscope init directory: %s" zz:tag-root))
-
-;; unset cscope tag root
-(defun zz:unset-root-cscope ()
-  (interactive)
-  (cscope-unset-initial-directory)
-  (setq helm-cscope-search-dir-init nil)
-  (remove-hook 'rscope-autoinit-cscope-dir-hooks
-               (function zz:rscope-autoinit-path))
-  (message "Unset cscope init directory"))
 
 ;;add  mode support
 (setq auto-mode-alist
@@ -360,9 +224,9 @@
           'yaml-mode-hook))
   (add-hook hook 'highlight-indent-guides-mode))
 
-;;load temp setting
-(when (file-exists-p zz:dev-set-file)
-  (load-file zz:dev-set-file))
+;;project-custom
+(zz:load-path "elisp")
+(require 'project-custom)
 
 
 (provide 'prog-setting)
---
+++ elisp/project-custom.el
@@ -0,0 +1,142 @@
+;;; project custom
+
+;;develop setting for tags path etc.
+(defvar zz:dev-set-file (format "%s/.emacs.d/temp-setting.el"
+                                (getenv "HOME"))
+  "temp project setting")
+
+;;generate temp-setting.el
+(defun zz:create-file (fpath content)
+  "Process the file at path FPATH ..."
+  (let ((tmp-buf-name (file-name-nondirectory fpath)))
+    (set-buffer (get-buffer-create tmp-buf-name))
+    (goto-char 0)
+    (dolist (item content)
+      (insert item)
+      (insert "\n"))
+    (write-file fpath)
+    (kill-buffer tmp-buf-name)))
+
+(defvar zz:proj-list (list zzemacs-path)
+  "project directory list")
+
+;;temp setting template
+(defconst zz:temp-template
+  '(
+    ";;;; temp-setting.el --- program temp file"
+    ""
+    ";; set project direcitory list"
+    "(setq zz:proj-list '("
+    "                     \"/usr/include\""
+    "                     \"~/work/project-1\""
+    "                     \"~/work/project-2\""
+    "                     ))"
+    ""
+     "(mapc #'zz:add-os-path"
+    "      '("
+    "        \"~/work/script\""
+    "        ))"
+    ""
+    "(mapc #'zz:add-lib-path"
+    "      '("
+    "        \"~/work/lib\""
+    "        ))"
+    ""
+    ";; project key setting"
+    ";(zz:exec-key f4-p-map \"f\" zz:firefox '(\"firefox\" \"http://www.baidu.com\"))"
+    ))
+
+(defun zz:temp-setting ()
+  "Create ~/.emacs.d/temp-setting.el"
+  (interactive)
+  (let ((path zz:dev-set-file))
+    (if (file-exists-p path)
+        (progn
+         (find-file path)
+         (message "open %s successful." path))
+        (progn
+         (zz:create-file path zz:temp-template)
+         (message "create %s successful." path))
+      )))
+
+(defun zz:temp-delete ()
+  "delete ~/.emacs.d/temp-setting.el"
+  (interactive)
+  (let ((path zz:dev-set-file))
+    (when (file-exists-p path)
+      (delete-file path)
+      (message "delete %s successful." path))
+      ))
+;;creast etags/cscope for multi project
+(defun zz:gen-proj-find-path (proj-list)
+  (let ((proj-path-parts ""))
+    (dolist (cell proj-list)
+      (setq proj-path-parts (concat proj-path-parts cell " ")))
+    (substring proj-path-parts 0 -1)))
+
+(defun zz:create-proj-etags ()
+  (interactive)
+  (zz:create-etags (zz:gen-proj-find-path zz:proj-list)))
+
+(defun zz:create-proj-cscope ()
+  (interactive)
+  (zz:create-cscope (zz:gen-proj-find-path zz:proj-list)))
+
+(defvar zz:tag-root (format "%s/.emacs.d/tags/"
+                            (getenv "HOME"))
+  "tag directory root")
+
+(unless (file-exists-p zz:tag-root)
+  (make-directory zz:tag-root))
+
+(let ((tag-path (concat zz:tag-root "TAGS")))
+  (when (not (member tag-path  tags-table-list))
+    (push tag-path tags-table-list)))
+
+;; create etags to zz:tag-root
+(defun zz:create-root-etags ()
+  (interactive)
+  (let ((default-directory zz:tag-root))
+    (unless (file-exists-p zz:tag-root)
+      (make-directory zz:tag-root))
+    (zz:create-proj-etags)))
+
+;; create cscope to zz:tag-root
+(defun zz:create-root-cscope ()
+  (interactive)
+  (let ((default-directory zz:tag-root))
+    (unless (file-exists-p zz:tag-root)
+      (make-directory zz:tag-root))
+    (zz:create-proj-cscope)))
+
+(defun zz:rscope-autoinit-path (buffer)
+  "Look the directory from zz:tag-root"
+  (when (file-readable-p (concat zz:tag-root rscope-database-name))
+    zz:tag-root))
+
+;; set cscope tag root
+(defun zz:set-root-cscope ()
+  (interactive)
+  (cscope-set-initial-directory zz:tag-root)
+  (setq helm-cscope-search-dir-init (list (list zz:tag-root)))
+  (add-hook 'rscope-autoinit-cscope-dir-hooks
+            (function zz:rscope-autoinit-path))
+  (message "Set cscope init directory: %s" zz:tag-root))
+
+;; unset cscope tag root
+(defun zz:unset-root-cscope ()
+  (interactive)
+  (cscope-unset-initial-directory)
+  (setq helm-cscope-search-dir-init nil)
+  (remove-hook 'rscope-autoinit-cscope-dir-hooks
+               (function zz:rscope-autoinit-path))
+  (message "Unset cscope init directory"))
+
+;;load temp setting
+(when (file-exists-p zz:dev-set-file)
+  (load-file zz:dev-set-file))
+
+
+(provide 'project-custom)
+
+;;; project-custom.el ends here

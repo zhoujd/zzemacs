46de893c94d9a5708a0dfc85979d14dcf2274e31
Zachary Zhou <zchrzhou@gmail.com>
2023-11-16 11:19:22 +0800

    Format code base flycheck

--- lib/python/okapi/framework/dispatch.py
+++ lib/python/okapi/framework/dispatch.py
@@ -7,6 +7,7 @@
 import os
 from . import config
 
+
 class Dispatch:
     def __init__(self, workdir, srcdir, argv, helpdoc=None):
         self.workdir = workdir
@@ -29,13 +30,13 @@ class Dispatch:
             app = None
             match = False
             for num in range(config.maxmatch, 1, -1):
-              if len(self.params) >= num and match is False:
-                cmd = "-".join(self.params[1:num])
-                args = " ".join(self.params[num:])
+                if len(self.params) >= num and match is False:
+                    cmd = "-".join(self.params[1:num])
+                    args = " ".join(self.params[num:])
                 if config.verbose is True:
                     print("try cmd: %s to usaged\n" % cmd)
                     print("try args: %s to usaged\n" % args)
-                app = self.findapp(cmd)
+                    app = self.findapp(cmd)
                 if app is not None:
                     match = True
                     break
@@ -78,8 +79,9 @@ class Dispatch:
             elif appext == "" and config.supportnoext is True:
                 apptype = ""
             if apptype is not None:
+                apppath = self.srcdir + "/" + config.appdirname + "/" + app
                 appinfo.append(apptype)
-                appinfo.append(self.srcdir + "/" + config.appdirname + "/" + app)
+                appinfo.append(apppath)
                 self.appmap[appname] = appinfo
 
     def getappmap(self):
--- lib/python/okapi/utils/common.py
+++ lib/python/okapi/utils/common.py
@@ -7,18 +7,22 @@ import os
 import platform
 from ..framework import config
 
+
 def path2unix(path):
     sysstr = platform.system()
     if sysstr == "Windows":
         path = path.replace("\\", "/")
     return path
 
+
 def getworkdir():
     return path2unix(os.getcwd())
-    
+
+
 def getfiledir(filepath):
     strfilepath = os.path.realpath(filepath)
     return path2unix(os.path.dirname(strfilepath))
+
 
 def setcoredir(dir):
     config.appdirname = dir

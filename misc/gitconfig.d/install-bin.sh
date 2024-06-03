161a713b38ed6be2022cf8d882d2c834f0349287
Zachary Zhou <zchrzhou@gmail.com>
2024-06-03 10:46:13 +0800

    Update for git bin install

--- misc/gitconfig.d/install-bin.sh
+++ misc/gitconfig.d/install-bin.sh
@@ -8,7 +8,11 @@ fi
 
 Install_bin() {
     if [ "$OS" = "Windows_NT" ] ; then
-        echo "The script is not support on Windows"
+        echo "The script is support Git bash on Windows"
+        ln -sfvT $SCRIPT_ROOT/bin/git-lmr /usr/bin/git-lmr
+        ln -sfvT $SCRIPT_ROOT/bin/git-lpr /usr/bin/git-lpr
+        ln -sfvT $SCRIPT_ROOT/bin/git-mr  /usr/bin/git-mr
+        ln -sfvT $SCRIPT_ROOT/bin/git-pr  /usr/bin/git-pr
     else
         echo "Install git tools on Linux"
         sudo ln -sfvT $SCRIPT_ROOT/bin/git-lmr /usr/bin/git-lmr

c790143790a5e697ba08e7fefde5dc58cdab67b8
Zachary Zhou <zchrzhou@gmail.com>
2024-07-11 17:25:22 +0800

    Update for mode line

--- configure/ui-setting.el
+++ configure/ui-setting.el
@@ -9,6 +9,10 @@
   "Return the percent position of the cursor in the current buffer."
   (format-mode-line "%p"))
 
+(defun simple-modeline-segment-end ()
+  "Return the end of mode line."
+  (concat " "))
+
 (defun zz:replace-git-status (tstr)
   (let* ((tstr (replace-regexp-in-string "Git" "" tstr))
          (first-char (substring tstr 0 1))
@@ -33,7 +37,8 @@
          simple-modeline-segment-vc
          simple-modeline-segment-misc-info
          simple-modeline-segment-process
-         simple-modeline-segment-major-mode)))
+         simple-modeline-segment-major-mode
+         simple-modeline-segment-end)))
 
 ;;modern-fringes
 (require 'modern-fringes)

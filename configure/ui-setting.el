e71d46ffa66c50e2fe3e91e017309fdd1b1cc39f
Zachary Zhou <zchrzhou@gmail.com>
2024-07-11 16:38:11 +0800

    Update for simple mode line

--- configure/ui-setting.el
+++ configure/ui-setting.el
@@ -1,10 +1,32 @@
 ;;;; ui-setting.el --- ui config file
 
+;;https://github.com/gexplorer/simple-modeline
+(zz:load-path "site-lisp/simple-modeline")
+(require 'simple-modeline)
+(simple-modeline-mode)
+
+(defun simple-modeline-segment-percent-position ()
+  "Return the percent position of the cursor in the current buffer."
+  (format-mode-line "%p"))
+
+(setq simple-modeline-segments
+      '((simple-modeline-segment-modified
+         simple-modeline-segment-buffer-name
+         simple-modeline-segment-position
+         simple-modeline-segment-percent-position)
+        (simple-modeline-segment-input-method
+         simple-modeline-segment-eol
+         simple-modeline-segment-encoding
+         simple-modeline-segment-vc
+         simple-modeline-segment-misc-info
+         simple-modeline-segment-process
+         simple-modeline-segment-major-mode)))
+
 ;;mode-line
-(zz:load-path "site-lisp/mood-line")
-(require 'mood-line)
-(setq mood-line-glyph-alist mood-line-glyphs-fira-code)
-(mood-line-mode t)
+;(zz:load-path "site-lisp/mood-line")
+;(require 'mood-line)
+;(setq mood-line-glyph-alist mood-line-glyphs-fira-code)
+;(mood-line-mode t)
 
 ;;modern-fringes
 (require 'modern-fringes)
---
+++ site-lisp/simple-modeline/simple-modeline-pkg.el
@@ -0,0 +1,14 @@
+(define-package "simple-modeline" "20210312.1048" "A simple mode-line configuration for Emacs"
+  '((emacs "26.1"))
+  :commit "119d8224a8ae0ee17b09ac1fed6cdb9cb1d048fd" :authors
+  '(("Eder Elorriaga" . "gexplorer8@gmail.com"))
+  :maintainers
+  '(("Eder Elorriaga" . "gexplorer8@gmail.com"))
+  :maintainer
+  '("Eder Elorriaga" . "gexplorer8@gmail.com")
+  :keywords
+  '("mode-line" "faces")
+  :url "https://github.com/gexplorer/simple-modeline")
+;; Local Variables:
+;; no-byte-compile: t
+;; End:

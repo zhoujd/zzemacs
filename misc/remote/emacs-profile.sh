fcbe81a4dfe0b75bc3be5676beec7ae313a7cbea
Zachary Zhou <zchrzhou@gmail.com>
2024-07-31 16:24:10 +0800

    Update for emacs term

--- misc/remote/emacs-profile.sh
+++ misc/remote/emacs-profile.sh
@@ -5,5 +5,6 @@ SCRIPT_ROOT=$(cd $(dirname $0) && pwd)
 echo setup emacs remote start ...
 
 ln -sfv $SCRIPT_ROOT/emacs/emacs_profile $HOME/.emacs_profile
+ln -sfv $SCRIPT_ROOT/emacs/emacs_term $HOME/.emacs_term
 
 echo setup emacs remote end ...

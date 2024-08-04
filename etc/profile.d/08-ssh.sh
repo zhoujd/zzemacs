e754b5a793603625ce01332cca8611118bb9ef3d
Zachary Zhou <zchrzhou@gmail.com>
2024-08-04 22:36:27 +0800

    format code

--- etc/profile.d/08-ssh.sh
+++ etc/profile.d/08-ssh.sh
@@ -14,7 +14,7 @@ cat ~/.ssh/config | \
 grep "^Host " | \
 awk '{print $2}'
 `
-    COMPREPLY=( $(compgen -W "${comp_ssh_hosts}" -- $cur))
+    COMPREPLY=($(compgen -W "${comp_ssh_hosts}" -- $cur))
     return 0
 }
 complete -F _complete_ssh_hosts ssh

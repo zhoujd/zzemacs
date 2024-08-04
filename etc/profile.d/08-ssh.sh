5fb4ebe90e50963628703101fabda25264cf3d7e
Zachary Zhou <zchrzhou@gmail.com>
2024-08-04 22:55:34 +0800

    Update for ssh

--- etc/profile.d/08-ssh.sh
+++ etc/profile.d/08-ssh.sh
@@ -4,17 +4,18 @@ _complete_ssh_hosts ()
 {
     COMPREPLY=()
     cur="${COMP_WORDS[COMP_CWORD]}"
-    comp_ssh_hosts=`cat ~/.ssh/known_hosts | \
+    comp_ssh_hosts=`cat ~/.ssh/known_hosts 2>/dev/null | \
 cut -f 1 -d ' ' | \
 sed -e s/,.*//g | \
 grep -v ^# | \
 uniq | \
 grep -v "\[" ;
-cat ~/.ssh/config | \
+cat ~/.ssh/config ~/.ssh/config.d/* 2>/dev/null | \
 grep "^Host " | \
+grep -v '[?*]' | \
 awk '{print $2}'
 `
-    COMPREPLY=($(compgen -W "${comp_ssh_hosts}" -- $cur))
+    COMPREPLY=( $(compgen -W "${comp_ssh_hosts}" -- ${cur}) )
     return 0
 }
 complete -F _complete_ssh_hosts ssh

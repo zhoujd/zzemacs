ac9b6f1b345bf61b3a77cec0b561615ac375f30b
Zachary Zhou <zchrzhou@gmail.com>
2024-08-04 23:26:46 +0800

    Format code

--- etc/profile.d/08-ssh.sh
+++ etc/profile.d/08-ssh.sh
@@ -5,16 +5,16 @@ _complete_ssh_hosts ()
     COMPREPLY=()
     cur="${COMP_WORDS[COMP_CWORD]}"
     comp_ssh_hosts=`cat ~/.ssh/known_hosts 2>/dev/null | \
-cut -f 1 -d ' ' | \
-sed -e s/,.*//g | \
-grep -v ^# | \
-uniq | \
-grep -v "\[" ;
-cat ~/.ssh/config ~/.ssh/config.d/* 2>/dev/null | \
-grep "^Host " | \
-grep -v '[?*]' | \
-awk '{print $2}'
-`
+                    cut -f 1 -d ' ' | \
+                    sed -e s/,.*//g | \
+                    grep -v ^# | \
+                    uniq | \
+                    grep -v "\[" ;
+                    cat ~/.ssh/config ~/.ssh/config.d/* 2>/dev/null | \
+                    grep --color=never "^Host " | \
+                    grep -v '[?*]' | \
+                    awk '{print $2}'
+                    `
     COMPREPLY=( $(compgen -W "${comp_ssh_hosts}" -- ${cur}) )
     return 0
 }

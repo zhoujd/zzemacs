97b01984d7c686bf26c220f168415dbcbca53666
Zachary Zhou <zchrzhou@gmail.com>
2024-08-06 10:51:54 +0800

    Clean code

--- configure/ivy-setting.el
+++ configure/ivy-setting.el
@@ -3,23 +3,6 @@
 (zz:load-path "site-lisp/ivy")
 (require 'ivy)
 
-(defun zz:ivy-recent-dirs ()
-  "Present a list of recently used directories and open the selected one in dired"
-  (interactive)
-  (let ((recent-dirs
-         (delete-dups
-          (mapcar (lambda (file)
-                    (if (file-directory-p file)
-                        file
-                        (file-name-directory file)))
-                  recentf-list))))
-    (let ((dir (ivy-read "Directory: "
-                         recent-dirs
-                         :re-builder #'ivy--regex
-                         :sort nil
-                         :initial-input nil)))
-      (dired dir))))
-
 
 (provide 'ivy-setting)
 

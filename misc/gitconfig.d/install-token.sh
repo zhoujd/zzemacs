ba074ef9e19152d25953ae4969cec582b7a6b3b7
Zachary Zhou <zchrzhou@gmail.com>
2024-08-08 22:57:14 +0800

    Update for git token

--- misc/gitconfig.d/install-token.sh
+++ misc/gitconfig.d/install-token.sh
@@ -1,9 +1,12 @@
 #!/bin/bash
 
 GITHUB_TOKEN=${GITHUB_TOKEN:-$1}
+GITURL_FILE=~/.gitconfig-url
+
 if [ -z "$GITHUB_TOKEN" ]; then
     echo "Usage: $(basename $0) {GITHUB_TOKEN}"
 else
-    git config --global url."https://${GITHUB_TOKEN}@github.com/zhoujd".insteadOf "https://github.com/zhoujd"
+    git config -f $GITURL_FILE \
+        url."https://${GITHUB_TOKEN}@github.com/zhoujd".insteadOf "https://github.com/zhoujd"
     echo "Install Token Done"
 fi    

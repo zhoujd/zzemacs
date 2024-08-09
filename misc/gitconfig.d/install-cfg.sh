9d12668f68b8a2c36f4a55789ddbbdc4a88e5408
Zachary Zhou <zchrzhou@gmail.com>
2024-08-09 21:59:12 +0800

    Update for git log

--- misc/gitconfig.d/install-cfg.sh
+++ misc/gitconfig.d/install-cfg.sh
@@ -73,14 +73,13 @@ git config --global alias.sts   "stash show --text"
 git config --global alias.ls    "ls-files"
 git config --global alias.ign   "ls-files -o -i --exclude-standard"
 git config --global alias.cat   "cat-file -p"
-git config --global alias.flog  "show --pretty=format: --name-only"
-git config --global alias.last  "log -1 HEAD"
+git config --global alias.flog  "show --pretty=format: --name-only -1"
 
-## git log
-git config --global alias.glog  "log --graph --pretty=format:'%Cred%h%Creset%C(yellow)%d%Creset %s %C(bold blue)<%an>%Creset %Cgreen(%cr)%Creset' --abbrev-commit"
-git config --global alias.hlog  '!git glog -10'
+## log
+git config --global alias.glog  "log --graph --pretty=format:'%Cred%h%Creset%C(yellow)%d%Creset %s %C(bold blue)<%an>%Creset %Cgreen(%cr)%Creset' --abbrev-commit -10"
+git config --global alias.hlog  'log --oneline -10'
 
-## git daemon
+## daemon
 git config --global alias.srv   '!git daemon --base-path=. --export-all --reuseaddr --informative-errors --verbose'
 git config --global alias.hub   '!git daemon --base-path=. --export-all --enable=receive-pack --reuseaddr --informative-errors --verbose'
 

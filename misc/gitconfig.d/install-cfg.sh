77c4400687b6c1aeb83d8b6eb179ef36947340e4
Zachary Zhou <zchrzhou@gmail.com>
2024-08-12 23:16:01 +0800

    Clean code

--- misc/gitconfig.d/install-cfg.sh
+++ misc/gitconfig.d/install-cfg.sh
@@ -76,7 +76,6 @@ git config --global alias.fname "show --pretty=format: --name-only"
 git config --global alias.dname "diff --pretty=format: --name-only"
 
 ## log
-git config --global alias.last  "log -1"
 git config --global alias.glog  "log --graph --pretty=format:'%Cred%h%Creset%C(yellow)%d%Creset %s %C(bold blue)<%an>%Creset %Cgreen(%cr)%Creset' --abbrev-commit"
 git config --global alias.hlog  'log --oneline'
 git config --global alias.lg    '!git glog -10'

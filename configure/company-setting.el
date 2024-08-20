dbcea1819be3464ea95b026346206350c4c6447a
Zachary Zhou <zchrzhou@gmail.com>
2024-08-20 09:11:18 +0800

    Update for company mode

--- configure/company-setting.el
+++ configure/company-setting.el
@@ -5,7 +5,7 @@
 (zz:load-path "site-lisp/company")
 (require 'company)
 (global-company-mode t)
-(setq company-idle-delay nil  ; never (nil)
+(setq company-idle-delay 0.1 ; menu show delay (nil for never)
       company-show-numbers t
       company-complete-number t
       company-dabbrev-ignore-case t

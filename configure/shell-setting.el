c44857bfe22cc50e358858209f3e44cbcfb4f833
Zachary Zhou <zchrzhou@gmail.com>
2024-08-20 22:19:17 +0800

    Update up for company-shell

--- configure/shell-setting.el
+++ configure/shell-setting.el
@@ -429,8 +429,13 @@ Dmitriy Igrishin's patched version of comint.el."
   (message "Select and press TAB to indent: %d" num))
 
 ;;company-shell
-;(require 'company-shell)
-;(add-to-list 'company-backends 'company-shell)
+(require 'company-shell)
+(defun zz:company-shell ()
+  "Input code from company backend using fuzzy matching."
+  (interactive)
+  (company-abort)
+  (let* ((company-backends '(company-shell)))
+    (company-complete-common)))
 
 ;;shell-mode use company-mode
 (defun zz:shell-company-hook ()

23bedfe79aac5668553c65daac1b8a7736ead02e
Zachary Zhou <zchrzhou@gmail.com>
2024-08-21 21:57:06 +0800

    Update for gdb init

--- misc/debug/install.sh
+++ misc/debug/install.sh
@@ -4,6 +4,7 @@ SCRIPT_ROOT=$(cd $(dirname $0) && pwd)
 
 install() {
     local target_dir=~/.gdbinit.d
+    rm -rf $target_dir
     mkdir -p $target_dir
     cp -fv $SCRIPT_ROOT/gdbinit.d/* $target_dir
     cp -fv $SCRIPT_ROOT/gdbinit ~/.gdbinit

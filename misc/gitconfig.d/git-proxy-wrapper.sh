5d5b474c2ed9c08bb23c624f0a63825de9e8eb14
Zachary Zhou <zchrzhou@gmail.com>
2024-09-06 14:38:49 +0800

    Format code for git proxy

--- misc/gitconfig.d/git-proxy-wrapper.sh
+++ misc/gitconfig.d/git-proxy-wrapper.sh
@@ -7,18 +7,24 @@
 # http://www.taiyo.co.jp/~gotoh/ssh/connect.c
 
 # Check host and port in $SOCKS_PROXY
-proxy_host=$(echo $SOCKS_PROXY | cut -d '/' -f 3 | awk -F':' '{print $1}')
-proxy_port=$(echo $SOCKS_PROXY | cut -d '/' -f 3 | awk -F':' '{print $2}')
+with_proxy() {
+    proxy_host=$(echo $SOCKS_PROXY | cut -d '/' -f 3 | awk -F':' '{print $1}')
+    proxy_port=$(echo $SOCKS_PROXY | cut -d '/' -f 3 | awk -F':' '{print $2}')
 
-connect_flag="y"
+    connect_flag="y"
 
-echo $1 | grep -E "\.intel\.com|\.kernel\.org" > /dev/null 2>&1
-if [ $? -eq 0 ] ; then
-    exec connect $@
-else
-    if [ $connect_flag = "y" ] ; then
-        exec connect -S $proxy_host:$proxy_port $@
+    echo $1 | grep -E "\.intel\.com|\.kernel\.org" > /dev/null 2>&1
+    if [ $? -eq 0 ] ; then
+        exec connect $@
     else
-        exec socat STDIO SOCKS4:$proxy_host:$1:$2,socksport=$proxy_port
+        if [ $connect_flag = "y" ] ; then
+            exec connect -S $proxy_host:$proxy_port $@
+        else
+            exec socat STDIO SOCKS4:$proxy_host:$1:$2,socksport=$proxy_port
+        fi
     fi
+}
+
+if [ -n "$SOCK_PROXY" ]; then
+    with_proxy
 fi

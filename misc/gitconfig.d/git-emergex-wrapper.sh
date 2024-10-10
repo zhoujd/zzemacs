929459ea5ae2d324df236666cf1ae8c60fae73cc
Zachary Zhou <zchrzhou@gmail.com>
2024-10-10 19:16:59 +0800

    Format tools

--- misc/gitconfig.d/git-emergex-wrapper.sh
+++ misc/gitconfig.d/git-emergex-wrapper.sh
@@ -12,18 +12,12 @@ if [ $# != 4 ]; then
 \$2 => \$LOCAL
 \$3 => \$REMOTE
 \$4 => \$MERGED
-
 This is merge tools for emacs"
     exit 1
 fi
 
 main() {
-    if [ "$OS" = "Windows_NT" ] ; then
-        ELISP_PATH="$(cd $(dirname $0)/../elisp && pwd -W)"
-    else
-        ELISP_PATH="$(cd $(dirname $0)/../elisp && pwd)"
-    fi
-
+    ELISP_PATH=${ELISP_PATH:-"~/zzemacs/elisp"}
     emacs --quick \
           --eval "(load-file \"$ELISP_PATH/ediff-sample.el\")" \
           --eval "(ediff-merge-files \"$2\" \"$3\" \"$1\" \"$4\")" \

09189dcfb694c194855fbd0a099dab207b2224a9
Zachary Zhou <zchrzhou@gmail.com>
2025-03-23 20:21:51 +0800

    Update python install-deps.sh

--- third-party/python/install-deps.sh
+++ third-party/python/install-deps.sh
@@ -3,19 +3,23 @@
 SCRIPT_ROOT=$(cd $(dirname "${BASH_SOURCE[0]}") && pwd)
 echo "For python os develop start ..."
 
-case $1 in
+. /etc/os-release
+
+case $ID in
     centos )
+        echo "Install on $ID"
         sudo yum install -y python3-pip
         sudo yum install -y python3-venv
         sudo yum install -y python3-virtualenv
         ;;
     ubuntu | debian )
+        echo "Install on $ID"
         sudo apt install -y python3-pip
         sudo apt install -y python3-venv
         sudo apt install -y python3-virtualenv
         ;;
     * )
-        echo "Usage: $0 {centos|ubuntu|debian}"
+        echo "Install on $ID is not supported"
         ;;
 esac
 

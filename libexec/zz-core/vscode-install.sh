2ec2ad848ec083155fff6a010e542955c59e2c15
Zachary Zhou <zchrzhou@gmail.com>
2025-03-23 16:30:16 +0800

    Update vscode-install.sh

--- libexec/zz-core/vscode-install.sh
+++ libexec/zz-core/vscode-install.sh
@@ -1,5 +1,11 @@
 #!/bin/bash
 
+## https://code.visualstudio.com/
+## https://code.visualstudio.com/download
+
+sudo apt update
+sudo apt install -y curl gpg software-properties-common apt-transport-https
+
 sudo tee /etc/apt/sources.list.d/vs-code.list <<EOF
 deb [arch=amd64] http://packages.microsoft.com/repos/vscode stable main
 EOF
@@ -8,6 +14,6 @@ curl https://packages.microsoft.com/keys/microsoft.asc | \
     sudo gpg --dearmor -o /etc/apt/trusted.gpg.d/microsoft.gpg
 
 sudo apt-get update
-sudo apt-get install code
+sudo apt-get install -y code
 
 echo "VSCode Install Done"

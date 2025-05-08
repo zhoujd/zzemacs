58bf2ba642a45766293bcadc9d34e6da2fae69d7
Zachary Zhou <zchrzhou@gmail.com>
2025-05-08 22:37:01 +0800

    Update install-docker.sh

--- docker/script/install-docker.sh
+++ docker/script/install-docker.sh
@@ -14,7 +14,7 @@ ubuntu() {
         && mkdir -p /etc/apt/keyrings \
         && curl -fsSL https://download.docker.com/linux/ubuntu/gpg | gpg --dearmor -o /etc/apt/keyrings/docker.gpg
     echo "deb [arch=$(dpkg --print-architecture) signed-by=/etc/apt/keyrings/docker.gpg] https://download.docker.com/linux/ubuntu $(lsb_release -cs) stable" \
-        | sudo tee /etc/apt/sources.list.d/docker.list
+        | tee /etc/apt/sources.list.d/docker.list
     apt-get update \
         && apt-get install -y --no-install-recommends \
                    docker-ce docker-ce-cli containerd.io docker-compose-plugin \

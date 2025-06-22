3422dbe8270a0fc7eca1f250a81e3e04d29b6133
Zachary Zhou <zchrzhou@gmail.com>
2025-06-22 10:31:42 +0800

    Update misc/font/install.sh

--- misc/font/install.sh
+++ misc/font/install.sh
@@ -6,6 +6,8 @@ install_font() {
     target=~/.config/fontconfig/fonts.conf
     rm -fv $target
     cp -vf $SCRIPT_ROOT/fonts.conf $target
+    fc-match monospace
+    fc-match monospace:bold
     echo "Install font done"
 }
 

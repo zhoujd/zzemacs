278aaaccd7f661943597e9288d836579e26a1a7f
Zachary Zhou <zchrzhou@gmail.com>
2025-06-24 13:42:13 +0800

    Update configure/company-setting.el on colors

--- configure/company-setting.el
+++ configure/company-setting.el
@@ -67,32 +67,6 @@
   ((kbd "C-c C-/") 'company-other-backend)
   ((kbd "TAB")     'company-indent-or-complete-common))
 
-(defun zz/company-hook ()
-  (require 'color)
-  (let ((darker-fg    "#e4e4ef")
-        (darker-fg+1  "#f4f4ff")
-        (darker-fg+2  "#f5f5f5")
-        (darker-bg-1  "#101010")
-        (darker-bg    "#181818")
-        (darker-bg+1  "#282828")
-        (darker-bg+2  "#453d41")
-        (darker-green "#73c936")
-        (darker-brown "#cc8c3c"))
-    (custom-set-faces
-     `(company-tooltip ((t (:foreground ,darker-fg :background ,darker-bg+1))))
-     `(company-tooltip-annotation ((t (:foreground ,darker-brown :background ,darker-bg+1))))
-     `(company-tooltip-annotation-selection ((t (:foreground ,darker-brown :background ,darker-bg-1))))
-     `(company-tooltip-selection ((t (:foreground ,darker-brown :background ,darker-bg-1))))
-     `(company-tooltip-mouse ((t (:background ,darker-bg-1))))
-     `(company-tooltip-common ((t (:foreground ,darker-green))))
-     `(company-tooltip-common-selection ((t (:foreground ,darker-green))))
-     `(company-scrollbar-fg ((t (:background ,darker-bg-1))))
-     `(company-scrollbar-bg ((t (:background ,darker-bg+2))))
-     `(company-preview ((t (:background ,darker-green))))
-     `(company-preview-common ((t (:foreground ,darker-green :background ,darker-bg-1))))
-     )))
-(add-hook 'company-mode-hook 'zz/company-hook)
-
 
 (provide 'company-setting)
 

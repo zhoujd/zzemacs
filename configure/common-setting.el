0f48f719ab052f645d5f83539fd7678d03874b50
Zachary Zhou <zchrzhou@gmail.com>
2025-06-24 13:32:45 +0800

    Add gruvbox themes

--- configure/common-setting.el
+++ configure/common-setting.el
@@ -103,9 +103,12 @@
   (mapc #'disable-theme custom-enabled-themes))
 
 ;;color theme
-(zz/load-path "site-lisp/emacs-color-themes")
-(require 'emacs-color-themes)
-(load-theme 'zz t)
+;(zz/load-path "site-lisp/emacs-color-themes")
+;(require 'emacs-color-themes)
+;(load-theme 'zz t)
+(zz/load-path "site-lisp/emacs-gruvbox-themes")
+(require 'gruvbox-theme)
+(load-theme 'gruvbox t)
 
 ;;default-frame-alist or initial-frame-alist
 (add-to-list 'default-frame-alist '(mouse-color . "white"))

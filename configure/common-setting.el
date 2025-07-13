5dce62775e774670738780adb9ffb7ac51adaa98
Zachary Zhou <zchrzhou@gmail.com>
2025-07-13 23:55:22 +0800

    Update configure/common-setting.el

--- configure/common-setting.el
+++ configure/common-setting.el
@@ -276,8 +276,8 @@
   (if (display-graphic-p frame)
       (progn
         (custom-set-faces
-         '(mode-line-inactive ((t :background "#504945" :foreground "#000000" :box (:line-width 1 :color "#504945"))))
-         '(mode-line ((t :background "#665c54" :foreground "#000000" :box (:line-width 1 :color "#665c54")))))
+         '(mode-line-inactive ((t :background "#32302f" :foreground "#1c1c1c" :box (:line-width 1 :color "#32302f"))))
+         '(mode-line ((t :background "#504945" :foreground "#1c1c1c" :box (:line-width 1 :color "#504945")))))
         (message "Start window system"))
       (message "Start non-window system")))
 ;;run for already-existing frames

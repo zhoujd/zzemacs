1dcf93dc62ac71562ce8ca8cd3c8fa75fde62740
Zachary Zhou <zchrzhou@gmail.com>
2025-09-13 20:08:47 +0800

    Update git-ver.sh

--- bin/git-ver.sh
+++ bin/git-ver.sh
@@ -5,8 +5,8 @@ SED=$(command -v sed)
 DIR=$(${GIT} rev-parse --show-toplevel 2>/dev/null)
 
 if [ -d "${DIR}/.git/" ] && [ -n "${GIT}" ]; then
-    BRTG="$(${GIT} describe --tags --always --all | ${SED} -e 's:heads/::')"
-    REV="$(${GIT} describe --tags --always --dirty| ${SED} -e 's:-g\([a-f0-9]\{7\}\):-git-\1:g')"
+    BRTG=$(${GIT} describe --tags --always --all | ${SED} -e 's:heads/::')
+    REV=$(${GIT} describe --tags --always --dirty| ${SED} -e "s:-g\([a-f0-9]\{7\}\):-git-\1:g")
     echo "#define GIT_VERSION ${REV} (${BRTG})"
 else
     echo "#undef GIT_VERSION"

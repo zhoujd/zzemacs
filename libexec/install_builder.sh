4f10082ee6c26b6e2475bf0be7aef54d22f3fc37
Zachary Zhou <zchrzhou@gmail.com>
2025-09-24 07:38:19 +0800

    Add install_builder.sh

---
+++ libexec/install_builder.sh
@@ -0,0 +1,33 @@
+#!/bin/bash
+
+die() {
+    echo "$1"
+    exit 1
+}
+
+if [ $# -ne 1 ]; then
+    echo "Missing arch"
+    exit 1
+fi
+ARCH="${1}"
+case $ARCH in
+    x86_64|i686|aarch64)
+        ARCH="${ARCH}-linux-musl"
+        ;;
+    x86)
+        ARCH="i686-linux-musl"
+        ;;
+    arm)
+        ARCH="arm-linux-musleabihf"
+        ;;
+    *)
+        echo "Invalid arch ${ARCH}"
+        exit 1
+        ;;
+esac
+HOST=http://musl.cc
+echo "Fetching ${HOST}/${ARCH}-cross.tgz"
+cd $PWD
+curl -so ${ARCH}-cross.tgz ${HOST}/${ARCH}-cross.tgz || die "Failed to download build compiler package"
+tar -xf ${ARCH}-cross.tgz || die "Failed to extract build compiler package"
+rm ${ARCH}-cross.tgz || die "Failed to remove build compiler package"

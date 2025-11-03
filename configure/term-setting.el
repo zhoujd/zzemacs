5cfeb123cb269a6190245d9e83bf411456aba320
Zachary Zhou <zchrzhou@gmail.com>
2025-11-03 10:11:08 +0800

    Add eat term

--- configure/term-setting.el
+++ configure/term-setting.el
@@ -304,6 +304,13 @@
                             (if (string= event "finished\n")
                                        (kill-buffer buff))))))))
 
+;;Eat: Emulate A Terminal
+;;https://codeberg.org/akib/emacs-eat
+;;https://github.com/kephale/emacs-eat
+(require 'eat)
+(setq eat-kill-buffer-on-exit t
+      eat-enable-shell-prompt-annotation nil)
+
 
 (provide 'term-setting)
 

31798e68fc34cf0a056635cd50777ef79d87f324
Zachary Zhou <zchrzhou@gmail.com>
2026-03-06 10:00:03 +0800

    Update 09-editor.sh

--- etc/profile.d/09-editor.sh
+++ etc/profile.d/09-editor.sh
@@ -1,9 +1,14 @@
 ### editor
 
-for e in me ec vi; do
-    if command -v $e >/dev/null 2>&1; then
-        export EDITOR=$e
-        export VISUAL=$e
-        break
-    fi
-done
+if [ -n "$INSIDE_EMACS" ]; then
+    export EDITOR=ec
+    export VISUAL=ec
+else
+    for e in me vi; do
+        if command -v $e >/dev/null 2>&1; then
+            export EDITOR=$e
+            export VISUAL=$e
+            break
+        fi
+    done
+fi

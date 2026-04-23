f2ca4a09da854a3898cf8c05926a50aefe7012c9
Zachary Zhou <zchrzhou@gmail.com>
2026-04-23 15:26:18 +0800

    Add xterm-256color file

--- misc/term/install.sh
+++ misc/term/install.sh
@@ -5,7 +5,7 @@ SCRIPT_ROOT=$(cd $(dirname $0) && pwd)
 install() {
     local target=~/.terminfo
     mkdir -p $target
-    cp -rfv ${SCRIPT_ROOT}/terminfo/{e,p,r,s,t} $target
+    cp -rfv ${SCRIPT_ROOT}/terminfo/{e,p,r,s,t,x} $target
     echo "Install Done"
 }
 

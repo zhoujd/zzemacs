02390faea7f3c2e394b02921ad3e81741579f0c4
Zachary Zhou <zchrzhou@gmail.com>
2026-09-06 17:04:11 +0800

    Update misc/remote/tool.sh

--- misc/remote/tool.sh
+++ misc/remote/tool.sh
@@ -2,17 +2,19 @@
 
 source /etc/os-release
 
-if [ "$ID" == "ubuntu" ]; then
-    echo "Run on Ubuntu"
-    sudo apt install -y silversearcher-ag cscope emacs-bin-common
-    sudo apt install -y ccls clangd
-elif [ "$ID" == "centos" ]; then
-    echo "Run on CentOS"
-    sudo yum install -y epel-release
-    sudo yum install -y silversearcher-ag cscope emacs-bin-common
-    sudo yum install -y ccls clangd
-else
-    echo "Unsupport OS: $ID"
-fi
+case $ID in
+   ubuntu|debian )
+      echo "Run on $ID"
+      sudo apt install -y ccls clangd
+      ;;
+   centos )
+      echo "Run on $ID"
+      sudo yum install -y epel-release
+      sudo yum install -y ccls clangd
+      ;;
+   * )
+      echo "Unsupport OS: $ID"
+      ;;
+esac
 
 echo "Install tool done"

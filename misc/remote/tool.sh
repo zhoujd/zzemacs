922263b3f11cf8c3630649b1a16c8092326ccbd9
Zachary Zhou <zchrzhou@gmail.com>
2026-09-07 15:17:41 +0800

    Update misc/remote/tool.sh

--- misc/remote/tool.sh
+++ misc/remote/tool.sh
@@ -2,19 +2,42 @@
 
 source /etc/os-release
 
-case $ID in
-   ubuntu|debian )
-      echo "Run on $ID"
-      sudo apt install -y ccls clangd
-      ;;
-   centos )
-      echo "Run on $ID"
-      sudo yum install -y epel-release
-      sudo yum install -y ccls clangd
-      ;;
-   * )
-      echo "Unsupport OS: $ID"
-      ;;
-esac
+install_cc() {
+    case $ID in
+        ubuntu|debian )
+            echo "Run on $ID"
+            sudo apt install -y ccls clangd
+            ;;
+        centos )
+            echo "Run on $ID"
+            sudo yum install -y epel-release
+            sudo yum install -y ccls clangd
+            ;;
+        * )
+            echo "Unsupport OS: $ID"
+            ;;
+    esac
+    echo "Install cc tool done"
+}
 
-echo "Install tool done"
+
+install_py() {
+    pip3 install python-lsp-server[all]
+    echo "Install python tool done"
+}
+
+case $1 in
+    cc )
+        install_cc
+        ;;
+    py )
+        install_py
+        ;;
+    -h|--help )
+        echo "$(basename $0) {cc|py|-h|--help}"
+        ;;
+    all|* )
+        install_cc
+        install_py
+        ;;
+esac

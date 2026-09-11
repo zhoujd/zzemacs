59f841a5f3b69679c590bb8df8ffe3c0537338f0
Zachary Zhou <zchrzhou@gmail.com>
2026-09-11 13:01:32 +0800

    Update elisp vterm-custom.el

--- elisp/vterm-custom.el
+++ elisp/vterm-custom.el
@@ -1,7 +1,6 @@
 ;;;; vterm-custom.el --- sample config file
 ;;;
 
-
 (zz/load-path "site-lisp/emacs-vterm")
 
 (require 'vterm)
@@ -21,7 +20,9 @@
 (defun zz/vterm-hook ()
   (defkeys-map vterm-mode-map
     ((kbd "C-c M-q") 'vterm-send-next-key)
-    ((kbd "C-c M-o") 'vterm-clear))
+    ((kbd "C-c M-o") 'vterm-clear)
+    ((kbd "C-c M-n") 'multi-vterm-next)
+    ((kbd "C-c M-p") 'multi-vterm-prev))
   (defkeys-map vterm-copy-mode-map
     ((kbd "M-w") 'vterm-copy-mode-done)))
 
@@ -29,25 +30,69 @@
 
 (defun zz/get-vterm ()
   (interactive)
-  (with-temp-buffer
-    (let* ((default-directory (file-name-as-directory
-                               (ido-read-directory-name "Directory: "))))
-      (multi-vterm))))
+  (let ((dir (file-name-as-directory (ido-read-directory-name "Directory: "))))
+    (let ((default-directory dir))
+      (multi-vterm))
+    (delete-other-windows)))
 
 (defun zz/home-vterm ()
   (interactive)
-  (with-temp-buffer
-    (let* ((default-directory "~"))
-      (multi-vterm))))
+  (let ((default-directory (expand-file-name "~")))
+    (multi-vterm)
+    (delete-other-windows)))
+
+(defun zz/vterm-list ()
+  (let (zz/vterms)
+    (dolist (b (buffer-list))
+      (when (string-match "^\\*vterminal<[0-9]+>\\*$" (buffer-name b))
+        (push (buffer-name b) zz/vterms)))
+    (nreverse zz/vterms)))
+
+(defun zz/switch-to-vterm (buf-name)
+  (interactive
+   (list (ido-completing-read "Vterm name: " (zz/vterm-list))))
+  (if (and buf-name (not (string-empty-p buf-name)) (get-buffer buf-name))
+      (progn
+        (switch-to-buffer buf-name)
+        (message "Switched to %s" buf-name)
+        (delete-other-windows))
+      (multi-vterm)
+      (delete-other-windows)
+      (message "Created and switched to new: %s" (buffer-name (current-buffer)))))
+
+(defun zz/get-remote-vterm (host)
+  "Connect to a remote host using standard vterm."
+  (let* ((buffer-name (format "*vterm ssh: %s*" host))
+         (vterm-buffer (get-buffer buffer-name)))
+    (if vterm-buffer
+        (pop-to-buffer vterm-buffer)
+        (with-current-buffer (generate-new-buffer buffer-name)
+          (vterm-mode)
+          (setq-local vterm-shell (executable-find "ssh"))
+          (setq-local vterm-kill-buffer-on-exit t)
+          (setq-local vterm-buffer-name buffer-name)
+          (vterm-send-string (format "ssh %s\n" host))
+          (pop-to-buffer (current-buffer))))
+    (message "Remote %s ready via vterm" host)))
 
 (defun zz/remote-vterm ()
-  "Open a remote vterm to a host"
+  "Connect to a remote term by parsing ssh config and using vterm."
   (interactive)
   (with-temp-buffer
-    (let* ((default-directory (zz/get-host)))
-      (when (file-exists-p default-directory)
-        (call-interactively 'zz/get-vterm))
-      )))
+    (let* ((path (mapconcat
+                  (lambda (x)
+                    (when (file-exists-p x)
+                      (concat x)))
+                  '("~/.ssh/config"
+                    "~/.ssh/config.d/*")
+                  " "))
+           (grep "grep -i -e '^host ' | grep -v '[*?]' | grep -v 'git'")
+           (awk "awk '/^Host/{if (NR!=1)print \"\"; printf $2}'")
+           (command (format "cat %s | %s | %s" path grep awk))
+           (host (ido-completing-read "Host: "
+                                      (split-string
+                                       (shell-command-to-string command)))))
+      (zz/get-remote-vterm host))))
 
 
 (provide 'vterm-custom)
